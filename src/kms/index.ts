0444f2f63c047689d62d0f01e338b0ed43c706b8
Samuel Sramko <samuel.sramko@tatum.io>
2020-07-18 13:18:30 +0200

    KMS - remove pagination

--- src/kms/index.ts
+++ src/kms/index.ts
@@ -10,6 +10,6 @@ export const completePendingTransactionKMS = async (id: string, txId: string): P
     await axios.put(`https://api.tatum.io/v3/ledger/kms/${id}/${txId}`, undefined, {headers: {'x-api-key': process.env.TATUM_API_KEY}});
 };
 
-export const getPendingTransactionsKMSByChain = async (chain: Currency, pageSize: number = 50, offset: number = 0): Promise<TransactionKMS[]> => {
-    return (await axios.get(`https://api.tatum.io/v3/ledger/kms/pending/${chain}?pageSize=${pageSize}&offset=${offset}`, {headers: {'x-api-key': process.env.TATUM_API_KEY}})).data;
+export const getPendingTransactionsKMSByChain = async (chain: Currency): Promise<TransactionKMS[]> => {
+    return (await axios.get(`https://api.tatum.io/v3/ledger/kms/pending/${chain}`, {headers: {'x-api-key': process.env.TATUM_API_KEY}})).data;
 };
