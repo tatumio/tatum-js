f943ecae6290a7660523f51ea8b7a25e4a7ef5a6
Samuel Sramko <samuel.sramko@tatum.io>
2020-07-20 19:16:47 +0200

    fix bug with KMS url

--- src/kms/index.ts
+++ src/kms/index.ts
@@ -7,9 +7,9 @@ export const getPendingTransactionKMS = async (id: string): Promise<TransactionK
 };
 
 export const completePendingTransactionKMS = async (id: string, txId: string): Promise<void> => {
-    await axios.put(`https://api.tatum.io/v3/ledger/kms/${id}/${txId}`, undefined, {headers: {'x-api-key': process.env.TATUM_API_KEY}});
+    await axios.put(`https://api.tatum.io/v3/kms/${id}/${txId}`, undefined, {headers: {'x-api-key': process.env.TATUM_API_KEY}});
 };
 
 export const getPendingTransactionsKMSByChain = async (chain: Currency): Promise<TransactionKMS[]> => {
-    return (await axios.get(`https://api.tatum.io/v3/ledger/kms/pending/${chain}`, {headers: {'x-api-key': process.env.TATUM_API_KEY}})).data;
+    return (await axios.get(`https://api.tatum.io/v3/kms/pending/${chain}`, {headers: {'x-api-key': process.env.TATUM_API_KEY}})).data;
 };
