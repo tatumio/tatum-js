bde5d1ffefe96e9e4b2777ea4a228ed417d35ebe
Samuel Sramko <samuel.sramko@tatum.io>
2020-09-30 13:09:41 +0200

    change API url to default api-eu1

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import {Currency} from './model';
 
-export const TATUM_API_URL = 'https://api.tatum.io';
+export const TATUM_API_URL = 'https://api-eu1.tatum.io';
 
 export const TEST_VET_URL = 'https://sync-testnet.vechain.org/';
 export const VET_URL = 'https://sync-mainnet.vechain.org/';
