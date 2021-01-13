0dd300381022bdf73ccd8a930340a1314f49ccc4
Samuel Sramko <samuel.sramko@tatum.io>
2021-01-13 14:03:36 +0100

    TRC10 detail props

--- src/model/response/tron/TronTrc10.ts
+++ src/model/response/tron/TronTrc10.ts
@@ -1,15 +1,10 @@
 export interface TronTrc10 {
     precision: number;
-    num: number;
     description: string;
     id: number;
     abbr: string;
     url: string;
     name: string;
-    voteScore: number;
-    startTime: number;
-    trxNum: number;
     totalSupply: number;
     ownerAddress: string;
-    endTime: number;
 }
