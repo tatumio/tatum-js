1ebc5f06ef9c02cbc30e7c2a32fb10bc19a144e9
Samuel Sramko <samuel.sramko@tatum.io>
2021-03-15 13:41:43 +0100

    quorum create record

--- src/model/request/CreateRecord.ts
+++ src/model/request/CreateRecord.ts
@@ -16,13 +16,13 @@ import {Fee} from './Fee';
 
 export class CreateRecord {
 
-    @ValidateIf(o => [Currency.ETH, Currency.QUORUM].includes(o.chain) && ((o.fromPrivateKey && o.signatureId) || !o.signatureId))
+    @ValidateIf(o => o.chain === Currency.ETH && ((o.fromPrivateKey && o.signatureId) || !o.signatureId))
     @Validate(SignatureIdValidator)
     @IsNotEmpty()
     @Length(66, 66)
     public fromPrivateKey: string;
 
-    @ValidateIf(o => [Currency.ETH, Currency.QUORUM].includes(o.chain) && ((o.fromPrivateKey && o.signatureId) || !o.fromPrivateKey))
+    @ValidateIf(o => o.chain === Currency.ETH && ((o.fromPrivateKey && o.signatureId) || !o.fromPrivateKey))
     @Validate(SignatureIdValidator)
     @Length(36, 36)
     @IsUUID('4')
