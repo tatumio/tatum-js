ef26e5b6c04cd05b1da969287f0c31e779e45326
Samuel Sramko <samuel.sramko@tatum.io>
2021-05-08 08:21:05 +0200

    fix validation for ETH/CELO offchain

--- src/model/request/TransferEthOffchain.ts
+++ src/model/request/TransferEthOffchain.ts
@@ -6,19 +6,19 @@ export class TransferEthOffchain extends BaseTransferEthErc20Offchain {
     @Length(1, 500)
     @ValidateIf(o => (o.mnemonic && o.index >= 0 && o.privateKey) || (o.index >= 0 && o.privateKey))
     @IsNotEmpty()
-    public mnemonic: string;
+    public mnemonic?: string;
 
     @ValidateIf(o => (o.mnemonic && o.index >= 0 && o.privateKey) || o.mnemonic)
     @Min(0)
     @IsNotEmpty()
     @IsInt()
     @Max(2147483647)
-    public index: number;
+    public index?: number;
 
     @ValidateIf(o => (o.mnemonic && o.index >= 0 && o.privateKey) || (!o.mnemonic && !o.signatureId && !o.index))
     @Length(66, 66)
     @IsNotEmpty()
-    public privateKey: string;
+    public privateKey?: string;
 
     @ValidateIf(o => !o.mnemonic && !o.privateKey)
     @Length(36, 36)
