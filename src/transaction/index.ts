03095cd60be5716e9263201407643d60372fb9ed
Samuel Sramko <samuel.sramko@tatum.io>
2021-05-26 14:48:53 +0200

    Flow NFT support - export from index

--- src/transaction/index.ts
+++ src/transaction/index.ts
@@ -1,6 +1,7 @@
 export * from './bitcoin';
 export * from './bnb';
 export * from './litecoin';
+export * from './flow';
 export * from './bcash';
 export * from './dogecoin';
 export * from './vet';
