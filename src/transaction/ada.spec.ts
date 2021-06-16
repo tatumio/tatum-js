5dc630e41826705e54e44518ae65b455704c256f
Hathoriel <lukas.kotol@tatum.io>
2021-06-16 11:12:01 +0200

    Added Cardano blockchain integration

---
+++ src/transaction/ada.spec.ts
@@ -0,0 +1,69 @@
+import { TransferBtcBasedBlockchain } from '../model';
+import { prepareAdaTransaction, sendAdaTransaction } from './ada'
+
+describe('ADA transactions', () => {
+  it('should test preparing ADA fromAddress transaction', async () => {
+    const body = new TransferBtcBasedBlockchain();
+    body.fromAddress = [{
+      address: 'addr_test1qqnp3dfw869cz74jfvkmvrs47fwmhwx7c96n44u80w2mrskkrxsadwhjss06kp6tke68rt0apcl864tlqw40ddy5jumqr7g7xy',
+      privateKey: 'a01136f20a0a1fb0ec7833111cbef4207f2b7fda0f942c6fccba0aa72fed115dc70795a4d49ec6b50e3bb1820e910389aed8ab0d75ec88eac0883b22d49473b17a72599737528714076956f71377dab57b1ceea5c63ac6669d547a22058d1f4402f74c3ba9d7061e0f4ac3c29770a20f91ae2e648059cf1010600e6e0f6b921d',
+    }];
+    body.to = [{
+      address: 'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3',
+      value: 1,
+    }, {
+      address: 'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3',
+      value: 1,
+    }];
+    try {
+      const txData = await prepareAdaTransaction(body);
+      expect(txData).toBe('83a40081825820ca5cd6764d36000c99576d2ab8d2d58e1ba4eb7f2cc279a3f6442e2929123dfd020183825839000743d16cfe3c4fcc0c11c2403bbc10dbc7ecdd4477e053481a368e7a06e2ae44dff6770dc0f4ada3cf4cf2605008e27aecdb332ad349fda71a000f4240825839000743d16cfe3c4fcc0c11c2403bbc10dbc7ecdd4477e053481a368e7a06e2ae44dff6770dc0f4ada3cf4cf2605008e27aecdb332ad349fda71a000f4240825839002618b52e3e8b817ab24b2db60e15f25dbbb8dec1753ad7877b95b1c2d619a1d6baf2841fab074bb67471adfd0e3e7d557f03aaf6b49497361a3aadc56f021a00029c7d031a01c4b82ba100818258207a72599737528714076956f71377dab57b1ceea5c63ac6669d547a22058d1f4458403c30ab577e96c9a4b4fb9e0e970f831033344d8e5efbfe20cce6b883dfeea4f31044bcb6ef1333e20202b4f7092d46667680478d4417c54a10582516ce55d909f6');
+    } catch (e) {
+      console.error(e);
+      fail(e);
+    }
+  });
+
+  it('should test sending ADA fromAddress transaction', async () => {
+    const body = new TransferBtcBasedBlockchain();
+    body.fromAddress = [{
+      address: 'addr_test1qqnp3dfw869cz74jfvkmvrs47fwmhwx7c96n44u80w2mrskkrxsadwhjss06kp6tke68rt0apcl864tlqw40ddy5jumqr7g7xy',
+      privateKey: 'a01136f20a0a1fb0ec7833111cbef4207f2b7fda0f942c6fccba0aa72fed115dc70795a4d49ec6b50e3bb1820e910389aed8ab0d75ec88eac0883b22d49473b17a72599737528714076956f71377dab57b1ceea5c63ac6669d547a22058d1f4402f74c3ba9d7061e0f4ac3c29770a20f91ae2e648059cf1010600e6e0f6b921d',
+    }];
+    body.to = [{
+      address: 'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3',
+      value: 1,
+    }, {
+      address: 'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3',
+      value: 1,
+    }];
+    try {
+      const txData = await sendAdaTransaction(body);
+      expect(txData).toHaveProperty('txId');
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+  it('should test sending ADA fromUTXO transaction', async () => {
+    const body = new TransferBtcBasedBlockchain();
+    body.fromUTXO = [{
+      txHash: 'c44db60c36824f1d8901ee1f7d5597d426fe00c1fa8e6872e70bef318e4bc051',
+      index: 2,
+      privateKey: 'a01136f20a0a1fb0ec7833111cbef4207f2b7fda0f942c6fccba0aa72fed115dc70795a4d49ec6b50e3bb1820e910389aed8ab0d75ec88eac0883b22d49473b17a72599737528714076956f71377dab57b1ceea5c63ac6669d547a22058d1f4402f74c3ba9d7061e0f4ac3c29770a20f91ae2e648059cf1010600e6e0f6b921d',
+    }];
+    body.to = [{
+      address: 'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3',
+      value: 1,
+    }, {
+      address: 'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3',
+      value: 1,
+    }];
+    try {
+      const txData = await sendAdaTransaction(body);
+      expect(txData).toHaveProperty('txId');
+    } catch (e) {
+      fail(e);
+    }
+  });
+});
--- src/transaction/ada.ts
+++ src/transaction/ada.ts
@@ -1,82 +1,215 @@
-import CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
-import {get} from '../connector/tatum';
-import {AdaUTxo, TransferAda} from '../model';
+import {
+  Address,
+  BigNum,
+  Bip32PrivateKey, hash_transaction,
+  LinearFee, make_vkey_witness, Transaction, TransactionBody,
+  TransactionBuilder, TransactionHash, TransactionInput, TransactionOutput, TransactionWitnessSet, Value, Vkeywitnesses,
+} from '@emurgo/cardano-serialization-lib-nodejs';
+import BigNumber from 'bignumber.js'
+import { adaBroadcast, adaGetBlockChainInfo, adaGetTransaction, adaGetUtxos } from '../blockchain/ada'
+import { validateBody } from '../connector/tatum';
+import { AdaUtxo, Currency, FromAddress, FromUTXO, To, TransactionKMS, TransferBtcBasedBlockchain } from '../model';
 
 /**
- * Prepare a signed ADA transaction with the private key locally. Nothing is broadcasted to the blockchain.
- * @param body content of the transaction to prepare.
+ * Prepare a signed Ada transaction with the private key locally. Nothing is broadcasted to the blockchain.
+ * @param transferBtcBasedBlockchain content of the transaction to prepare.
  * @returns raw transaction data in hex, to be broadcasted to blockchain.
  */
-export const prepareADATransaction = async (body: TransferAda) => {
-  const fromAddress = CardanoWasm.Address.from_bech32(body.from);
-  const toAddress = CardanoWasm.Address.from_bech32(body.to);
+export const prepareAdaTransaction = async (transferBtcBasedBlockchain: TransferBtcBasedBlockchain) => {
+  await validateBody(transferBtcBasedBlockchain, TransferBtcBasedBlockchain);
+  const txBuilder = await initTransactionBuilder()
+  const { to } = transferBtcBasedBlockchain
 
-  const utxos : AdaUTxo[] = await get(`/v3/cardano/${body.from}/utxos`);
-  let fromQuantity = 0;
-  for (const utxo of utxos) {
-    fromQuantity += parseInt(utxo.value);
+  const { privateKeysToSign, amount: fromAmount } = await addInputs(txBuilder, transferBtcBasedBlockchain)
+  const toAmount = addOutputs(txBuilder, to)
+  await processFeeAndRest(txBuilder, fromAmount, toAmount, transferBtcBasedBlockchain)
+
+  return signTransaction(txBuilder, transferBtcBasedBlockchain, privateKeysToSign)
+};
+
+/**
+ * Send Ada transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
+ * This operation is irreversible.
+ * @param body content of the transaction to broadcast
+ * @returns transaction id of the transaction in the blockchain
+ */
+export const sendAdaTransaction = async (body: TransferBtcBasedBlockchain) => {
+  return adaBroadcast(await prepareAdaTransaction(body))
+}
+
+/**
+ * Sign Ada pending transaction from Tatum KMS
+ * @param tx pending transaction from KMS
+ * @param privateKeys private keys to sign transaction with.
+ * @returns transaction data to be broadcast to blockchain.
+ */
+export const signAdaKMSTransaction = async (tx: TransactionKMS, privateKeys: string[]) => {
+  if (tx.chain !== Currency.ADA) {
+    throw Error('Unsupported chain.');
   }
-
-  const prvKey = CardanoWasm.Bip32PrivateKey.from_128_xprv(
-      Buffer.from(body.privateKey, 'hex')
-    ).to_raw_key();
-
-  const txBuilder = CardanoWasm.TransactionBuilder.new(
-    CardanoWasm.LinearFee.new(
-      CardanoWasm.BigNum.from_str('44'),
-      CardanoWasm.BigNum.from_str('155381'),
-    ),
-    CardanoWasm.BigNum.from_str('1000000'),
-    CardanoWasm.BigNum.from_str('500000000'),
-    CardanoWasm.BigNum.from_str('2000000'),
-  );
-  const { tip : { slotNo } } = await get('/v3/cardano/info');
-  txBuilder.set_ttl(slotNo + 200);
-
-  let total = 0;
-  for (const utxo of utxos) {
-    let amount = parseInt(utxo.value);
-    if (total + amount > body.amount) {
-      amount = body.amount - total;
-    }
-    txBuilder.add_key_input(
-      prvKey.to_public().hash(),
-      CardanoWasm.TransactionInput.new(
-        CardanoWasm.TransactionHash.from_bytes(Buffer.from(utxo.txHash, 'hex')),
-        utxo.index
-      ),
-      CardanoWasm.Value.new(CardanoWasm.BigNum.from_str(String(amount))),
-    )
-    total += amount;
-    if (total === body.amount) break;
+  const transactionBody = TransactionBody.from_bytes(Uint8Array.from(Array.from(tx.serializedTransaction).map(Number)))
+  const txHash = hash_transaction(transactionBody);
+  const vKeyWitnesses = Vkeywitnesses.new();
+  for (const privateKey of privateKeys) {
+    makeWitness(privateKey, txHash, vKeyWitnesses)
   }
-
-  txBuilder.add_output(
-    CardanoWasm.TransactionOutput.new(
-      toAddress,
-      CardanoWasm.Value.new(CardanoWasm.BigNum.from_str(String(body.amount))),
-    ),
-  );
-  const tmpOutput = CardanoWasm.TransactionOutput.new(
-    fromAddress,
-    CardanoWasm.Value.new(CardanoWasm.BigNum.from_str(String('1000000'))),
-  );
-  const fee = parseInt(txBuilder.min_fee().to_str()) + parseInt(txBuilder.fee_for_output(tmpOutput).to_str());
-  txBuilder.add_output(CardanoWasm.TransactionOutput.new(
-    fromAddress,
-    CardanoWasm.Value.new(CardanoWasm.BigNum.from_str(String(fromQuantity - body.amount - fee))),
-  ));
-  txBuilder.set_fee(CardanoWasm.BigNum.from_str(String(fee)));
-
-  const txBody = txBuilder.build();
-  const txHash = CardanoWasm.hash_transaction(txBody);
-
-  const vkeyWitnesses = CardanoWasm.Vkeywitnesses.new();
-  vkeyWitnesses.add(CardanoWasm.make_vkey_witness(txHash, prvKey));
-  const witnesses = CardanoWasm.TransactionWitnessSet.new();
-  witnesses.set_vkeys(vkeyWitnesses);
-
+  const witnesses = TransactionWitnessSet.new();
+  witnesses.set_vkeys(vKeyWitnesses);
   return Buffer.from(
-    CardanoWasm.Transaction.new(txBody, witnesses).to_bytes(),
+    Transaction.new(transactionBody, witnesses).to_bytes(),
   ).toString('hex')
 };
+
+const addOutputs = (transactionBuilder: TransactionBuilder, tos: To[]) => {
+  let amount = new BigNumber(0)
+  for (const to of tos) {
+    const value = new BigNumber(1000000).times(to.value)
+    amount = value.plus(amount)
+    addOutput(transactionBuilder, to.address, value.toString())
+  }
+  return amount
+}
+
+const addInputs = async (transactionBuilder: TransactionBuilder, transferBtcBasedBlockchain: TransferBtcBasedBlockchain) => {
+  const { fromUTXO, fromAddress } = transferBtcBasedBlockchain
+  if (fromAddress) {
+    return addAddressInputs(transactionBuilder, fromAddress)
+  }
+  if (fromUTXO) {
+    return addUtxoInputs(transactionBuilder, fromUTXO)
+  }
+  throw new Error('Field fromAddress or fromUTXO is not filled.')
+}
+
+const addAddressInputs = async (transactionBuilder: TransactionBuilder, fromAddresses: FromAddress[]) => {
+  let amount = new BigNumber(0)
+  const privateKeysToSign: string[] = [];
+  for (const fromAddress of fromAddresses) {
+    const { address, privateKey } = fromAddress
+    const utxos: AdaUtxo[] = await adaGetUtxos(address)
+    for (const utxo of utxos) {
+      amount = amount.plus(utxo.value);
+      addInput(transactionBuilder, privateKey, utxo, address)
+      privateKeysToSign.push(fromAddress.signatureId || fromAddress.privateKey)
+    }
+  }
+  return { amount, privateKeysToSign }
+}
+
+const addUtxoInputs = async (transactionBuilder: TransactionBuilder, fromUTXOs: FromUTXO[]) => {
+  let amount = new BigNumber(0)
+  const privateKeysToSign: string[] = [];
+  for (const utxo of fromUTXOs) {
+    const transaction = await adaGetTransaction(utxo.txHash)
+    const output = transaction.outputs.find(output => output.index === utxo.index)
+    if (output) {
+      const value = output.value;
+      amount = amount.plus(value);
+      addInput(transactionBuilder, utxo.privateKey, { value, ...utxo }, output.address)
+      privateKeysToSign.push(utxo.signatureId || utxo.privateKey)
+    }
+  }
+  return { amount, privateKeysToSign }
+}
+
+const addOutput = (transactionBuilder: TransactionBuilder, address: string, amount: string) => {
+  transactionBuilder.add_output(TransactionOutput.new(
+    Address.from_bech32(address),
+    Value.new(BigNum.from_str(amount)),
+  ));
+}
+
+const addInput = (transactionBuilder: TransactionBuilder, privateKey: string, utxo: AdaUtxo, address: string) => {
+  transactionBuilder.add_input(
+    Address.from_bech32(address),
+    TransactionInput.new(
+      TransactionHash.from_bytes(Buffer.from(utxo.txHash, 'hex')),
+      utxo.index,
+    ),
+    Value.new(BigNum.from_str(utxo.value)),
+  )
+}
+
+const initTransactionBuilder = async () => {
+  const txBuilder = TransactionBuilder.new(
+    LinearFee.new(
+      BigNum.from_str('44'),
+      BigNum.from_str('155381'),
+    ),
+    BigNum.from_str('1000000'),
+    BigNum.from_str('500000000'),
+    BigNum.from_str('2000000'),
+  );
+  const { tip: { slotNo } } = await adaGetBlockChainInfo();
+  txBuilder.set_ttl(slotNo + 200);
+  return txBuilder
+}
+
+const createWitnesses = (transactionBody: TransactionBody, transferBtcBasedBlockchain: TransferBtcBasedBlockchain) => {
+  const { fromAddress, fromUTXO } = transferBtcBasedBlockchain
+  const txHash = hash_transaction(transactionBody);
+  const vKeyWitnesses = Vkeywitnesses.new();
+  if (fromAddress) {
+    for (const address of fromAddress) {
+      makeWitness(address.privateKey, txHash, vKeyWitnesses)
+    }
+  } else if (fromUTXO) {
+    for (const utxo of fromUTXO) {
+      makeWitness(utxo.privateKey, txHash, vKeyWitnesses)
+    }
+  } else {
+    throw new Error('No private key for witness found.')
+  }
+  const witnesses = TransactionWitnessSet.new();
+  witnesses.set_vkeys(vKeyWitnesses);
+  return witnesses
+}
+
+const makeWitness = (privateKey: string, txHash: TransactionHash, vKeyWitnesses: Vkeywitnesses) => {
+  const privateKeyCardano = Bip32PrivateKey.from_128_xprv(
+    Buffer.from(privateKey, 'hex'),
+  ).to_raw_key();
+  vKeyWitnesses.add(make_vkey_witness(txHash, privateKeyCardano));
+}
+
+const processFeeAndRest = async (transactionBuilder: TransactionBuilder, fromAmount: BigNumber, toAmount: BigNumber, transferBtcBasedBlockchain: TransferBtcBasedBlockchain) => {
+  const { fromAddress, fromUTXO } = transferBtcBasedBlockchain
+  if (fromAddress) {
+    addFeeAndRest(transactionBuilder, fromAddress[0].address, fromAmount, toAmount)
+  } else if (fromUTXO) {
+    const txHash = fromUTXO[0].txHash
+    const transaction = await adaGetTransaction(txHash)
+    const output = transaction.outputs.find(output => output.index === fromUTXO[0].index)
+    if (output) {
+      addFeeAndRest(transactionBuilder, output.address, fromAmount, toAmount)
+    }
+  } else {
+    throw new Error('Field fromAddress or fromUTXO is not filled.')
+  }
+}
+
+const addFeeAndRest = (transactionBuilder: TransactionBuilder, address: string, fromAmount: BigNumber, toAmount: BigNumber) => {
+  const fromRest = Address.from_bech32(address);
+  const tmpOutput = TransactionOutput.new(
+    fromRest,
+    Value.new(BigNum.from_str(String('1000000'))),
+  );
+  const fee = parseInt(transactionBuilder.min_fee().to_str()) + parseInt(transactionBuilder.fee_for_output(tmpOutput).to_str());
+  addOutput(transactionBuilder, address, fromAmount.minus(toAmount).minus(fee).toString())
+  transactionBuilder.set_fee(BigNum.from_str(String(fee)));
+}
+
+const signTransaction = (transactionBuilder: TransactionBuilder, transferBtcBasedBlockchain: TransferBtcBasedBlockchain, privateKeysToSign: string[]) => {
+  const txBody = transactionBuilder.build();
+  const { fromAddress, fromUTXO } = transferBtcBasedBlockchain
+
+  if ((fromAddress && fromAddress[0].signatureId) || (fromUTXO && fromUTXO[0].signatureId)) {
+    return JSON.stringify({ txData: JSON.stringify(txBody.to_bytes()), privateKeysToSign });
+  }
+
+  const witnesses = createWitnesses(txBody, transferBtcBasedBlockchain)
+
+  return Buffer.from(
+    Transaction.new(txBody, witnesses).to_bytes(),
+  ).toString('hex')
+}
