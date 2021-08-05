9e3846008f949168cf5c926aee51ac39bddb23c7
Rachit-gaur <rayy.gaur@outlook.com>
2021-08-06 00:48:59 +0530

    adding multitoken get functions

--- src/multiToken/index.ts
+++ src/multiToken/index.ts
@@ -1,3 +1,4 @@
+import { get, post } from '../connector/tatum';
 import {
     CeloBurnMultiToken,
     CeloBurnMultiTokenBatch,
@@ -54,8 +55,36 @@ import {
 } from '../transaction';
 
 /**
- * For more details, see <a href="https://tatum.io/apidoc#operation/NftGetBalanceErc721" target="_blank">Tatum API documentation</a>
+* For more details, see <a href="https://tatum.io/apidoc#operation/MultiTokenGetContractAddress" target="_blank">Tatum API documentation</a>
+*/
+export const getMultiTokenContractAddress = async (chain: Currency, txId: string): Promise<{ contractAddress: string }> =>
+    get(`/v3/multitoken/address/${chain}/${txId}`);
+
+/**
+ * For more details, see <a href="https://tatum.io/apidoc#operation/MultiTokenGetBalanceBatch" target="_blank">Tatum API documentation</a>
  */
+
+export const getMultiTokensBalance = async (chain: Currency, contractAddress: string, address: string, tokenId: string): Promise<string[]> =>
+    get(`/v3/multitoken/balance/${chain}/${contractAddress}/${address}/${tokenId}`);
+
+/**
+* For more details, see <a href="https://tatum.io/apidoc#operation/MultiTokenGetBalance" target="_blank">Tatum API documentation</a>
+*/
+export const getMultiTokensBatchBalance = async (chain: Currency, contractAddress: string, address: string, tokenIds: string): Promise<string[]> =>
+    get(`/v3/multitoken/balance/batch/${chain}/${contractAddress}?address=${address}&tokenId=${tokenIds}`);
+
+
+/**
+* For more details, see <a href="https://tatum.io/apidoc#operation/MultiTokenGetTransaction" target="_blank">Tatum API documentation</a>
+*/
+export const getMultiTokenTransaction = async (chain: Currency, txId: string): Promise<any> =>
+    get(`/v3/multitoken/transaction/${chain}/${txId}`);
+/**
+* For more details, see <a href="https://tatum.io/apidoc#operation/MultiTokenGetMetadata" target="_blank">Tatum API documentation</a>
+*/
+export const getMultiTokenMetadata = async (chain: Currency, contractAddress: string, tokenId: string): Promise<{ data: string }> =>
+    get(`/v3/multitoken/metadata/${chain}/${contractAddress}/${tokenId}`);
+
 
 export const deployMultiToken = async (testnet: boolean, body: CeloDeployMultiToken | EthDeployMultiToken, provider?: string) => {
     switch (body.chain) {
