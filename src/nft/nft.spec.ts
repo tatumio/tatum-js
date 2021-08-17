bb0019920b1316a21906451bc8e300fa4c812dbe
Samuel Sramko <samuel.sramko@tatum.io>
2021-08-17 11:25:32 +0200

    improved tests

--- src/nft/nft.spec.ts
+++ src/nft/nft.spec.ts
@@ -273,6 +273,18 @@ describe('NFT tests', () => {
                 console.log(e);
             }
         });
+        it('should test valid mint 721 transaction on IPFS', async () => {
+            const body: MintErc721 = {
+                to: '0x811dfbff13adfbc3cf653dcc373c03616d3471c9',
+                chain: Currency.BSC,
+                tokenId: `${Date.now()}`,
+                url: '',
+                fromPrivateKey: '0x1a4344e55c562db08700dd32e52e62e7c40b1ef5e27c6ddd969de9891a899b29',
+                contractAddress: '0xC25f71fEaD66A160758265321e4c2Fb93b83BabF',
+            };
+            console.log(await mintNFTWithIPFSMetadata(true, body, readFileSync('/Users/ssramko/Downloads/logo_tatum.png'),
+                'Tatum LOGO', 'description'));
+        });
         it('should test BSC send transaction', async () => {
             const sendErc721Token = await transferNFT(true, {
                 to: '0x1a4344e55c562db08700dd32e52e62e7c40b1ef5e27c6ddd969de9891a899b29',
@@ -294,7 +306,7 @@ describe('NFT tests', () => {
                 chain: Currency.MATIC,
                 fromPrivateKey: '0x1a4344e55c562db08700dd32e52e62e7c40b1ef5e27c6ddd969de9891a899b29',
                 name: 'TatumToken',
-                fee: {gasLimit: '6000000', gasPrice: '100'}
+                fee: {gasLimit: '6000000', gasPrice: '5'}
             }, 'https://rpc-mumbai.matic.today');
             expect(deployErc721Token).not.toBeNull();
             console.log(deployErc721Token);
@@ -318,12 +330,12 @@ describe('NFT tests', () => {
         });
         it('should test valid mint 721 transaction on IPFS', async () => {
             const body: MintErc721 = {
-                to: '0x4b812a77b109A150C2Fc89eD133EaBC78bC9EC8f',
+                to: '0x811dfbff13adfbc3cf653dcc373c03616d3471c9',
                 chain: Currency.MATIC,
                 tokenId: `${Date.now()}`,
                 url: '',
                 fromPrivateKey: '0x1a4344e55c562db08700dd32e52e62e7c40b1ef5e27c6ddd969de9891a899b29',
-                contractAddress: '0xdf82c2f74aa7b629bda65b1cfd258248c9c2b7d3',
+                contractAddress: '0xdb778b39bd7a7c479b3bb1d70df6665fe73e7e1d',
             };
             console.log(await mintNFTWithIPFSMetadata(true, body, readFileSync('/Users/ssramko/Downloads/logo_tatum.png'),
                 'Tatum LOGO', 'description', undefined, 'https://rpc-mumbai.matic.today'));
