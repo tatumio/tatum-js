0f87d0cc19ba642c91cca57c045533fd95b73ed1
Kennedy <kennedyidialu@gmail.com>
2021-08-23 22:30:41 +0100

    Added optional index field

--- src/model/request/TransferEthOffchainKMS.ts
+++ src/model/request/TransferEthOffchainKMS.ts
@@ -1,15 +1,30 @@
-import {IsNotEmpty, IsOptional, IsUUID, Length, MaxLength, ValidateIf,} from 'class-validator'
-import {BaseTransferEthErc20Offchain} from './BaseTransferEthErc20Offchain'
+import {
+	IsInt,
+	IsNotEmpty,
+	IsOptional,
+	IsUUID,
+	Length,
+	Max,
+	MaxLength,
+	Min,
+	ValidateIf,
+} from 'class-validator';
+import { BaseTransferEthErc20Offchain } from './BaseTransferEthErc20Offchain';
 
 export class TransferEthOffchainKMS extends BaseTransferEthErc20Offchain {
+	@ValidateIf((o) => !o.mnemonic && !o.privateKey)
+	@Length(36, 36)
+	@IsUUID('4')
+	@IsNotEmpty()
+	public signatureId?: string;
 
-    @ValidateIf(o => !o.mnemonic && !o.privateKey)
-    @Length(36, 36)
-    @IsUUID('4')
-    @IsNotEmpty()
-    public signatureId?: string;
+	@MaxLength(50000)
+	@IsOptional()
+	public data?: string;
 
-    @MaxLength(50000)
-    @IsOptional()
-    public data?: string;
+	@Min(0)
+	@IsInt()
+	@Max(2147483647)
+	@IsOptional()
+	public index?: number;
 }
