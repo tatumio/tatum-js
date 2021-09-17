67fb9fda5e235e15352d4eaf7981c7edcbc7729f
Samuel Sramko <samuel.sramko@tatum.io>
2021-09-17 15:40:44 +0200

    decrease BSC gasPrice

--- src/transaction/bsc.ts
+++ src/transaction/bsc.ts
@@ -42,15 +42,14 @@ import {
     TransferMultiTokenBatch,
     UpdateCashbackErc721
 } from '../model';
+import {mintNFT} from '../nft';
 import {obtainCustodialAddressType} from '../wallet';
-import { ValidationError } from 'class-validator'
-import { mintNFT } from '../nft'
 
 /**
  * Estimate Gas price for the transaction.
  */
 export const bscGetGasPriceInWei = async () => {
-    return Web3.utils.toWei('20', 'gwei');
+    return Web3.utils.toWei('5', 'gwei');
 };
 
 /**
