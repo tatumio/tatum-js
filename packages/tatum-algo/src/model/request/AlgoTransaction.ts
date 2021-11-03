f005fbc9eb89d53bb064af70181001c5457407b8
Prokop Simek <prokop@dxheroes.io>
2021-11-03 22:26:38 +0100

    chore: correct dependency

--- packages/tatum-algo/src/model/request/AlgoTransaction.ts
+++ packages/tatum-algo/src/model/request/AlgoTransaction.ts
@@ -1,27 +1,26 @@
-import { PrivateKeyOrSignatureId } from "@tatum/tatum-core";
-import { IsNotEmpty, IsNumberString, IsOptional, Length, Matches, MaxLength, } from 'class-validator';
+import { PrivateKeyOrSignatureId } from '@tatumio/tatum-core'
+import { IsNotEmpty, IsNumberString, IsOptional, Length, Matches, MaxLength } from 'class-validator'
 
 export class AlgoTransaction extends PrivateKeyOrSignatureId {
+  @IsNotEmpty()
+  @Length(58, 58)
+  public from: string
 
-    @IsNotEmpty()
-    @Length(58, 58)
-    public from: string;
+  @IsNotEmpty()
+  @Length(58, 58)
+  public to: string
 
-    @IsNotEmpty()
-    @Length(58, 58)
-    public to: string;
+  @IsNotEmpty()
+  @IsNumberString()
+  @Matches(/^[+]?((\d+(\.\d*)?)|(\.\d+))$/)
+  public fee: string
 
-    @IsNotEmpty()
-    @IsNumberString()
-    @Matches(/^[+]?((\d+(\.\d*)?)|(\.\d+))$/)
-    public fee: string;
+  @IsNotEmpty()
+  @IsNumberString()
+  @Matches(/^[+]?((\d+(\.\d*)?)|(\.\d+))$/)
+  public amount: string
 
-    @IsNotEmpty()
-    @IsNumberString()
-    @Matches(/^[+]?((\d+(\.\d*)?)|(\.\d+))$/)
-    public amount: string;
-
-    @IsOptional()
-    @MaxLength(30)
-    public note: string;
+  @IsOptional()
+  @MaxLength(30)
+  public note: string
 }
