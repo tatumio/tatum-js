ab0b29a4514d42d472ae6b0f7f0254dae3234d58
jay_sun <jay.sun@tatum.io>
2021-11-05 21:18:31 +0800

    updated but with build fail generateWallet

--- packages/tatum-algo/src/ledger/account.ts
+++ packages/tatum-algo/src/ledger/account.ts
@@ -1,19 +1,11 @@
 import { 
-    CreateAccount,
-    createAccount, 
-    generateDepositAddress
-} from '@tatumio/tatum-core'  
-import { generateAlgoWallet } from '../wallet'
+    CreateAccount, 
+    generateAccount as generateAccountCore
+  } from '@tatumio/tatum-core'  
+import { generateWallet } from '../wallet'
 
 export const generateAccount = async (account: CreateAccount, generateNewWallet = true, testnet = true, webhookUrl?: string) => {
-    let w;
-    if (generateNewWallet) {
-        w = await generateAlgoWallet();
-        account.xpub = w.address;
-    }
-    const a = await createAccount(account)
-    const address = await generateDepositAddress(a.id)
-    return { account: a, address, wallet: w }
+    return generateAccountCore(account, generateWallet, generateNewWallet, testnet, webhookUrl)
 }
   
 export {
