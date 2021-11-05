947dd641982b80fd07fb0f1084b5f9a3b5577dd1
Rachit-gaur <rayy.gaur@outlook.com>
2021-11-05 15:45:36 +0530

    fixing btc submodule imports

--- packages/tatum-btc/src/model/request/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './PrivateKeyOrSignatureIdBtcBased'
-export * from './TransferBtcBasedBlockchain'
-export * from './TransferBtcBasedOffchain'
-export * from './TransferBtcBasedOffchainKMS'
