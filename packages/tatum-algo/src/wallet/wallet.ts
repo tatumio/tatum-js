5530ae658dd0743549ee8110f1fe47b7a1954237
Prokop Simek <prokop@dxheroes.io>
2021-11-05 15:28:08 +0100

    chore: wallet with adress for algo

--- packages/tatum-algo/src/wallet/wallet.ts
+++ packages/tatum-algo/src/wallet/wallet.ts
@@ -1,19 +1,19 @@
 const algosdk = require('algosdk')
 const base32 = require('base32.js')
-import {Currency} from '@tatumio/tatum-core'
+import { Currency, WalletWithAddress } from '@tatumio/tatum-core'
 /**
  * Generate Algo wallet
  * @param mnem mnemonic seed to use
  * @returns address and secret
  */
- export const generateAlgoWallet = async (mnem?: string) => {
-    const account = mnem ? algosdk.mnemonicToSecretKey(mnem) : algosdk.generateAccount();
-    const encoder = new base32.Encoder({type: "rfc4648"});
-    const secret = encoder.write(account.sk).finalize();
-    return {
-        address: account.addr,
-        secret: secret,
-    }
+export const generateAlgoWallet = async (mnem?: string): Promise<WalletWithAddress> => {
+  const account = mnem ? algosdk.mnemonicToSecretKey(mnem) : algosdk.generateAccount()
+  const encoder = new base32.Encoder({ type: 'rfc4648' })
+  const secret = encoder.write(account.sk).finalize()
+  return {
+    address: account.addr,
+    privateKey: secret,
+  }
 }
 
 /**
@@ -23,6 +23,6 @@ import {Currency} from '@tatumio/tatum-core'
  * @param mnemonic mnemonic seed to use. If not present, new one will be generated
  * @returns wallet or a combination of address and private key
  */
- export const generateWallet = (currency: Currency, testnet: boolean, mnemonic?: string) => {
-    return generateAlgoWallet(mnemonic)
+export const generateWallet = (currency: Currency, testnet: boolean, mnemonic?: string) => {
+  return generateAlgoWallet(mnemonic)
 }
