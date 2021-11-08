02d69cb74c52cfccf4cfa69eaaf0137c76261cf2
Prokop Simek <prokop@dxheroes.io>
2021-11-08 09:30:28 +0100

    chore: revert back missing ledger folder in bsc

---
+++ packages/tatum-bsc/src/ledger/account.ts
@@ -0,0 +1,33 @@
+import { CreateAccount, generateAccount as generateAccountCore } from '@tatumio/tatum-core'
+import { generateWallet } from '../wallet'
+
+/**
+ * Abstraction unification endpoint for creating new ledger account, optionally added wallet generation, generating deposit blockchain address
+ * and register incoming TX webhook notification.
+ * @param account Account to be created.
+ * @param generateNewWallet If new wallet should be created as well
+ * @param testnet if we are using testnet or not
+ * @param webhookUrl optional URL, where webhook will be post for every incoming blockchain transaction to the address
+ */
+export const generateAccount = async (account: CreateAccount, generateNewWallet = true, testnet = true, webhookUrl?: string) => {
+  return generateAccountCore(account, generateWallet, generateNewWallet, testnet, webhookUrl)
+}
+
+export {
+  getAccountById,
+  createAccount,
+  updateAccount,
+  createAccounts,
+  getBlockedAmountsByAccountId,
+  blockAmount,
+  deleteBlockedAmount,
+  deleteBlockedAmountWithTransaction,
+  deleteBlockedAmountForAccount,
+  activateAccount,
+  deactivateAccount,
+  freezeAccount,
+  unfreezeAccount,
+  getAccountsByCustomerId,
+  getAllAccounts,
+  getAccountBalance,
+} from '@tatumio/tatum-core'
