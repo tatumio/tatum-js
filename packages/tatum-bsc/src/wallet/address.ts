44b8771b7631851a3212754f7da84836a76cb863
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2021-11-11 22:03:24 +0100

    bsc - remove unused in package - generatePrivateKeyFromMnemonic

--- packages/tatum-bsc/src/wallet/address.ts
+++ packages/tatum-bsc/src/wallet/address.ts
@@ -50,13 +50,12 @@ export const generateAddressFromXPub = (xpub: string, i: number) => {
 
 /**
  * Generate private key from mnemonic seed
- * @param currency type of blockchain
  * @param testnet testnet or mainnet version of address
  * @param mnemonic mnemonic to generate private key from
  * @param i derivation index of private key to generate.
  * @returns blockchain private key to the address
  */
-export const generatePrivateKeyFromMnemonic = (currency: Currency, testnet: boolean, mnemonic: string, i: number) => {
+export const generatePrivateKeyFromMnemonic = (testnet: boolean, mnemonic: string, i: number) => {
   return generateBscPrivateKey(testnet, mnemonic, i)
 }
 
