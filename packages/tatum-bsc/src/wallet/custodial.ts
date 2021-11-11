4fc7a3d04f67d97a2be099d57f6989109c547ac7
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2021-11-11 22:08:44 +0100

    bsc - remove unused in package - sendCustodialWallet

--- packages/tatum-bsc/src/wallet/custodial.ts
+++ packages/tatum-bsc/src/wallet/custodial.ts
@@ -46,12 +46,11 @@ export const prepareCustodialWallet = async (body: GenerateCustodialAddress, pro
 /**
  * Generate new smart contract based custodial wallet. This wallet is able to receive any type of assets, btu transaction costs connected to the withdrawal
  * of assets is covered by the deployer.
- * @param testnet chain to work with
  * @param body request data
  * @param provider optional provider to enter. if not present, Tatum Web3 will be used.
  * @returns {txId: string} Transaction ID of the operation, or signatureID in case of Tatum KMS
  */
-export const sendCustodialWallet = async (testnet: boolean, body: GenerateCustodialAddress, provider?: string) => {
+export const sendCustodialWallet = async (body: GenerateCustodialAddress, provider?: string) => {
   const txData = await prepareBscGenerateCustodialWalletSignedTransaction(body, provider)
   return helperBroadcastTx(txData, body.signatureId)
 }
