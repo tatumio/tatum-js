4760c23eff3a14b10b3111a99e30477c612da07e
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2021-11-11 14:44:17 +0100

    algo - remove unused in package

--- packages/tatum-algo/src/helpers/index.ts
+++ packages/tatum-algo/src/helpers/index.ts
@@ -1,6 +1,5 @@
 import { algorandBroadcast } from '../blockchain'
-import { Currency } from '@tatumio/tatum-core'
 
-export const helperBroadcastTx = async (chain: Currency, txData: string, signatureId?: string) => {
+export const helperBroadcastTx = async (txData: string, signatureId?: string) => {
   return await algorandBroadcast(txData, signatureId)
 }
