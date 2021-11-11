9d6e54e4b13e183da65a73528f183f65b226c5a9
martinrucki <marthy94@gmail.com>
2021-11-11 15:40:56 +0100

    chore: chain submodules export wallet from ledger

--- packages/tatum-quorum/src/index.ts
+++ packages/tatum-quorum/src/index.ts
@@ -10,3 +10,4 @@ export * from '@tatumio/tatum-core/src/storage'
 export * from '@tatumio/tatum-core/src/tatum'
 
 export * from '@tatumio/tatum-defi/src/wallet'
+export * from '@tatumio/tatum-ledger/src/wallet'
