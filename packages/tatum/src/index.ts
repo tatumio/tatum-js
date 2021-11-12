81bd6a92451f93ade8acba89e379b485606394ce
martinrucki <marthy94@gmail.com>
2021-11-12 14:03:13 +0100

    chore: remove redundant ledger fn and imports

--- packages/tatum/src/index.ts
+++ packages/tatum/src/index.ts
@@ -1,19 +1,12 @@
 import 'reflect-metadata'
 
 export * from './wallet'
-// export * from './model';
 export * from './ledger'
-// export * from './tatum';
-// export * from './security';
-// export * from './storage';
 export * from './offchain'
 export * from './fungible'
-// export * from './blockchain';
-// export * from './transaction';
-// export * from './record'
 export * from './nft'
 export * from './multiToken'
 
-// export * from '@tatumio/tatum-core'
-// export * from '@tatumio/tatum-defi'
-// export * from '@tatumio/tatum-ledger'
+export * from '@tatumio/tatum-core'
+export * from '@tatumio/tatum-defi'
+export * from '@tatumio/tatum-ledger'
