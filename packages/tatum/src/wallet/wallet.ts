dd44518bbd5768cd327a25f1049952f758353c75
martinrucki <marthy94@gmail.com>
2021-11-14 22:20:10 +0100

    chore: use scrypta generateWallet

--- packages/tatum/src/wallet/wallet.ts
+++ packages/tatum/src/wallet/wallet.ts
@@ -17,7 +17,7 @@ import { generateWallet as generateXrpWallet } from '@tatumio/tatum-xrp'
 import { generateWallet as generateXlmWallet } from '@tatumio/tatum-xlm'
 import { generateWallet as generateVetWallet } from '@tatumio/tatum-vet'
 // import { generateWallet as generateNeoWallet } from '@tatumio/tatum-neo'
-// import { generateWallet as generateLyraWallet } from '@tatumio/tatum-lyra'
+import { generateWallet as generateLyraWallet } from '@tatumio/tatum-scrypta'
 import { generateWallet as generateBnbWallet } from '@tatumio/tatum-bnb'
 import { generateWallet as generateAdaWallet } from '@tatumio/tatum-ada'
 import { generateWallet as generateAlgoWallet } from '@tatumio/tatum-algo'
@@ -113,8 +113,8 @@ export const generateWallet = (currency: Currency, testnet: boolean, mnemonic?: 
     //   return generateNeoWallet()
     case Currency.BNB:
       return generateBnbWallet(testnet)
-    // case Currency.LYRA:
-    //   return generateLyraWallet(testnet, mnem)
+    case Currency.LYRA:
+      return generateLyraWallet(testnet, mnem)
     case Currency.ADA:
       return generateAdaWallet(mnem)
     case Currency.ALGO:
