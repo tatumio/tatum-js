f2d4377c5593fd26b8ed1e258c42147f092eb004
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2021-11-15 12:56:34 +0100

    flow - remove unused in package - generateAddressFromXPub

--- packages/tatum-flow/src/wallet/address.spec.ts
+++ packages/tatum-flow/src/wallet/address.spec.ts
@@ -4,7 +4,7 @@ import {generateAddressFromXPub, generatePrivateKeyFromMnemonic} from './address
 describe('Address tests', () => {
 
     it('should generate public key 1 for FLOW mainnet', () => {
-        const address = generateAddressFromXPub(Currency.FLOW, false, 'xpub6EVKqCYcoa9DXpjAACsdyQTUZ5tgx3DUyt5Yy8xx9kmVKMCsn3vtLictDQMjdEtpo5CpwVwipVxThFKwh49xNJ5Fy752ifnM5mwYy28AtVv', 1)
+        const address = generateAddressFromXPub('xpub6EVKqCYcoa9DXpjAACsdyQTUZ5tgx3DUyt5Yy8xx9kmVKMCsn3vtLictDQMjdEtpo5CpwVwipVxThFKwh49xNJ5Fy752ifnM5mwYy28AtVv', 1)
         expect(address).toBe('968c3ce11e871cb2b7161b282655ee5fcb051f3c04894705d771bf11c6fbebfc6556ab8a0c04f45ea56281312336d0668529077c9d66891a6cad3db877acbe90')
     })
 
