3f8dda08850f289780bb521efe41fb9127cbd44f
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2021-11-15 17:16:15 +0100

    polygon - remove unused in package - preparePolygonBurnErc20SignedTransaction

--- packages/tatum-polygon/src/transaction/polygon.ts
+++ packages/tatum-polygon/src/transaction/polygon.ts
@@ -256,12 +256,11 @@ export const preparePolygonMintErc20SignedTransaction = async (body: MintErc20, 
 
 /**
  * Sign Polygon burn erc20 transaction with private keys locally. Nothing is broadcast to the blockchain.
- * @param testnet mainnet or testnet version
  * @param body content of the transaction to broadcast
  * @param provider url of the Polygon Server to connect to. If not set, default public server will be used.
  * @returns transaction data to be broadcast to blockchain.
  */
-export const preparePolygonBurnErc20SignedTransaction = async (testnet: boolean, body: BurnErc20, provider?: string) => {
+export const preparePolygonBurnErc20SignedTransaction = async (body: BurnErc20, provider?: string) => {
   await validateBody(body, BurnErc20)
   const client = await preparePolygonClient(provider, body.fromPrivateKey)
   // @ts-ignore
@@ -929,13 +928,12 @@ export const sendPolygonMintErc20SignedTransaction = async (body: MintErc20, pro
 /**
  * Send Polygon burn erc20 transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
  * This operation is irreversible.
- * @param testnet
  * @param body content of the transaction to broadcast
  * @param provider url of the Harmony Server to connect to. If not set, default public server will be used.
  * @returns transaction id of the transaction in the blockchain
  */
-export const sendPolygonBurnErc20SignedTransaction = async (testnet: boolean, body: BurnErc20, provider?: string) =>
-  polygonBroadcast(await preparePolygonBurnErc20SignedTransaction(testnet, body, provider), body.signatureId)
+export const sendPolygonBurnErc20SignedTransaction = async (body: BurnErc20, provider?: string) =>
+  polygonBroadcast(await preparePolygonBurnErc20SignedTransaction(body, provider), body.signatureId)
 
 /**
  * Send Polygon transfer erc20 transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
