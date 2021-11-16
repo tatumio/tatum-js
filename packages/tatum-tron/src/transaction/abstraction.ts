a6c60579acd0e0d6e3e96a7be33042b65170b56c
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2021-11-16 16:15:19 +0100

    tron - remove unused in package - sendTransaction

--- packages/tatum-tron/src/transaction/abstraction.ts
+++ packages/tatum-tron/src/transaction/abstraction.ts
@@ -1,15 +1,11 @@
-import {Currency} from '@tatumio/tatum-core';
 import {sendTronTransaction} from './tron';
-import {TransferTron} from "src/model";
+import {TransferTron} from 'src/model';
 
 /**
  * Perform any native asset transaction.
  * @param testnet if we are on testnet or not
- * @param chain Blockchain to work with. ETH,CELO,MATIC,ONE,TRON,BSC supported now.
  * @param body Body of the transaction.
- * @param provider Optional provider to use for broadcasting signed tx to the blockchain.
  */
-export const sendTransaction = async (testnet: boolean, chain: Currency,
-                                      body: TransferTron, provider?: string) => {
+export const sendTransaction = async (testnet: boolean, body: TransferTron) => {
     return sendTronTransaction(testnet, body as TransferTron);
 };
