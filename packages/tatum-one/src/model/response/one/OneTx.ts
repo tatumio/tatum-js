10375e8b840e7e759dcc4fe65422c7c5e7a4662a
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2021-11-30 18:51:26 +0100

    Export getNFTTransaction from tatum-one

--- packages/tatum-one/src/model/response/one/OneTx.ts
+++ packages/tatum-one/src/model/response/one/OneTx.ts
@@ -1,42 +1,15 @@
+import { EthTx as EthLikeTx } from '@tatumio/tatum-core'
+
 /**
  *
  * @export
  * @interface OneTx
  */
-export interface OneTx {
-  blockHash: string
-  blockNumber: number
-  ethHash: string
-  from: string
-  gas: number
-  gasPrice: number
+export interface OneTx extends EthLikeTx {
   hash: string
-  input: string
-  nonce: number
   shardID: number
   timestamp: number
-  to: string
   toShardID: number
-  transactionIndex: number
-  value: number
-  contractAddress: string
-  cumulativeGasUsed: number
-  gasUsed: number
-  logs: [
-    {
-      address: string
-      blockHash: string
-      blockNumber: number
-      data: string
-      logIndex: number
-      removed: false
-      topics: string[]
-      transactionHash: string
-      transactionIndex: number
-    }
-  ]
   logsBloom: string
   root: string
-  status: boolean
-  transactionHash: string
 }
