dbecdcec7ef88ce6ff1411c4f0c8f12e639683cb
martinrucki <marthy94@gmail.com>
2021-12-03 11:21:37 +0100

    chore: remove chain arg from BNB submodule interfaces

--- packages/tatum-bnb/src/index.ts
+++ packages/tatum-bnb/src/index.ts
@@ -1,8 +1,28 @@
 export * from './blockchain'
 export * from './offchain'
+export * from './record'
+export * from './tatum'
 export * from './transaction'
 export * from './wallet'
 
-export * from '@tatumio/tatum-core'
-export * from '@tatumio/tatum-defi'
+export {
+  checkMaliciousAddress,
+  generateDepositAddress,
+  generateDepositAddresses,
+  assignDepositAddress,
+  removeDepositAddress,
+  getDepositAddressesForAccount,
+  offchainBroadcast,
+  offchainStoreWithdrawal,
+  offchainCancelWithdrawal,
+  offchainCompleteWithdrawal,
+  ipfsDelete,
+  ipfsGet,
+  ipfsUpload,
+  getUsage,
+  getTransactionKMS,
+  deleteTransactionKMS,
+  completePendingTransactionKMS,
+} from '@tatumio/tatum-core'
+
 export { WalletWithAddress } from '@tatumio/tatum-ledger'
