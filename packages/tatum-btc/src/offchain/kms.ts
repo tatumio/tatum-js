b9ca52198c21342d4174bb6a284e399a9a5d606e
martinrucki <marthy94@gmail.com>
2021-12-03 11:33:36 +0100

    chore: remove chain arg from BTC submodule interfaces

--- packages/tatum-btc/src/offchain/kms.ts
+++ packages/tatum-btc/src/offchain/kms.ts
@@ -1,7 +1,16 @@
-import { post, SignatureId } from '@tatumio/tatum-core'
-import { TransferBtcBasedOffchainKMS } from '@tatumio/tatum-core'
+import {
+  TransferBtcBasedOffchainKMS,
+  Currency,
+  getPendingTransactionsKMSByChain as getPendingTransactionsKMSByChainCore,
+  post,
+  SignatureId,
+} from '@tatumio/tatum-core'
 /**
  * For more details, see <a href="https://tatum.io/apidoc#operation/BtcTransfer" target="_blank">Tatum API documentation</a>
  */
 export const offchainTransferBtcKMS = async (body: TransferBtcBasedOffchainKMS): Promise<SignatureId> =>
   post(`/v3/offchain/bitcoin/transfer`, body, TransferBtcBasedOffchainKMS)
+
+export const getPendingTransactionsKMSByChain = () => {
+  return getPendingTransactionsKMSByChainCore(Currency.BTC)
+}
