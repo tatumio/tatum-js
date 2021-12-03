f18b4166bf733159ab55068b7379604091e28339
martinrucki <marthy94@gmail.com>
2021-12-03 15:29:04 +0100

    chore: remove chain arg from DOGE submodule interfaces

--- packages/tatum-doge/src/index.ts
+++ packages/tatum-doge/src/index.ts
@@ -5,9 +5,29 @@ export * from './model'
 export * from './ledger'
 export * from './offchain'
 export * from './blockchain'
+export * from './record'
+export * from './tatum'
 export * from './transaction'
 export * from './constants'
 
-export * from '@tatumio/tatum-core'
-export * from '@tatumio/tatum-defi'
+export {
+  checkMaliciousAddress,
+  generateDepositAddress,
+  generateDepositAddresses,
+  assignDepositAddress,
+  removeDepositAddress,
+  getDepositAddressesForAccount,
+  offchainBroadcast,
+  offchainStoreWithdrawal,
+  offchainCancelWithdrawal,
+  offchainCompleteWithdrawal,
+  ipfsDelete,
+  ipfsGet,
+  ipfsUpload,
+  getUsage,
+  getTransactionKMS,
+  deleteTransactionKMS,
+  completePendingTransactionKMS,
+} from '@tatumio/tatum-core'
+
 export { WalletWithMnemonic } from '@tatumio/tatum-ledger'
--- packages/tatum-doge/src/offchain/kms.ts
+++ packages/tatum-doge/src/offchain/kms.ts
@@ -1,7 +1,17 @@
-import { post, SignatureId, TransferBtcBasedOffchainKMS } from '@tatumio/tatum-core'
+import {
+  Currency,
+  getPendingTransactionsKMSByChain as getPendingTransactionsKMSByChainCore,
+  post,
+  SignatureId,
+  TransferBtcBasedOffchainKMS,
+} from '@tatumio/tatum-core'
 
 /**
  * For more details, see <a href="https://tatum.io/apidoc#operation/DogeTransfer" target="_blank">Tatum API documentation</a>
  */
 export const offchainTransferDogeKMS = async (body: TransferBtcBasedOffchainKMS): Promise<SignatureId> =>
   post(`/v3/offchain/dogecoin/transfer`, body, TransferBtcBasedOffchainKMS)
+
+export const getPendingTransactionsKMSByChain = () => {
+  return getPendingTransactionsKMSByChainCore(Currency.DOGE)
+}
