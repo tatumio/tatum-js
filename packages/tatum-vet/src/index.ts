36c3221e510a6a49d0abb93bd5a9822b086ce4b7
martinrucki <marthy94@gmail.com>
2021-12-06 11:35:10 +0100

    chore: remove chain arg VET submodule interfaces

--- packages/tatum-vet/src/index.ts
+++ packages/tatum-vet/src/index.ts
@@ -1,9 +1,29 @@
 export * from './blockchain'
 export * from './model'
 export * from './offchain'
+export * from './record'
+export * from './tatum'
 export * from './transaction'
 export * from './wallet'
 
-export * from '@tatumio/tatum-core'
-export * from '@tatumio/tatum-defi'
+export {
+  checkMaliciousAddress,
+  generateDepositAddress,
+  generateDepositAddresses,
+  assignDepositAddress,
+  removeDepositAddress,
+  getDepositAddressesForAccount,
+  offchainBroadcast,
+  offchainStoreWithdrawal,
+  offchainCancelWithdrawal,
+  offchainCompleteWithdrawal,
+  ipfsDelete,
+  ipfsGet,
+  ipfsUpload,
+  getUsage,
+  getTransactionKMS,
+  deleteTransactionKMS,
+  completePendingTransactionKMS,
+} from '@tatumio/tatum-core'
+
 export { WalletWithMnemonic } from '@tatumio/tatum-ledger'
