702ae596c311b355022beb37642e642bde2cd482
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-01-19 13:25:46 +0100

    feat(celo): bootstrap sdk lib

---
+++ packages/blockchain/celo/src/lib/blockchain-celo.spec.ts
@@ -0,0 +1,7 @@
+import { blockchainCelo } from './blockchain-celo'
+
+describe('blockchainCelo', () => {
+  it('should work', () => {
+    expect(blockchainCelo()).toEqual('blockchain-celo')
+  })
+})
