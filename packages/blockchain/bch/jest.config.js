7be16573b09304b8bfa847bc8db2031c7ced63c1
jakcinmarina <marina.jakcin@applifting.cz>
2022-01-24 16:43:07 +0100

    Config tweaks

--- packages/blockchain/bch/jest.config.js
+++ packages/blockchain/bch/jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/packages/blockchain/btc',
+  coverageDirectory: '../../../coverage/packages/blockchain/bch',
 }
