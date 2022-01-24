10618e9cc3ff960ee62da8843deffcb74822bd54
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-01-24 10:30:26 +0100

    feat(kcs): tests

--- packages/shared/testing/src/lib/shared-testing.ts
+++ packages/shared/testing/src/lib/shared-testing.ts
@@ -3,6 +3,7 @@ import { BTC_TEST_DATA } from './test-data/btc.test-data'
 import { DOGE_TEST_DATA } from './test-data/doge.test-data'
 import { CELO_TEST_DATA } from './test-data/celo.test-data'
 import { POLYGON_TEST_DATA } from './test-data/polygon.test-data'
+import { KCS_TEST_DATA } from './test-data/kcs.test-data'
 
 export type BlockchainNetworkTestData = {
   XPUB: string
@@ -30,6 +31,7 @@ export const TEST_DATA = {
   DOGE: DOGE_TEST_DATA,
   CELO: CELO_TEST_DATA,
   POLYGON: POLYGON_TEST_DATA,
+  KCS: KCS_TEST_DATA,
 }
 
 /**
