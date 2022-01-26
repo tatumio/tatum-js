df4e8222a89ab67a46c4fcb943c71186ac24d3a4
Nick Papirniy <nikolaspapirniywork@gmail.com>
2022-01-26 09:37:14 +0100

    ci: sonarcloud config file (#324)

--- packages/sdk/src/lib/services/sdk.kms.ts
+++ packages/sdk/src/lib/services/sdk.kms.ts
@@ -1,6 +1,5 @@
 import { SecurityKeyManagementSystemService } from '@tatumio/api-client'
 import { abstractSdkKms } from '@tatumio/abstract-sdk'
-import { Blockchain, blockchainHelper, ChainTransactionKMS, Currency } from '@tatumio/shared-core'
 import { SDKS } from '../sdk.common'
 
 export const sdkKms = (args: { sdks: SDKS }) => {
