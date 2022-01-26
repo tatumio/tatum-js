2a178786070ae097798765576883e959e4dc6e5b
jakcinmarina <marina.jakcin@applifting.cz>
2022-01-26 15:57:41 +0100

    Add missing examples - kcs

---
+++ examples/kcs-example/src/app/kcs.nft.example.ts
@@ -0,0 +1,40 @@
+import { TatumKcsSDK } from '@tatumio/kcs'
+import { Currency } from '@tatumio/shared-core';
+import { REPLACE_ME_WITH_TATUM_API_KEY } from '@tatumio/shared-testing'
+
+const kcsSDK = TatumKcsSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
+
+export async function kcsNftExample() {
+  const metadataURI = await kcsSDK.nft.getNFTMetadataURI(Currency.KCS, "0x94Ce79B9F001E25BBEbE7C01998A78F7B27D1326", "1")
+  const provenanceData = await kcsSDK.nft.getNFTProvenanceData(Currency.KCS, "0x94Ce79B9F001E25BBEbE7C01998A78F7B27D1326", "1")
+  const royalty = await kcsSDK.nft.getNFTRoyalty(Currency.KCS, "0x94Ce79B9F001E25BBEbE7C01998A78F7B27D1326", "1")
+  const transaction = await kcsSDK.nft.getNFTTransaction(Currency.KCS, "0xe6e7340394958674cdf8606936d292f565e4ecc476aaa8b258ec8a141f7c75d7")
+  const nftByAddress = await kcsSDK.nft.getNFTsByAddress(Currency.KCS, "NTAESFCB3WOD7SAOL42KSPVARLB3JFA3MNX3AESWHYVT2RMYDVZI6YLG4Y", "0x45871ED5F15203C0ce791eFE5f4B5044833aE10e")
+  
+  // TODO bug in openapi - chain and feeCurrency
+  // const minted = await kcsSDK.nft.mintNFT({
+  //   chain: Currency.KCS,
+  //   tokenId: "100000",
+  //   to: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  //   erc20: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  //   contractAddress: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  //   url: "https://my_token_data.com",
+  //   authorAddresses: [
+  //     "0x687422eEA2cB73B5d3e242bA5456b782919AFc85"
+  //   ],
+  //   provenance: true,
+  //   cashbackValues: [
+  //     "0.5"
+  //   ],
+  //   fixedValues: [
+  //     "0.5"
+  //   ],
+  //   fromPrivateKey: "0x05e150c73f1920ec14caa1e0b6aa09940899678051a78542840c2668ce5080c2",
+  //   nonce: 0,
+  //   feeCurrency: Currency.KCS
+  // })
+ 
+  await kcsSDK.nft.prepareAddNFTMinterAbstraction(Currency.KCS, "0x687422eEA2cB73B5d3e242bA5456b782919AFc85", "1000")
+}
+
+
---
+++ examples/kcs-example/src/app/kcs.offchain.example.ts
@@ -0,0 +1,26 @@
+import { TatumKcsSDK } from '@tatumio/kcs'
+import { REPLACE_ME_WITH_TATUM_API_KEY } from '@tatumio/shared-testing'
+
+const kcsSdk = TatumKcsSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
+
+export async function kcsOffchainExample() {
+  const account = await kcsSdk.offchain.depositAddress.checkExists('0xa7673161CbfE0116A4De9E341f8465940c2211d4')
+  const address = await kcsSdk.offchain.depositAddress.create('0xa7673161CbfE0116A4De9E341f8465940c2211d4', 1);
+  const adresses = await kcsSdk.offchain.depositAddress.createMultiple({
+    addresses: [
+      {
+        accountId: "5e6be8e9e6aa436299950c41",
+        derivationKey: 0
+      },
+      {
+        accountId: "5e6be8e9e6aa436299951n35",
+        derivationKey: 1
+      }
+    ]
+  });
+  const assignedAddress = await kcsSdk.offchain.depositAddress.assign('5e6be8e9e6aa436299950c41', '0xa7673161CbfE0116A4De9E341f8465940c2211d4');
+  const addressByAccount = await kcsSdk.offchain.depositAddress.getByAccount('5e6be8e9e6aa436299950c41');
+  await kcsSdk.offchain.depositAddress.remove('5e6be8e9e6aa436299950c41', '0xa7673161CbfE0116A4De9E341f8465940c2211d4');
+  await kcsSdk.offchain.storeTokenAddress('0xa7673161CbfE0116A4De9E341f8465940c2211d4', 'MY_TOKEN');
+  const withdrawals = await kcsSdk.offchain.withdrawal.getAll('Done')
+}
