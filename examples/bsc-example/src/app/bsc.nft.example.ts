64d68cb23f41a5e49fe76be20600ceb79c759830
jakcinmarina <marina.jakcin@applifting.cz>
2022-01-26 15:20:57 +0100

    Add missing examples - bsc

---
+++ examples/bsc-example/src/app/bsc.nft.example.ts
@@ -0,0 +1,40 @@
+import { TatumBscSDK } from '@tatumio/bsc'
+import { Currency } from '@tatumio/shared-core';
+import { REPLACE_ME_WITH_TATUM_API_KEY } from '@tatumio/shared-testing'
+
+const bscSDK = TatumBscSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
+
+export async function bscNftExample() {
+  const metadataURI = await bscSDK.nft.getNFTMetadataURI(Currency.BSC, "0x94Ce79B9F001E25BBEbE7C01998A78F7B27D1326", "1")
+  const provenanceData = await bscSDK.nft.getNFTProvenanceData(Currency.BSC, "0x94Ce79B9F001E25BBEbE7C01998A78F7B27D1326", "1")
+  const royalty = await bscSDK.nft.getNFTRoyalty(Currency.BSC, "0x94Ce79B9F001E25BBEbE7C01998A78F7B27D1326", "1")
+  const transaction = await bscSDK.nft.getNFTTransaction(Currency.BSC, "0xe6e7340394958674cdf8606936d292f565e4ecc476aaa8b258ec8a141f7c75d7")
+  const nftByAddress = await bscSDK.nft.getNFTsByAddress(Currency.BSC, "NTAESFCB3WOD7SAOL42KSPVARLB3JFA3MNX3AESWHYVT2RMYDVZI6YLG4Y", "0x45871ED5F15203C0ce791eFE5f4B5044833aE10e")
+  
+  // TODO bug in openapi - chain and feeCurrency
+  // const minted = await bscSDK.nft.mintNFT({
+  //   chain: Currency.BSC,
+  //   tokenId: "100000",
+  //   to: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  //   erc20: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  //   contractAddress: "0x687422eEA2cB73B5d3e242bA5456b782919AFc85",
+  //   url: "https://my_token_data.com",
+  //   authorAddresses: [
+  //     "0x687422eEA2cB73B5d3e242bA5456b782919AFc85"
+  //   ],
+  //   provenance: true,
+  //   cashbackValues: [
+  //     "0.5"
+  //   ],
+  //   fixedValues: [
+  //     "0.5"
+  //   ],
+  //   fromPrivateKey: "0x05e150c73f1920ec14caa1e0b6aa09940899678051a78542840c2668ce5080c2",
+  //   nonce: 0,
+  //   feeCurrency: Currency.BSC
+  // })
+ 
+  await bscSDK.nft.prepareAddNFTMinterAbstraction(Currency.BSC, "0x687422eEA2cB73B5d3e242bA5456b782919AFc85", "1000")
+}
+
+
---
+++ examples/bsc-example/src/app/bsc.subscriptions.example.ts
@@ -0,0 +1,24 @@
+import { CreateSubscriptionIncoming } from '@tatumio/api-client';
+import { TatumBscSDK } from '@tatumio/bsc'
+import { REPLACE_ME_WITH_TATUM_API_KEY } from '@tatumio/shared-testing'
+
+const bscSDK = TatumBscSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
+
+export async function bscSubscriptionsExample() {
+  const id = await bscSDK.subscriptions.createSubscription({
+    // TODO openapi bug
+    type: CreateSubscriptionIncoming.type.ACCOUNT_INCOMING_BLOCKCHAIN_TRANSACTION,
+    attr: {
+      id: "5e6be8e9e6aa436299950c41",
+      url: "https://webhook.tatum.io/account"
+    }
+  })
+  await bscSDK.subscriptions.deleteSubscription("5e68c66581f2ee32bc354087")
+  await bscSDK.subscriptions.disableWebHookHmac()
+  await bscSDK.subscriptions.enableWebHookHmac({
+    hmacSecret: "1f7f7c0c-3906-4aa1-9dfe-4b67c43918f6"
+  })
+
+  const subscriptionReport = await bscSDK.subscriptions.getSubscriptionReport("5e68c66581f2ee32bc354087")
+  const subscriptions = await bscSDK.subscriptions.getSubscriptions(10)
+}
