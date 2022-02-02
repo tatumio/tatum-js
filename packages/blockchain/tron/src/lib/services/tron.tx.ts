77d51af4aa7a6b51680489f880fa21cea317fab2
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-02-02 22:16:23 +0100

    feat(tron): trc20 txs

--- packages/blockchain/tron/src/lib/services/tron.tx.ts
+++ packages/blockchain/tron/src/lib/services/tron.tx.ts
@@ -1,8 +1,10 @@
 import { tronTrc10 } from './tron.trc10'
+import { tronTrc20 } from './tron.trc20'
 import { ITronWeb } from './tron.web'
 
 export const tronTx = (args: { tronWeb: ITronWeb }) => {
   return {
     trc10: tronTrc10(args),
+    trc20: tronTrc20(args),
   }
 }
