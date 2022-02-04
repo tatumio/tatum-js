43ee65077d621e4d7abba64d209a209c5412ce72
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2022-02-04 13:20:26 +0100

    #484 - Update TransferAdaBlockchain model

---
+++ src/model/request/TransferAdaBlockchain.ts
@@ -0,0 +1,19 @@
+import {
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator'
+import { TransferBtcBasedBlockchain } from './'
+
+export class TransferAdaBlockchain extends TransferBtcBasedBlockchain {
+  @IsNumberString()
+  @IsOptional()
+  public fee: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(30, 110)
+  public changeAddress: string
+}
