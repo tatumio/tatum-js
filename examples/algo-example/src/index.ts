05383b00af65cf52461993e94bfdb4b05c03b3a8
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-02-09 21:27:20 +0100

    feat(algo): web client

--- examples/algo-example/src/index.ts
+++ examples/algo-example/src/index.ts
@@ -1,3 +1,5 @@
 import { algoBlockchainExample } from './app/algo.blockchain.example'
+import { algoWeb3Example } from './app/algo.web3.example'
 
 console.log(`Running ${algoBlockchainExample()}`)
+console.log(`Running ${algoWeb3Example()}`)
--- packages/blockchain/algo/src/lib/algo.sdk.ts
+++ packages/blockchain/algo/src/lib/algo.sdk.ts
@@ -2,15 +2,19 @@ import { Blockchain } from '@tatumio/shared-core'
 import { SDKArguments } from '@tatumio/shared-abstract-sdk'
 import { abstractBlockchainSdk } from '@tatumio/shared-blockchain-abstract'
 import { BlockchainAlgorandAlgoService } from '@tatumio/api-client'
+import { algoWeb } from './services/algo.web'
 
 const blockchain = Blockchain.ALGO
 
 export const TatumAlgoSDK = (args: SDKArguments) => {
+  const web = algoWeb()
+
   return {
     ...abstractBlockchainSdk({
       ...args,
       blockchain,
     }),
+    algoWeb: web,
     blockchain: {
       broadcast: BlockchainAlgorandAlgoService.algoandBroadcast,
       getBlock: BlockchainAlgorandAlgoService.algorandGetBlock,
