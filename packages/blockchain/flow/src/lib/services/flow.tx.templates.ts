a563da9b518eab2728b458a21ab419785595ad80
martinrucki <marthy94@gmail.com>
2022-02-09 17:31:01 +0100

    chore(flow): flow tx refactoring

--- packages/blockchain/flow/src/lib/services/flow.tx.templates.ts
+++ packages/blockchain/flow/src/lib/services/flow.tx.templates.ts
@@ -1,10 +1,12 @@
 import dedent from 'dedent-js'
 import { FLOW_MAINNET_ADDRESSES, FLOW_TESTNET_ADDRESSES } from './flow.tx'
 
-export const deployNftTokenTypeWithMinterTxTemplate = (testnet: boolean) => dedent`
+export const flowTxTemplates = () => {
+  return {
+    deployNftTokenTypeWithMinterTxTemplate: (testnet: boolean) => dedent`
 import TatumMultiNFT from ${
-  testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
-}
+      testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
+    }
 transaction(type: String) {
     // local variable for storing the minter reference
     let minter: &TatumMultiNFT.AdminMinter
@@ -20,12 +22,11 @@ transaction(type: String) {
         self.minter.addMinter(minterAccount: self.newMinter, type: type)
     }
 }
-`
-
-export const metadataNftTokenScript = (testnet: boolean) => dedent`
+`,
+    metadataNftTokenScript: (testnet: boolean) => dedent`
 import TatumMultiNFT from ${
-  testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
-}
+      testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
+    }
 pub fun main(account: Address, id: UInt64, type: String): String {
     let collectionRef = getAccount(account)
         .getCapability(TatumMultiNFT.CollectionPublicPath)
@@ -37,24 +38,22 @@ pub fun main(account: Address, id: UInt64, type: String): String {
     } else {
       return panic("No such token");
     }
-}`
-
-export const tokenByAddressNftTokenScript = (testnet: boolean) => dedent`
+}`,
+    tokenByAddressNftTokenScript: (testnet: boolean) => dedent`
 import TatumMultiNFT from ${
-  testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
-}
+      testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
+    }
 pub fun main(address: Address, type: String): [UInt64] {
     let collectionRef = getAccount(address)
         .getCapability(TatumMultiNFT.CollectionPublicPath)
         .borrow<&{TatumMultiNFT.TatumMultiNftCollectionPublic}>()
         ?? panic("Could not borrow capability from public collection")
     return collectionRef.getIDsByType(type: type)
-}`
-
-export const mintNftTokenTxTemplate = (testnet: boolean) => dedent`
+}`,
+    mintNftTokenTxTemplate: (testnet: boolean) => dedent`
 import TatumMultiNFT from ${
-  testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
-}
+      testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
+    }
 transaction(recipient: Address, url: String, type: String) {
     // local variable for storing the minter reference
     let minter: &TatumMultiNFT.NFTMinter
@@ -74,12 +73,11 @@ transaction(recipient: Address, url: String, type: String) {
         // mint the NFT and deposit it to the recipient's collection
         self.minter.mintNFT(recipient: receiver, type: type, url: url, address: recipient)
     }
-}`
-
-export const mintMultipleNftTokenTxTemplate = (testnet: boolean) => dedent`
+}`,
+    mintMultipleNftTokenTxTemplate: (testnet: boolean) => dedent`
 import TatumMultiNFT from ${
-  testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
-}
+      testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
+    }
 transaction(recipient: [Address], url: [String], type: String) {
     // local variable for storing the minter reference
     let minter: &TatumMultiNFT.NFTMinter
@@ -103,12 +101,11 @@ transaction(recipient: [Address], url: [String], type: String) {
             a = a + 1
         }
     }
-}`
-
-export const burnNftTokenTxTemplate = (testnet: boolean) => dedent`
+}`,
+    burnNftTokenTxTemplate: (testnet: boolean) => dedent`
 import TatumMultiNFT from ${
-  testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
-}
+      testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
+    }
 transaction(withdrawID: UInt64, type: String) {
     // local variable for storing the minter reference
     let senderCollection: &{TatumMultiNFT.TatumMultiNftCollectionPublic}
@@ -125,12 +122,11 @@ transaction(withdrawID: UInt64, type: String) {
         // Destroy the nft
         destroy nft
     }
-}`
-
-export const transferNftTokenTxTemplate = (testnet: boolean) => dedent`
+}`,
+    transferNftTokenTxTemplate: (testnet: boolean) => dedent`
 import TatumMultiNFT from ${
-  testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
-}
+      testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
+    }
 transaction(recipient: Address, withdrawID: UInt64) {
     // local variable for storing the minter reference
     let senderCollection: &{TatumMultiNFT.TatumMultiNftCollectionPublic}
@@ -149,24 +145,22 @@ transaction(recipient: Address, withdrawID: UInt64) {
         // Deposit the NFT in the recipient's collection
         depositRef.deposit(token: <-nft)
     }
-}`
-
-export const prepareAddPublicKeyToAccountTxTemplate = () =>
-  dedent`transaction(publicKey: String) {
+}`,
+    prepareAddPublicKeyToAccountTxTemplate: () =>
+      dedent`transaction(publicKey: String) {
 prepare(signer: AuthAccount) {
 signer.addPublicKey(publicKey.decodeHex())
 }
-}`
-
-export const prepareTransferTxTemplate = (
-  testnet: boolean,
-  tokenAddress: string,
-  tokenName: string,
-  tokenStorage: string,
-) =>
-  dedent`import FungibleToken from ${
-    testnet ? FLOW_TESTNET_ADDRESSES.FungibleToken : FLOW_MAINNET_ADDRESSES.FungibleToken
-  }
+}`,
+    prepareTransferTxTemplate: (
+      testnet: boolean,
+      tokenAddress: string,
+      tokenName: string,
+      tokenStorage: string,
+    ) =>
+      dedent`import FungibleToken from ${
+        testnet ? FLOW_TESTNET_ADDRESSES.FungibleToken : FLOW_MAINNET_ADDRESSES.FungibleToken
+      }
   import ${tokenName} from ${tokenAddress}
 transaction(amount: UFix64, recipient: Address) {
   let sentVault: @FungibleToken.Vault
@@ -182,12 +176,11 @@ transaction(amount: UFix64, recipient: Address) {
         ?? panic("failed to borrow reference to recipient vault")
     receiverRef.deposit(from: <-self.sentVault)
   }
-}`
-
-export const prepareCreateAccountWithFUSDFromPublicKeyTxTemplate = (testnet: boolean) =>
-  dedent`import FungibleToken from ${
-    testnet ? FLOW_TESTNET_ADDRESSES.FungibleToken : FLOW_MAINNET_ADDRESSES.FungibleToken
-  }
+}`,
+    prepareCreateAccountWithFUSDFromPublicKeyTxTemplate: (testnet: boolean) =>
+      dedent`import FungibleToken from ${
+        testnet ? FLOW_TESTNET_ADDRESSES.FungibleToken : FLOW_MAINNET_ADDRESSES.FungibleToken
+      }
   import FUSD from ${testnet ? FLOW_TESTNET_ADDRESSES.FUSD : FLOW_MAINNET_ADDRESSES.FUSD}
   import TatumMultiNFT from ${
     testnet ? FLOW_TESTNET_ADDRESSES.TatumMultiNFT : FLOW_MAINNET_ADDRESSES.TatumMultiNFT
@@ -219,4 +212,6 @@ export const prepareCreateAccountWithFUSDFromPublicKeyTxTemplate = (testnet: boo
       )
     }
   }
-  `
+  `,
+  }
+}
