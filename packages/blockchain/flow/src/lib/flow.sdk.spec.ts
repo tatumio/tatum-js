a705f331a9f060f709b4c29957c53f0028d26a59
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-02-23 17:02:02 +0100

    chore(flow): remove commented out tests

--- packages/blockchain/flow/src/lib/flow.sdk.spec.ts
+++ packages/blockchain/flow/src/lib/flow.sdk.spec.ts
@@ -18,30 +18,6 @@ describe('TatumFlowSDK', () => {
     })
 
     describe('Address from private key', () => {
-      // describe('mainnet', () => {
-      //   it('valid', async () => {
-      //     const address0 = sdk.wallet.generateAddressFromPrivateKey(TEST_DATA.FLOW.MAINNET.PRIVATE_KEY_0)
-      //     expect(address0).toBe(TEST_DATA.FLOW.MAINNET.ADDRESS_0)
-
-      //     const address100 = sdk.wallet.generateAddressFromPrivateKey(TEST_DATA.FLOW.MAINNET.PRIVATE_KEY_100)
-      //     expect(address100).toBe(TEST_DATA.FLOW.MAINNET.ADDRESS_100)
-      //   })
-      // })
-
-      // describe('testnet', () => {
-      //   it('testnet', async () => {
-      //     const address0 = sdk.wallet.generateAddressFromPrivateKey(TEST_DATA.FLOW.TESTNET.PRIVATE_KEY_0, {
-      //       testnet: true,
-      //     })
-      //     expect(address0).toBe(TEST_DATA.FLOW.TESTNET.ADDRESS_0)
-
-      //     const address100 = sdk.wallet.generateAddressFromPrivateKey(TEST_DATA.FLOW.TESTNET.PRIVATE_KEY_100, {
-      //       testnet: true,
-      //     })
-      //     expect(address100).toBe(TEST_DATA.FLOW.TESTNET.ADDRESS_100)
-      //   })
-      // })
-
       walletTestFactory.generateAddressFromPrivateKey(sdk.wallet, TEST_DATA.FLOW)
     })
   })
