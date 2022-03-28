80f8b892fd2f7a460303719b3c3b39a2e08cce04
ssramko <samuel.sramko@tatum.io>
2022-03-28 21:08:32 +0200

    PLT-1328 - add SOL managed transfer examples

--- packages/blockchain/solana/src/lib/__tests__/solana.tx.spec.ts
+++ packages/blockchain/solana/src/lib/__tests__/solana.tx.spec.ts
@@ -1,12 +1,15 @@
 import { TatumSolanaSDK } from '../solana.sdk'
 import { REPLACE_ME_WITH_TATUM_API_KEY } from '@tatumio/shared-testing-common'
 import { SolanaNftMetadata } from '../schema'
+import { LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from '@solana/web3.js'
+import BigNumber from 'bignumber.js'
 
 jest.setTimeout(99999)
 
 describe('SolanaSDK - tx', () => {
   const sdk = TatumSolanaSDK({
     apiKey: REPLACE_ME_WITH_TATUM_API_KEY,
+    provider: 'https://api.devnet.solana.com',
   })
 
   afterEach(() => {
@@ -22,6 +25,30 @@ describe('SolanaSDK - tx', () => {
       amount: '0.001',
     })
     expect(tx).toBeDefined()
+  })
+
+  it('should generate raw data for external signing', async () => {
+    const from = '7Ha3XA3EtpJESic7MZhk2WBYFPi8whJJnWknYRj4g1Zh'
+    const to = 'FZAS4mtPvswgVxbpc117SqfNgCDLTCtk5CoeAtt58FWU'
+    const amount = '0.000001'
+
+    const devnet_fee_payer = '5zPr5331CtBjgVeLedhmJPEpFaUsorLCnb3aCQPsUc9w'
+    const mainnet_fee_payer = 'DSpHmb7hLnetoybammcJBJiyqMVR3pDhCuW6hqVg9eBF'
+
+    const fromPubkey = new PublicKey(from)
+
+    const transaction = new Transaction({ feePayer: new PublicKey(devnet_fee_payer) })
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: fromPubkey,
+        toPubkey: new PublicKey(to),
+        lamports: new BigNumber(amount).multipliedBy(LAMPORTS_PER_SOL).toNumber(),
+      }),
+    )
+    transaction.recentBlockhash = '7WyEshBZcZwEbJsvSeGgCkSNMxxxFAym3x7Cuj6UjAUE'
+    const txData = transaction.compileMessage().serialize().toString('hex')
+    expect(txData).toBeDefined()
+    console.log(txData)
   })
 
   // it('should send SOL KMS', async () => {
@@ -50,6 +77,19 @@ describe('SolanaSDK - tx', () => {
         chain: 'SOL',
         from: 'FykfMwA9WNShzPJbbb9DNXsfgDgS3XZzWiFgrVXfWoPJ',
         fromPrivateKey:
+          '2B7RyZEuZr9PpfRrn7nYhSXhjeuzte65UYeeKJFQJCvsi3ZQJk5AfmWptwDpD2Xtz22nv1aTg5rmKq13ggB7Fkep',
+        to: 'FZAS4mtPvswgVxbpc117SqfNgCDLTCtk5CoeAtt58FWU',
+        metadata: new SolanaNftMetadata('Tatum', 'TTM', 'https://tatum.io/images/logo/logo.svg', 0),
+      }),
+    )
+  })
+
+  it.skip('should prepare mint NFT tx for KMS / Custodial signing', async () => {
+    console.log(
+      await sdk.transaction.mintNft({
+        chain: 'SOL',
+        from: 'DSpHmb7hLnetoybammcJBJiyqMVR3pDhCuW6hqVg9eBF',
+        signatureId:
           '2B7RyZEuZr9PpfRrn7nYhSXhjeuzte65UYeeKJFQJCvsi3ZQJk5AfmWptwDpD2Xtz22nv1aTg5rmKq13ggB7Fkep',
         to: 'FZAS4mtPvswgVxbpc117SqfNgCDLTCtk5CoeAtt58FWU',
         metadata: new SolanaNftMetadata('Tatum', 'TTM', 'https://tatum.io/images/logo/logo.svg', 0),
