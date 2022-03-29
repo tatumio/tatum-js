79ce58d78213dc00f8b65264dc0aaabfff3c4346
Jan Dočekal <jan.docekal@dxheroes.io>
2022-03-29 10:41:38 +0200

    Lint

--- packages/blockchain/kcs/src/lib/__tests__/kcs.nft.spec.ts
+++ packages/blockchain/kcs/src/lib/__tests__/kcs.nft.spec.ts
@@ -1,84 +1,126 @@
 import {
-    commonTestFactory,
-    REPLACE_ME_WITH_TATUM_API_KEY,
-    TEST_DATA,
-    TestCasesApiCallMapping,
-  } from '@tatumio/shared-testing-common'
-  import { TatumKcsSDK } from '../kcs.sdk'
-  import * as apiClient from '@tatumio/api-client'
-  import {
-    Currency,
-  } from '@tatumio/api-client'
-  
-  jest.mock('@tatumio/api-client')
-  const mockedApi = jest.mocked(apiClient.ApiServices, true)
-  
-  describe('KcsSDK - nft', () => {
-    const sdk = TatumKcsSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
-  
-    afterEach(() => {
-      jest.clearAllMocks()
-    })
-  
-    const nft = sdk.nft
-    const api = mockedApi.nft
-    const testData = TEST_DATA.KCS
-  
-    const nftFunctionsMapping: Omit<TestCasesApiCallMapping<typeof nft>, 'prepare'|'send'> = {
-        deployNFTSmartContract: [api.nftDeployErc721, {
-            chain: Currency.KCS,
-            name: 'erc721-token',
-            symbol: 'erc721-token',
-            fromPrivateKey: testData.TESTNET.ERC_721.PRIVATE_KEY,
-        }],
-        mintNFT: [api.nftMintErc721, {
-            chain: Currency.KCS,
-            to: testData.TESTNET.ERC_721?.ADDRESS,
-            url: 'https://google.com/'
-        }],
-        transferNFT: [api.nftTransferErc721, {
-            chain: Currency.KCS,
-            to: testData.TESTNET.ERC_721?.ADDRESS,
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            tokenId: 'erc721-token',
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        mintMultipleNFTs: [api.nftMintMultipleErc721, {
-            chain: Currency.KCS,
-            to: testData.TESTNET.ERC_721?.ADDRESS,
-            tokenId: 'erc721-token',
-            minter: testData.TESTNET.ERC_721?.ADDRESS,
-            url: 'https://google.com',
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-        }],
-        burnNFT: [api.nftBurnErc721, {
-            chain: Currency.KCS,
-            tokenId: 'erc721-token',
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        addNFTMinter: [api.nftAddMinter, {
-            chain: Currency.KCS,
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            minter: testData.TESTNET.ERC_721?.ADDRESS,
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        updateNFTRoyalty: [api.nftUpdateCashbackErc721, {
-            chain: Currency.KCS,
-            tokenId: 'erc721-token',
-            cashbackValue: '1',
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        getNFTTransaction: [api.nftGetTransactErc721, Currency.KCS, '0xe6e7340394958674cdf8606936d292f565e4ecc476aaa8b258ec8a141f7c75d7'],
-        getNFTAccountBalance: [api.nftGetBalanceErc721, Currency.KCS, testData.TESTNET.ERC_721?.ADDRESS, testData.TESTNET.ERC_721?.CONTRACT_ADDRESS],
-        getNFTProvenanceData: [api.nftGetProvenanceDataErc721, Currency.KCS, 'erc721-token', testData.TESTNET.ERC_721?.CONTRACT_ADDRESS],
-        getNFTMetadataURI: [api.nftGetMetadataErc721, Currency.KCS, testData.TESTNET.ERC_721?.CONTRACT_ADDRESS, 'erc721-token'],
-        getNFTRoyalty: [api.nftGetRoyaltyErc721, Currency.KCS, testData.TESTNET.ERC_721?.CONTRACT_ADDRESS, 'erc721-token'],
-    }
-  
-    describe('API methods mapping', () => {
-      commonTestFactory.apiMethods<Omit<typeof nft, 'prepare'|'send'>>(nft, nftFunctionsMapping)
-    })
+  commonTestFactory,
+  REPLACE_ME_WITH_TATUM_API_KEY,
+  TEST_DATA,
+  TestCasesApiCallMapping,
+} from '@tatumio/shared-testing-common'
+import { TatumKcsSDK } from '../kcs.sdk'
+import * as apiClient from '@tatumio/api-client'
+import { Currency } from '@tatumio/api-client'
+
+jest.mock('@tatumio/api-client')
+const mockedApi = jest.mocked(apiClient.ApiServices, true)
+
+describe('KcsSDK - nft', () => {
+  const sdk = TatumKcsSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
+
+  afterEach(() => {
+    jest.clearAllMocks()
   })
-  
+
+  const nft = sdk.nft
+  const api = mockedApi.nft
+  const testData = TEST_DATA.KCS
+
+  const nftFunctionsMapping: Omit<TestCasesApiCallMapping<typeof nft>, 'prepare' | 'send'> = {
+    deployNFTSmartContract: [
+      api.nftDeployErc721,
+      {
+        chain: Currency.KCS,
+        name: 'erc721-token',
+        symbol: 'erc721-token',
+        fromPrivateKey: testData.TESTNET.ERC_721.PRIVATE_KEY,
+      },
+    ],
+    mintNFT: [
+      api.nftMintErc721,
+      {
+        chain: Currency.KCS,
+        to: testData.TESTNET.ERC_721?.ADDRESS,
+        url: 'https://google.com/',
+      },
+    ],
+    transferNFT: [
+      api.nftTransferErc721,
+      {
+        chain: Currency.KCS,
+        to: testData.TESTNET.ERC_721?.ADDRESS,
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        tokenId: 'erc721-token',
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    mintMultipleNFTs: [
+      api.nftMintMultipleErc721,
+      {
+        chain: Currency.KCS,
+        to: testData.TESTNET.ERC_721?.ADDRESS,
+        tokenId: 'erc721-token',
+        minter: testData.TESTNET.ERC_721?.ADDRESS,
+        url: 'https://google.com',
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+      },
+    ],
+    burnNFT: [
+      api.nftBurnErc721,
+      {
+        chain: Currency.KCS,
+        tokenId: 'erc721-token',
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    addNFTMinter: [
+      api.nftAddMinter,
+      {
+        chain: Currency.KCS,
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        minter: testData.TESTNET.ERC_721?.ADDRESS,
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    updateNFTRoyalty: [
+      api.nftUpdateCashbackErc721,
+      {
+        chain: Currency.KCS,
+        tokenId: 'erc721-token',
+        cashbackValue: '1',
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    getNFTTransaction: [
+      api.nftGetTransactErc721,
+      Currency.KCS,
+      '0xe6e7340394958674cdf8606936d292f565e4ecc476aaa8b258ec8a141f7c75d7',
+    ],
+    getNFTAccountBalance: [
+      api.nftGetBalanceErc721,
+      Currency.KCS,
+      testData.TESTNET.ERC_721?.ADDRESS,
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+    ],
+    getNFTProvenanceData: [
+      api.nftGetProvenanceDataErc721,
+      Currency.KCS,
+      'erc721-token',
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+    ],
+    getNFTMetadataURI: [
+      api.nftGetMetadataErc721,
+      Currency.KCS,
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+      'erc721-token',
+    ],
+    getNFTRoyalty: [
+      api.nftGetRoyaltyErc721,
+      Currency.KCS,
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+      'erc721-token',
+    ],
+  }
+
+  describe('API methods mapping', () => {
+    commonTestFactory.apiMethods<Omit<typeof nft, 'prepare' | 'send'>>(nft, nftFunctionsMapping)
+  })
+})
--- packages/blockchain/one/src/lib/__tests__/one.nft.spec.ts
+++ packages/blockchain/one/src/lib/__tests__/one.nft.spec.ts
@@ -1,84 +1,126 @@
 import {
-    commonTestFactory,
-    REPLACE_ME_WITH_TATUM_API_KEY,
-    TEST_DATA,
-    TestCasesApiCallMapping,
-  } from '@tatumio/shared-testing-common'
-  import { TatumOneSDK } from '../one.sdk'
-  import * as apiClient from '@tatumio/api-client'
-  import {
-    Currency,
-  } from '@tatumio/api-client'
-  
-  jest.mock('@tatumio/api-client')
-  const mockedApi = jest.mocked(apiClient.ApiServices, true)
-  
-  describe('EthSDK - nft', () => {
-    const sdk = TatumOneSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
-  
-    afterEach(() => {
-      jest.clearAllMocks()
-    })
-  
-    const nft = sdk.nft
-    const api = mockedApi.nft
-    const testData = TEST_DATA.ONE
-  
-    const nftFunctionsMapping: Omit<TestCasesApiCallMapping<typeof nft>, 'prepare'|'send'> = {
-        deployNFTSmartContract: [api.nftDeployErc721, {
-            chain: Currency.ONE,
-            name: 'erc721-token',
-            symbol: 'erc721-token',
-            fromPrivateKey: testData.TESTNET.ERC_721.PRIVATE_KEY,
-        }],
-        mintNFT: [api.nftMintErc721, {
-            chain: Currency.ONE,
-            to: testData.TESTNET.ERC_721?.ADDRESS,
-            url: 'https://google.com/'
-        }],
-        transferNFT: [api.nftTransferErc721, {
-            chain: Currency.ONE,
-            to: testData.TESTNET.ERC_721?.ADDRESS,
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            tokenId: 'erc721-token',
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        mintMultipleNFTs: [api.nftMintMultipleErc721, {
-            chain: Currency.ONE,
-            to: testData.TESTNET.ERC_721?.ADDRESS,
-            tokenId: 'erc721-token',
-            minter: testData.TESTNET.ERC_721?.ADDRESS,
-            url: 'https://google.com',
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-        }],
-        burnNFT: [api.nftBurnErc721, {
-            chain: Currency.ONE,
-            tokenId: 'erc721-token',
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        addNFTMinter: [api.nftAddMinter, {
-            chain: Currency.ONE,
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            minter: testData.TESTNET.ERC_721?.ADDRESS,
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        updateNFTRoyalty: [api.nftUpdateCashbackErc721, {
-            chain: Currency.ONE,
-            tokenId: 'erc721-token',
-            cashbackValue: '1',
-            contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
-            fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
-        }],
-        getNFTTransaction: [api.nftGetTransactErc721, Currency.ONE, '0xe6e7340394958674cdf8606936d292f565e4ecc476aaa8b258ec8a141f7c75d7'],
-        getNFTAccountBalance: [api.nftGetBalanceErc721, Currency.ONE, testData.TESTNET.ERC_721?.ADDRESS, testData.TESTNET.ERC_721?.CONTRACT_ADDRESS],
-        getNFTProvenanceData: [api.nftGetProvenanceDataErc721, Currency.ONE, 'erc721-token', testData.TESTNET.ERC_721?.CONTRACT_ADDRESS],
-        getNFTMetadataURI: [api.nftGetMetadataErc721, Currency.ONE, testData.TESTNET.ERC_721?.CONTRACT_ADDRESS, 'erc721-token'],
-        getNFTRoyalty: [api.nftGetRoyaltyErc721, Currency.ONE, testData.TESTNET.ERC_721?.CONTRACT_ADDRESS, 'erc721-token'],
-    }
-  
-    describe('API methods mapping', () => {
-      commonTestFactory.apiMethods<Omit<typeof nft, 'prepare'|'send'>>(nft, nftFunctionsMapping)
-    })
+  commonTestFactory,
+  REPLACE_ME_WITH_TATUM_API_KEY,
+  TEST_DATA,
+  TestCasesApiCallMapping,
+} from '@tatumio/shared-testing-common'
+import { TatumOneSDK } from '../one.sdk'
+import * as apiClient from '@tatumio/api-client'
+import { Currency } from '@tatumio/api-client'
+
+jest.mock('@tatumio/api-client')
+const mockedApi = jest.mocked(apiClient.ApiServices, true)
+
+describe('EthSDK - nft', () => {
+  const sdk = TatumOneSDK({ apiKey: REPLACE_ME_WITH_TATUM_API_KEY })
+
+  afterEach(() => {
+    jest.clearAllMocks()
   })
-  
+
+  const nft = sdk.nft
+  const api = mockedApi.nft
+  const testData = TEST_DATA.ONE
+
+  const nftFunctionsMapping: Omit<TestCasesApiCallMapping<typeof nft>, 'prepare' | 'send'> = {
+    deployNFTSmartContract: [
+      api.nftDeployErc721,
+      {
+        chain: Currency.ONE,
+        name: 'erc721-token',
+        symbol: 'erc721-token',
+        fromPrivateKey: testData.TESTNET.ERC_721.PRIVATE_KEY,
+      },
+    ],
+    mintNFT: [
+      api.nftMintErc721,
+      {
+        chain: Currency.ONE,
+        to: testData.TESTNET.ERC_721?.ADDRESS,
+        url: 'https://google.com/',
+      },
+    ],
+    transferNFT: [
+      api.nftTransferErc721,
+      {
+        chain: Currency.ONE,
+        to: testData.TESTNET.ERC_721?.ADDRESS,
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        tokenId: 'erc721-token',
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    mintMultipleNFTs: [
+      api.nftMintMultipleErc721,
+      {
+        chain: Currency.ONE,
+        to: testData.TESTNET.ERC_721?.ADDRESS,
+        tokenId: 'erc721-token',
+        minter: testData.TESTNET.ERC_721?.ADDRESS,
+        url: 'https://google.com',
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+      },
+    ],
+    burnNFT: [
+      api.nftBurnErc721,
+      {
+        chain: Currency.ONE,
+        tokenId: 'erc721-token',
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    addNFTMinter: [
+      api.nftAddMinter,
+      {
+        chain: Currency.ONE,
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        minter: testData.TESTNET.ERC_721?.ADDRESS,
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    updateNFTRoyalty: [
+      api.nftUpdateCashbackErc721,
+      {
+        chain: Currency.ONE,
+        tokenId: 'erc721-token',
+        cashbackValue: '1',
+        contractAddress: testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+        fromPrivateKey: testData.TESTNET.ERC_721?.PRIVATE_KEY,
+      },
+    ],
+    getNFTTransaction: [
+      api.nftGetTransactErc721,
+      Currency.ONE,
+      '0xe6e7340394958674cdf8606936d292f565e4ecc476aaa8b258ec8a141f7c75d7',
+    ],
+    getNFTAccountBalance: [
+      api.nftGetBalanceErc721,
+      Currency.ONE,
+      testData.TESTNET.ERC_721?.ADDRESS,
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+    ],
+    getNFTProvenanceData: [
+      api.nftGetProvenanceDataErc721,
+      Currency.ONE,
+      'erc721-token',
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+    ],
+    getNFTMetadataURI: [
+      api.nftGetMetadataErc721,
+      Currency.ONE,
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+      'erc721-token',
+    ],
+    getNFTRoyalty: [
+      api.nftGetRoyaltyErc721,
+      Currency.ONE,
+      testData.TESTNET.ERC_721?.CONTRACT_ADDRESS,
+      'erc721-token',
+    ],
+  }
+
+  describe('API methods mapping', () => {
+    commonTestFactory.apiMethods<Omit<typeof nft, 'prepare' | 'send'>>(nft, nftFunctionsMapping)
+  })
+})
