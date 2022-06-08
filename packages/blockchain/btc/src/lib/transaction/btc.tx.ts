bb358223a441ec80fbbbd01f63a6bbab2596f2d3
alexloiko <13203602+alexloiko@users.noreply.github.com>
2022-06-08 19:37:12 +0300

    Release 2.0.1-alpha.254
    * PLT-1091 Refactoring of btc sdk
    
    * PLT-1091 Refactoring of btc, ltc, doge sdk
    
    * PLT-1091 Release version change
    
    * Release 2.0.1-alpha.254
    
    Co-authored-by: Oleksandr Loiko <oleksandr.loiko@tatum.io>
    Co-authored-by: github-actions <github-actions@github.com>

--- packages/blockchain/btc/src/lib/transaction/btc.tx.ts
+++ packages/blockchain/btc/src/lib/transaction/btc.tx.ts
@@ -5,140 +5,160 @@ import {
   BtcTransactionFromAddressKMS,
   BtcTransactionFromUTXO,
   BtcTransactionFromUTXOKMS,
+  BtcTx,
   TransactionHashKMS,
 } from '@tatumio/api-client'
 import { amountUtils, SdkErrorCode } from '@tatumio/shared-abstract-sdk'
 import { BtcSdkError } from '../btc.sdk.errors'
 import { BtcBasedTx } from '@tatumio/shared-blockchain-btc-based'
 
-type BtcTransaction =
+export type BtcTransactionType =
   | BtcTransactionFromAddress
   | BtcTransactionFromAddressKMS
   | BtcTransactionFromUTXO
   | BtcTransactionFromUTXOKMS
 
-const sendTransaction = async (body: BtcTransaction): Promise<TransactionHashKMS> => {
-  return ApiServices.blockchain.bitcoin.btcBroadcast({
-    txData: await prepareSignedTransaction(body),
-  })
-}
+export const btcTransactions = (
+  apiCalls: {
+    btcGetTxByAddress: typeof ApiServices.blockchain.bitcoin.btcGetTxByAddress
+    btcBroadcast: typeof ApiServices.blockchain.bitcoin.btcBroadcast
+    btcGetRawTransaction: typeof ApiServices.blockchain.bitcoin.btcGetRawTransaction
+  } = {
+    btcGetTxByAddress: ApiServices.blockchain.bitcoin.btcGetTxByAddress,
+    btcBroadcast: ApiServices.blockchain.bitcoin.btcBroadcast,
+    btcGetRawTransaction: ApiServices.blockchain.bitcoin.btcGetRawTransaction,
+  },
+): BtcBasedTx<BtcTransactionType> => {
+  const privateKeysFromAddress = async (
+    transaction: Transaction,
+    body: BtcTransactionFromAddress | BtcTransactionFromAddressKMS,
+  ): Promise<Array<string>> => {
+    try {
+      const privateKeysToSign = []
+      for (const item of body.fromAddress) {
+        const txs = await apiCalls.btcGetTxByAddress(item.address, 50) // @TODO OPENAPI remove pageSize
 
-const prepareSignedTransaction = async (body: BtcTransaction): Promise<string> => {
-  try {
-    const tx = new Transaction()
-    let privateKeysToSign: string[] = []
+        for (const tx of txs) {
+          if (!tx.outputs) throw new BtcSdkError(SdkErrorCode.BTC_UTXO_NOT_FOUND)
 
-    body.to.forEach((to) => {
-      tx.to(to.address, amountUtils.toSatoshis(to.value))
-    })
+          for (const [i, o] of tx.outputs.entries()) {
+            if (o.address !== item.address) {
+              continue
+            }
 
-    if ('fromAddress' in body) {
-      privateKeysToSign = await privateKeysFromAddress(tx, body)
+            transaction.from([
+              Transaction.UnspentOutput.fromObject({
+                txId: tx.hash,
+                outputIndex: i,
+                script: Script.fromAddress(item.address).toString(),
+                satoshis: o.value,
+              }),
+            ])
 
-      const fromAddress = body.fromAddress
-      if (fromAddress && 'signatureId' in fromAddress[0] && fromAddress[0].signatureId) {
-        return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
-      }
-    } else if ('fromUTXO' in body) {
-      privateKeysToSign = await privateKeysFromUTXO(tx, body)
-
-      const fromUTXO = body.fromUTXO
-      if (fromUTXO && 'signatureId' in fromUTXO[0] && fromUTXO[0].signatureId) {
-        return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
-      }
-    }
-
-    privateKeysToSign.forEach((key) => {
-      tx.sign(new PrivateKey(key))
-    })
-
-    return tx.serialize()
-  } catch (e: any) {
-    throw new BtcSdkError(e)
-  }
-}
-
-const privateKeysFromAddress = async (
-  transaction: Transaction,
-  body: BtcTransactionFromAddress | BtcTransactionFromAddressKMS,
-): Promise<Array<string>> => {
-  try {
-    const privateKeysToSign = []
-    for (const item of body.fromAddress) {
-      const txs = await ApiServices.blockchain.bitcoin.btcGetTxByAddress(item.address, 50) // @TODO OPENAPI remove pageSize
-
-      for (const tx of txs) {
-        if (!tx.outputs) throw new BtcSdkError(SdkErrorCode.BTC_UTXO_NOT_FOUND)
-
-        for (const [i, o] of tx.outputs.entries()) {
-          if (o.address !== item.address) {
-            continue
+            if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
+            else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
           }
-
-          transaction.from([
-            Transaction.UnspentOutput.fromObject({
-              txId: tx.hash,
-              outputIndex: i,
-              script: Script.fromAddress(item.address).toString(),
-              satoshis: o.value,
-            }),
-          ])
-
-          if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
-          else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
         }
       }
+      return privateKeysToSign
+    } catch (e: any) {
+      throw new BtcSdkError(e)
     }
-    return privateKeysToSign
-  } catch (e: any) {
-    throw new BtcSdkError(e)
+  }
+
+  const privateKeysFromUTXO = async (
+    transaction: Transaction,
+    body: BtcTransactionFromUTXO | BtcTransactionFromUTXOKMS,
+  ): Promise<Array<string>> => {
+    try {
+      const privateKeysToSign = []
+
+      for (const item of body.fromUTXO) {
+        const tx = await apiCalls.btcGetRawTransaction(item.txHash)
+
+        const output = (tx.outputs ?? [])[item.index]
+        if (!output || !output.address || !output.value)
+          throw new BtcSdkError(SdkErrorCode.BTC_UTXO_NOT_FOUND)
+
+        const script = Script.fromAddress(output.address).toString()
+        transaction.from([
+          Transaction.UnspentOutput.fromObject({
+            txId: item.txHash,
+            outputIndex: item.index,
+            script: script,
+            satoshis: output.value,
+          }),
+        ])
+
+        if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
+        else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
+      }
+
+      return privateKeysToSign
+    } catch (e: any) {
+      throw new BtcSdkError(e)
+    }
+  }
+
+  const prepareSignedTransaction = async function (
+    body: BtcTransactionType,
+    options: { testnet: boolean },
+  ): Promise<string> {
+    try {
+      const tx = new Transaction()
+      let privateKeysToSign: string[] = []
+
+      body.to.forEach((to) => {
+        tx.to(to.address, amountUtils.toSatoshis(to.value))
+      })
+
+      if ('fromAddress' in body) {
+        privateKeysToSign = await privateKeysFromAddress(tx, body)
+
+        const fromAddress = body.fromAddress
+        if (fromAddress && 'signatureId' in fromAddress[0] && fromAddress[0].signatureId) {
+          return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
+        }
+      } else if ('fromUTXO' in body) {
+        privateKeysToSign = await privateKeysFromUTXO(tx, body)
+
+        const fromUTXO = body.fromUTXO
+        if (fromUTXO && 'signatureId' in fromUTXO[0] && fromUTXO[0].signatureId) {
+          return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
+        }
+      }
+
+      privateKeysToSign.forEach((key) => {
+        tx.sign(new PrivateKey(key))
+      })
+
+      return tx.serialize()
+    } catch (e: any) {
+      throw new BtcSdkError(e)
+    }
+  }
+
+  const sendTransaction = async function (
+    body: BtcTransactionType,
+    options: { testnet: boolean },
+  ): Promise<TransactionHashKMS> {
+    return apiCalls.btcBroadcast({
+      txData: await prepareSignedTransaction(body, options),
+    })
+  }
+
+  return {
+    /**
+     * Send Bitcoin transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
+     * This operation is irreversible.
+     * @param body content of the transaction to broadcast
+     * @returns transaction id of the transaction in the blockchain
+     */
+    sendTransaction,
+    /**
+     * Prepare a signed Btc transaction with the private key locally. Nothing is broadcasted to the blockchain.
+     * @returns raw transaction data in hex, to be broadcasted to blockchain.
+     */
+    prepareSignedTransaction,
   }
 }
-
-const privateKeysFromUTXO = async (
-  transaction: Transaction,
-  body: BtcTransactionFromUTXO | BtcTransactionFromUTXOKMS,
-): Promise<Array<string>> => {
-  try {
-    const privateKeysToSign = []
-
-    for (const item of body.fromUTXO) {
-      const tx = await ApiServices.blockchain.bitcoin.btcGetRawTransaction(item.txHash)
-
-      const output = (tx.outputs ?? [])[item.index]
-      if (!output || !output.address || !output.value) throw new BtcSdkError(SdkErrorCode.BTC_UTXO_NOT_FOUND)
-
-      const script = Script.fromAddress(output.address).toString()
-      transaction.from([
-        Transaction.UnspentOutput.fromObject({
-          txId: item.txHash,
-          outputIndex: item.index,
-          script: script,
-          satoshis: output.value,
-        }),
-      ])
-
-      if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
-      else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
-    }
-
-    return privateKeysToSign
-  } catch (e: any) {
-    throw new BtcSdkError(e)
-  }
-}
-
-export const btcTransactions = (): BtcBasedTx<BtcTransaction> => ({
-  /**
-   * Send Bitcoin transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
-   * This operation is irreversible.
-   * @param body content of the transaction to broadcast
-   * @returns transaction id of the transaction in the blockchain
-   */
-  sendTransaction,
-  /**
-   * Prepare a signed Btc transaction with the private key locally. Nothing is broadcasted to the blockchain.
-   * @returns raw transaction data in hex, to be broadcasted to blockchain.
-   */
-  prepareSignedTransaction,
-})
--- packages/blockchain/ltc/src/lib/transaction/ltc.tx.ts
+++ packages/blockchain/ltc/src/lib/transaction/ltc.tx.ts
@@ -13,134 +13,146 @@ import BigNumber from 'bignumber.js'
 import { LtcSdkError } from '../ltc.sdk.errors'
 import { BtcBasedTx } from '@tatumio/shared-blockchain-btc-based'
 
-type LtcTransaction =
+export type LtcTransactionTypes =
   | LtcTransactionAddress
   | LtcTransactionAddressKMS
   | LtcTransactionUTXO
   | LtcTransactionUTXOKMS
 
-const sendTransaction = async (body: LtcTransaction): Promise<TransactionHashKMS> => {
-  return ApiServices.blockchain.ltc.ltcBroadcast({
-    txData: await prepareSignedTransaction(body),
-  })
-}
-
-const prepareSignedTransaction = async (body: LtcTransaction): Promise<string> => {
-  try {
-    const tx = new Transaction()
-    let privateKeysToSign: string[] = []
-
-    body.to.forEach((to) => {
-      tx.to(to.address, amountUtils.toSatoshis(to.value))
+export const ltcTransactions = (
+  apiCalls: {
+    ltcGetTxByAddress: typeof ApiServices.blockchain.ltc.ltcGetTxByAddress
+    ltcBroadcast: typeof ApiServices.blockchain.ltc.ltcBroadcast
+    ltcGetRawTransaction: typeof ApiServices.blockchain.ltc.ltcGetRawTransaction
+  } = {
+    ltcGetTxByAddress: ApiServices.blockchain.ltc.ltcGetTxByAddress,
+    ltcBroadcast: ApiServices.blockchain.ltc.ltcBroadcast,
+    ltcGetRawTransaction: ApiServices.blockchain.ltc.ltcGetRawTransaction,
+  },
+): BtcBasedTx<LtcTransactionTypes> => {
+  const sendTransaction = async (body: LtcTransactionTypes): Promise<TransactionHashKMS> => {
+    return apiCalls.ltcBroadcast({
+      txData: await prepareSignedTransaction(body),
     })
-
-    if ('fromAddress' in body) {
-      privateKeysToSign = await privateKeysFromAddress(tx, body)
-
-      const fromAddress = body.fromAddress
-      if (fromAddress && 'signatureId' in fromAddress[0] && fromAddress[0].signatureId) {
-        return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
-      }
-    } else if ('fromUTXO' in body) {
-      privateKeysToSign = await privateKeysFromUTXO(tx, body)
-
-      const fromUTXO = body.fromUTXO
-      if (fromUTXO && 'signatureId' in fromUTXO[0] && fromUTXO[0].signatureId) {
-        return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
-      }
-    }
-
-    verifyAmounts(tx, body)
-
-    privateKeysToSign.forEach((key) => {
-      tx.sign(PrivateKey.fromWIF(key))
-    })
-
-    // @TODO unchecked serialize
-    return tx.serialize(true)
-  } catch (e: any) {
-    throw new LtcSdkError(e)
-  }
-}
-
-function verifyAmounts(tx: Transaction, body: LtcTransaction) {
-  const outputsSum: BigNumber = body.to
-    .map((to) => amountUtils.toSatoshis(to.value))
-    .reduce((e, acc) => e.plus(acc), new BigNumber(0))
-
-  const inputsSum: BigNumber = tx.inputs
-    .map((i: any) => new BigNumber(i.output.satoshis))
-    .reduce((v: BigNumber, acc: BigNumber) => v.plus(acc), new BigNumber(0))
-
-  if (outputsSum.eq(inputsSum)) {
-    throw new LtcSdkError(SdkErrorCode.BTC_FEE_TOO_SMALL)
   }
 
-  if (outputsSum.gt(inputsSum)) {
-    throw new LtcSdkError(SdkErrorCode.BTC_NOT_ENOUGH_BALANCE)
-  }
-}
+  const prepareSignedTransaction = async (body: LtcTransactionTypes): Promise<string> => {
+    try {
+      const tx = new Transaction()
+      let privateKeysToSign: string[] = []
 
-const privateKeysFromAddress = async (
-  transaction: Transaction,
-  body: LtcTransactionAddress | LtcTransactionAddressKMS,
-): Promise<Array<string>> => {
-  const privateKeysToSign = []
-  for (const item of body.fromAddress) {
-    const txs = await ApiServices.blockchain.ltc.ltcGetTxByAddress(item.address, 50) // @TODO OPENAPI remove pageSize
+      body.to.forEach((to) => {
+        tx.to(to.address, amountUtils.toSatoshis(to.value))
+      })
 
-    for (const tx of txs) {
-      for (const [i, o] of tx.outputs!.entries()) {
-        if (o.address !== item.address) {
-          continue
+      if ('fromAddress' in body) {
+        privateKeysToSign = await privateKeysFromAddress(tx, body)
+
+        const fromAddress = body.fromAddress
+        if (fromAddress && 'signatureId' in fromAddress[0] && fromAddress[0].signatureId) {
+          return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
         }
+      } else if ('fromUTXO' in body) {
+        privateKeysToSign = await privateKeysFromUTXO(tx, body)
 
-        try {
-          transaction.from({
-            txId: tx.hash,
-            outputIndex: i,
-            script: Script.fromAddress(item.address).toString(),
-            satoshis: amountUtils.toSatoshis(o.value!), // @TODO null check
-          })
-
-          if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
-          else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
-        } catch (e: any) {
-          console.error(e.toString())
+        const fromUTXO = body.fromUTXO
+        if (fromUTXO && 'signatureId' in fromUTXO[0] && fromUTXO[0].signatureId) {
+          return JSON.stringify({ txData: JSON.stringify(tx), privateKeysToSign })
         }
       }
+
+      verifyAmounts(tx, body)
+
+      privateKeysToSign.forEach((key) => {
+        tx.sign(PrivateKey.fromWIF(key))
+      })
+
+      // @TODO unchecked serialize
+      return tx.serialize(true)
+    } catch (e: any) {
+      throw new LtcSdkError(e)
     }
   }
-  return privateKeysToSign
-}
 
-const privateKeysFromUTXO = async (
-  transaction: Transaction,
-  body: LtcTransactionUTXO | LtcTransactionUTXOKMS,
-): Promise<Array<string>> => {
-  const privateKeysToSign = []
+  function verifyAmounts(tx: Transaction, body: LtcTransactionTypes) {
+    const outputsSum: BigNumber = body.to
+      .map((to) => amountUtils.toSatoshis(to.value))
+      .reduce((e, acc) => e.plus(acc), new BigNumber(0))
 
-  for (const item of body.fromUTXO) {
-    const tx = await ApiServices.blockchain.ltc.ltcGetRawTransaction(item.txHash)
+    const inputsSum: BigNumber = tx.inputs
+      .map((i: any) => new BigNumber(i.output.satoshis))
+      .reduce((v: BigNumber, acc: BigNumber) => v.plus(acc), new BigNumber(0))
 
-    const output = (tx.outputs ?? [])[item.index]
-    if (!output || !output.address || !output.value) throw new LtcSdkError(SdkErrorCode.BTC_UTXO_NOT_FOUND)
+    if (outputsSum.eq(inputsSum)) {
+      throw new LtcSdkError(SdkErrorCode.BTC_FEE_TOO_SMALL)
+    }
 
-    transaction.from({
-      txId: item.txHash,
-      outputIndex: item.index,
-      script: Script.fromAddress(output.address).toString(),
-      satoshis: amountUtils.toSatoshis(output.value),
-    })
-
-    if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
-    else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
+    if (outputsSum.gt(inputsSum)) {
+      throw new LtcSdkError(SdkErrorCode.BTC_NOT_ENOUGH_BALANCE)
+    }
   }
 
-  return privateKeysToSign
-}
+  const privateKeysFromAddress = async (
+    transaction: Transaction,
+    body: LtcTransactionAddress | LtcTransactionAddressKMS,
+  ): Promise<Array<string>> => {
+    const privateKeysToSign = []
+    for (const item of body.fromAddress) {
+      const txs = await apiCalls.ltcGetTxByAddress(item.address, 50) // @TODO OPENAPI remove pageSize
 
-export const ltcTransactions = (): BtcBasedTx<LtcTransaction> => ({
-  sendTransaction,
-  prepareSignedTransaction,
-})
+      for (const tx of txs) {
+        for (const [i, o] of tx.outputs!.entries()) {
+          if (o.address !== item.address) {
+            continue
+          }
+
+          try {
+            transaction.from({
+              txId: tx.hash,
+              outputIndex: i,
+              script: Script.fromAddress(item.address).toString(),
+              satoshis: amountUtils.toSatoshis(o.value!), // @TODO null check
+            })
+
+            if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
+            else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
+          } catch (e: any) {
+            console.error(e.toString())
+          }
+        }
+      }
+    }
+    return privateKeysToSign
+  }
+
+  const privateKeysFromUTXO = async (
+    transaction: Transaction,
+    body: LtcTransactionUTXO | LtcTransactionUTXOKMS,
+  ): Promise<Array<string>> => {
+    const privateKeysToSign = []
+
+    for (const item of body.fromUTXO) {
+      const tx = await apiCalls.ltcGetRawTransaction(item.txHash)
+
+      const output = (tx.outputs ?? [])[item.index]
+      if (!output || !output.address || !output.value) throw new LtcSdkError(SdkErrorCode.BTC_UTXO_NOT_FOUND)
+
+      transaction.from({
+        txId: item.txHash,
+        outputIndex: item.index,
+        script: Script.fromAddress(output.address).toString(),
+        satoshis: amountUtils.toSatoshis(output.value),
+      })
+
+      if ('signatureId' in item) privateKeysToSign.push(item.signatureId)
+      else if ('privateKey' in item) privateKeysToSign.push(item.privateKey)
+    }
+
+    return privateKeysToSign
+  }
+
+  return {
+    sendTransaction,
+    prepareSignedTransaction,
+  }
+}
