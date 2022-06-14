b987a804c7d0f0fdd02ff93e7959bd96f8fcb691
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-06-14 16:14:59 +0200

    chore(solana): lint

--- packages/blockchain/solana/src/lib/__tests__/solana.tx.spec.ts
+++ packages/blockchain/solana/src/lib/__tests__/solana.tx.spec.ts
@@ -1,6 +1,6 @@
-import { TatumSolanaSDK } from '@tatumio/solana'
 import { REPLACE_ME_WITH_TATUM_API_KEY } from '@tatumio/shared-testing-common'
 import { SolanaNftMetadata } from '../schema'
+import { TatumSolanaSDK } from '../solana.sdk'
 
 jest.setTimeout(99999)
 
