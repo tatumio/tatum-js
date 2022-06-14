88d2eb6bcd7d2a584e14f5a2048dddccf60c302d
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2022-06-14 17:02:14 +0200

    chore: lint

--- packages/blockchain/polygon/src/lib/services/polygon.web3.ts
+++ packages/blockchain/polygon/src/lib/services/polygon.web3.ts
@@ -2,7 +2,7 @@ import { evmBasedWeb3 } from '@tatumio/shared-blockchain-evm-based'
 import Web3 from 'web3'
 import { EvmBasedBlockchain, httpHelper, THIRD_PARTY_API } from '@tatumio/shared-core'
 
-export const polygonWeb3 = (args?: { blockchain: EvmBasedBlockchain; client?: Web3 }) => {
+export const polygonWeb3 = (args: { blockchain: EvmBasedBlockchain; client?: Web3 }) => {
   const evmBasedWeb3Result = evmBasedWeb3({
     blockchain: args.blockchain,
   })
