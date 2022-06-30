602e32833341ac85ccdcb37ebbdecbbf922717b3
Sceri <scerimail@gmail.com>
2022-06-30 12:23:20 +0500

    NOISSUE - run solana tests on devnet (#512)
    * NOISSUE - run solana tests on devnet
    
    * NOISSUE temporary fixed bch test
    
    Co-authored-by: Oleksandr Loiko <oleksandr.loiko@tatum.io>

--- packages/blockchain/solana/src/lib/solana.sdk.spec.ts
+++ packages/blockchain/solana/src/lib/solana.sdk.spec.ts
@@ -99,12 +99,12 @@ describe('TatumSolanaSDK', () => {
             to,
             amount: '0.001',
           },
-          'https://api.testnet.solana.com',
+          TEST_DATA.SOLANA.PROVIDER,
         )
         const result = await sdk.kms.sign(
           { hashes: [], id: '', serializedTransaction: JSON.stringify(txData) },
           '3abc79a31093e4cfa4a724e94a44906cbbc3a32e2f75f985a28616676a5dbaf1de8d82a7e1d0561bb0e1b729c7a9b9b1708cf2803ad0ca928a332587ace391ad',
-          'https://api.testnet.solana.com',
+          TEST_DATA.SOLANA.PROVIDER,
         )
 
         expect(result).toBeDefined()
