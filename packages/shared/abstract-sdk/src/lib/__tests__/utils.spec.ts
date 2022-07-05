eb411554be1cca4dc316ab750f3701081fbf4bff
Oleksandr Loiko <oleksandr.loiko@tatum.io>
2022-07-05 13:34:01 +0300

    PLT-1091 Satoshi conversion errors

---
+++ packages/shared/abstract-sdk/src/lib/__tests__/utils.spec.ts
@@ -0,0 +1,40 @@
+import { amountUtils, SdkErrorCode } from '@tatumio/shared-abstract-sdk'
+
+describe('Utils', function () {
+  describe('Amount utils', function () {
+    describe('valid cases', function () {
+      const validCases = [
+        [1001, 100100000000],
+        [1, 100000000],
+        [0, 0],
+        [0.1, 10000000],
+        [0.0001, 10000],
+        [0.00000001, 1],
+        [12345.67891011, 1234567891011],
+        [-1001, -100100000000],
+        [-1, -100000000],
+        [-0.00000001, -1],
+      ]
+
+      it.each(validCases)('valid amount %d', (amount, expectedSatoshis) => {
+        expect(amountUtils.toSatoshis(amount)).toBe(expectedSatoshis)
+        expect(amountUtils.toSatoshis(amount.toString())).toBe(expectedSatoshis)
+      })
+    })
+
+    describe('invalid cases', function () {
+      const invalidCases = [
+        [0.000000001],
+        [1.000000002],
+        [12345.67891011111],
+        [-1001.0000000000001],
+        [-123123.000000001],
+      ]
+
+      it.each(invalidCases)('invalid amount %d', (amount) => {
+        expect(() => amountUtils.toSatoshis(amount)).toThrow(SdkErrorCode.BTC_BASED_AMOUNT)
+        expect(() => amountUtils.toSatoshis(amount.toString())).toThrow(SdkErrorCode.BTC_BASED_AMOUNT)
+      })
+    })
+  })
+})
