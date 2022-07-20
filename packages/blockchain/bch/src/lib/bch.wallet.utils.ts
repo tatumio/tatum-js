c4cfa08db934e3f77cb47d0252042c9b595e3bf8
Oleksandr Loiko <oleksandr.loiko@tatum.io>
2022-07-20 11:31:42 +0300

    PLT-1091 Missing private key check

---
+++ packages/blockchain/bch/src/lib/bch.wallet.utils.ts
@@ -0,0 +1,31 @@
+import { Blockchain, blockchainUtils } from '@tatumio/shared-core'
+
+import bcash from '@tatumio/bitcoincashjs2-lib'
+import cashaddr from 'cashaddrjs'
+import { BtcBasedWalletUtils, btcBasedWalletUtils } from '@tatumio/shared-blockchain-btc-based'
+import { bcashAddressHelper } from './utils/bch.address'
+import { ECPair, payments } from 'bitcoinjs-lib'
+
+export const bchWalletUtils = (): BtcBasedWalletUtils => {
+  const blockchain = Blockchain.BCH
+  return {
+    ...btcBasedWalletUtils(blockchain),
+    generateAddressFromXPub: (xpub: string, i: number, options?: { testnet: boolean }): string => {
+      const network = blockchainUtils.getNetworkConfig(blockchain, options)
+      const hdNode = bcash.HDNode.fromBase58(xpub, network)
+      const legacy = hdNode.derivePath(String(i)).getAddress()
+
+      const decoded = bcashAddressHelper.decode(legacy)
+
+      return cashaddr.encode(decoded.prefix, decoded.type, decoded.hash)
+    },
+    generateAddressFromPrivateKey: (privateKey: string, options?: { testnet: boolean }): string => {
+      const network = blockchainUtils.getNetworkConfig(blockchain, options)
+      const keyPair = ECPair.fromWIF(privateKey, network)
+      const legacy = payments.p2pkh({ pubkey: keyPair.publicKey, network }).address
+      const decoded = bcashAddressHelper.decode(legacy)
+
+      return cashaddr.encode(decoded.prefix, decoded.type, decoded.hash)
+    },
+  }
+}
--- packages/shared/blockchain/btc-based/src/lib/btc-based.sdk.ts
+++ packages/shared/blockchain/btc-based/src/lib/btc-based.sdk.ts
@@ -1,13 +1,11 @@
 import { BtcBasedBlockchain } from '@tatumio/shared-core'
 import { abstractBlockchainSdk } from '@tatumio/shared-blockchain-abstract'
 import { btcBasedKmsService } from './nested/btc-based.kms'
-import { btcBasedWallet } from './nested/btc-based.wallet'
 import { TatumUrl } from '@tatumio/api-client'
 
 export const btcBasedSdk = (args: { apiKey: string; url?: TatumUrl; blockchain: BtcBasedBlockchain }) => {
   return {
     ...abstractBlockchainSdk(args),
     kms: btcBasedKmsService(args),
-    wallet: btcBasedWallet(args),
   }
 }
