f939ae08ff5d98a3227d3dff8f4977a3e0b438d7
ssramko <samuel.sramko@tatum.io>
2022-08-08 11:35:01 +0200

    TT-633 - add Collection for NFT minting in Solana

--- packages/blockchain/solana/src/lib/services/solana.tx.ts
+++ packages/blockchain/solana/src/lib/services/solana.tx.ts
@@ -384,7 +384,9 @@ const verifyNftInCollection = async (
   instructions.push(
     createVerifySizedCollectionItemInstruction({
       metadata: metadataAccount,
-      collectionAuthority: collectionVerifierPrivateKey ? web3.generateKeyPair(collectionVerifierPrivateKey).publicKey : feePayerAccount,
+      collectionAuthority: collectionVerifierPrivateKey
+        ? web3.generateKeyPair(collectionVerifierPrivateKey).publicKey
+        : feePayerAccount,
       payer: feePayerAccount,
       collectionMint: collectionAccount,
       collection: collectionMetadataAccount,
