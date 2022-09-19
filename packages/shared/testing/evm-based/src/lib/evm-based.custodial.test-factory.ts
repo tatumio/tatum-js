11ab6a17fd00004e5c7861fddaa9de0365ecec00
Damilola Fagoyinbo <fagoyinbo.damilola@tatum.io>
2022-09-19 20:31:48 +0300

    TT-33: Added gas fee

--- packages/shared/testing/evm-based/src/lib/evm-based.custodial.test-factory.ts
+++ packages/shared/testing/evm-based/src/lib/evm-based.custodial.test-factory.ts
@@ -96,6 +96,10 @@ export const custodialTestFactory = {
           {
             chain,
             fromPrivateKey,
+            fee: {
+              gasLimit: "25824",
+              gasPrice: "12000"
+            },
             custodialAddress: '0x009bc01b990e2781e8a961fd792f4ebb12a683b4',
             tokenId: ['0', '1', '0'],
             amount: ['1', '1', '0.00001'],
