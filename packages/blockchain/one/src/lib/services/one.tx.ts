96b0e467e5007ac10c26854efc0a35c06c63f664
Damilola Fagoyinbo <fagoyinbo.damilola@tatum.io>
2022-09-27 10:37:38 +0300

    One activate for gas pump

--- packages/blockchain/one/src/lib/services/one.tx.ts
+++ packages/blockchain/one/src/lib/services/one.tx.ts
@@ -1,4 +1,4 @@
-import { EvmBasedWeb3 } from '@tatumio/shared-blockchain-evm-based'
+import { EvmBasedWeb3, gasPump } from '@tatumio/shared-blockchain-evm-based'
 import { EvmBasedBlockchain } from '@tatumio/shared-core'
 import { oneCustodial } from './one.custodial'
 import { oneErc20 } from './one.erc20'
@@ -7,6 +7,7 @@ import { oneMarketplace } from './one.marketplace'
 import { oneMultiToken } from './one.multitoken'
 import { oneNative } from './one.native'
 import { oneSmartContract } from './one.smartContract'
+import { oneGasPump } from './one.gasPump'
 
 export const oneTxService = (args: { blockchain: EvmBasedBlockchain; web3: EvmBasedWeb3 }) => {
   return {
@@ -16,6 +17,7 @@ export const oneTxService = (args: { blockchain: EvmBasedBlockchain; web3: EvmBa
     marketplace: oneMarketplace(args),
     multiToken: oneMultiToken(args),
     custodial: oneCustodial(args),
+    gasPump: oneGasPump(args),
     smartContract: oneSmartContract(args),
   }
 }
