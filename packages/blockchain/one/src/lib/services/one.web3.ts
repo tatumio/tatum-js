2ded8d93e38792aec0420e789dc1d1a0a0e07cb0
Nick Papirniy <nick.papirnyi@tatum.io>
2022-10-13 12:39:55 +0300

    fix: added client as param for one web3 service

--- packages/blockchain/one/src/lib/services/one.web3.ts
+++ packages/blockchain/one/src/lib/services/one.web3.ts
@@ -2,10 +2,10 @@ import { evmBasedWeb3 } from '@tatumio/shared-blockchain-evm-based'
 import Web3 from 'web3'
 import { EvmBasedBlockchain } from '@tatumio/shared-core'
 
-export const oneWeb3 = (args: { blockchain: EvmBasedBlockchain }) => {
+export const oneWeb3 = (args: { blockchain: EvmBasedBlockchain; client?: Web3 }) => {
   const evmBasedWeb3Result = evmBasedWeb3(args)
   const getOneClient = (provider?: string, fromPrivateKey?: string): Web3 => {
-    const web3 = evmBasedWeb3Result.getClient(provider)
+    const web3 = args?.client ?? evmBasedWeb3Result.getClient(provider)
 
     if (fromPrivateKey) {
       web3.eth.accounts.wallet.add(fromPrivateKey)
