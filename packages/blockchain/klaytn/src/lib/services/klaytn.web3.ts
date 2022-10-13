4d24bc02b05a7bff1c99aace9de196087bd25921
Nick Papirniy <nick.papirnyi@tatum.io>
2022-10-13 13:14:13 +0300

    fix: added client as param for klaytn web3 service

--- packages/blockchain/klaytn/src/lib/services/klaytn.web3.ts
+++ packages/blockchain/klaytn/src/lib/services/klaytn.web3.ts
@@ -1,11 +1,11 @@
 import { evmBasedWeb3 } from '@tatumio/shared-blockchain-evm-based'
 import { EvmBasedBlockchain } from '@tatumio/shared-core'
-import caver from 'caver-js'
+import Caver from 'caver-js'
 
-export const klaytnWeb3 = (args: { blockchain: EvmBasedBlockchain }) => {
+export const klaytnWeb3 = (args: { blockchain: EvmBasedBlockchain; client?: Caver }) => {
   const evmBasedWeb3Result = evmBasedWeb3(args)
   const getKlaytnClient = (provider?: string, fromPrivateKey?: string): any => {
-    const client = new caver(provider)
+    const client = args.client ?? new Caver(provider)
 
     if (fromPrivateKey) {
       client.klay.accounts.wallet.add(fromPrivateKey)
