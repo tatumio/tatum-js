053d8e232eb16d44a2e92e0e5c8dce29a50ef2ad
ssramko <samuel.sramko@tatum.io>
2022-10-18 13:12:32 +0200

    add support + examples ofr XLM + XRP

--- packages/blockchain/xlm/src/lib/services/xlm.tx.ts
+++ packages/blockchain/xlm/src/lib/services/xlm.tx.ts
@@ -8,156 +8,160 @@ import { FromSecretOrSignatureId } from '@tatumio/shared-blockchain-abstract'
 type TransferXlm = FromSecretOrSignatureId<TransferXlmBlockchain>
 type CreateTrustline = FromSecretOrSignatureId<TrustLineXlmBlockchain>
 
-export const xlmTxService = () => {
+export const xlmTxService = (
+  apiCalls: {
+    getAccountInfo: typeof ApiServices.blockchain.xlm.xlmGetAccountInfo
+  } = { getAccountInfo: ApiServices.blockchain.xlm.xlmGetAccountInfo },
+) => {
+  /**
+   * Send Stellar transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
+   * This operation is irreversible.
+   * @param options mainnet or testnet version. If token and issuerAccount are present, instead of native XLM transfer, the transfer of a token is performed.
+   * @param body content of the transaction to broadcast
+   * @returns transaction id of the transaction in the blockchain
+   */
+  const sendTransaction = async (
+    body: TransferXlm,
+    options?: { testnet: boolean; token?: string; issuerAccount?: string },
+  ) => {
+    return ApiServices.blockchain.xlm.xlmBroadcast({ txData: await prepareSignedTransaction(body, options) })
+  }
+
+  /**
+   * Send Stellar create trustline transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
+   * This operation is irreversible.
+   * @param options mainnet or testnet version.
+   * @param body content of the transaction to broadcast
+   * @returns transaction id of the transaction in the blockchain
+   */
+  const sendTrustlineTransaction = async (body: CreateTrustline, options?: { testnet: boolean }) => {
+    return ApiServices.blockchain.xlm.xlmBroadcast({
+      txData: await prepareSignedTrustlineTransaction(body, options),
+    })
+  }
+
+  /**
+   * Sign Stellar transaction with private keys locally. Nothing is broadcast to the blockchain.
+   * @param options mainnet or testnet version. If token and issuerAccount are present, instead of native XLM transfer, the transfer of a token is performed.
+   * @param body content of the transaction to broadcast
+   * @returns transaction data to be broadcast to blockchain.
+   */
+  const prepareSignedTransaction = async (
+    body: TransferXlm,
+    options?: { testnet: boolean; token?: string; issuerAccount?: string },
+  ) => {
+    try {
+      const { fromAccount, fromSecret, to, amount, message, initialize, signatureId } = body
+
+      if (!fromSecret && !signatureId) {
+        throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
+      }
+      if (fromSecret && signatureId) {
+        throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
+      }
+      let memPhrase
+      if (message) {
+        memPhrase = message?.length > 28 ? Memo.hash(message) : Memo.text(message)
+      }
+      const memo = memPhrase
+      const account = await apiCalls.getAccountInfo(fromAccount)
+      const balance = account.balances?.find((b) => b.asset_type === Asset.native().getCode())
+      if (
+        !balance ||
+        new BigNumber(balance.balance as string).isLessThan(new BigNumber(amount).plus(0.00001))
+      ) {
+        throw new XlmSdkError(SdkErrorCode.INSUFFICIENT_FUNDS)
+      }
+
+      if (!account?.sequence || typeof account.sequence !== 'string') {
+        throw new XlmSdkError(SdkErrorCode.XLM_NO_SEQUENCE)
+      }
+
+      const builder = new TransactionBuilder(new Account(fromAccount, account.sequence), {
+        fee: '100',
+        networkPassphrase: options?.testnet ? Networks.TESTNET : Networks.PUBLIC,
+        memo,
+      }).setTimeout(300)
+
+      const operation = initialize
+        ? Operation.createAccount({
+            destination: to.trim(),
+            startingBalance: amount,
+          })
+        : Operation.payment({
+            destination: to.trim(),
+            asset:
+              options?.token && options?.issuerAccount
+                ? new Asset(options.token, options.issuerAccount)
+                : Asset.native(),
+            amount,
+          })
+
+      const tx = builder.addOperation(operation).build()
+
+      if (fromSecret) {
+        tx.sign(Keypair.fromSecret(fromSecret))
+      }
+      return tx.toEnvelope().toXDR().toString('base64')
+    } catch (e: any) {
+      throw new XlmSdkError(e)
+    }
+  }
+
+  /**
+   * Sign Stellar create trustline transaction with private keys locally. Nothing is broadcast to the blockchain.
+   * @param options mainnet or testnet version.
+   * @param body content of the transaction to broadcast
+   * @returns transaction data to be broadcast to blockchain.
+   */
+  const prepareSignedTrustlineTransaction = async (body: CreateTrustline, options?: { testnet: boolean }) => {
+    try {
+      const { fromAccount, fromSecret, issuerAccount, token, limit, signatureId } = body
+
+      if (!fromSecret && !signatureId) {
+        throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
+      }
+      if (fromSecret && signatureId) {
+        throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
+      }
+      const account = await apiCalls.getAccountInfo(fromAccount)
+      const balance = account.balances?.find((b) => b.asset_type === Asset.native().getCode())
+      if (!balance || new BigNumber(balance.balance as string).isLessThan(0.00001)) {
+        throw new XlmSdkError(SdkErrorCode.INSUFFICIENT_FUNDS)
+      }
+
+      if (!account?.sequence || typeof account.sequence !== 'string') {
+        throw new XlmSdkError(SdkErrorCode.XLM_NO_SEQUENCE)
+      }
+
+      const builder = new TransactionBuilder(new Account(fromAccount, account.sequence), {
+        fee: '100',
+        networkPassphrase: options?.testnet ? Networks.TESTNET : Networks.PUBLIC,
+      }).setTimeout(300)
+
+      const tx = builder
+        .addOperation(
+          Operation.changeTrust({
+            limit,
+            asset: new Asset(token, issuerAccount),
+          }),
+        )
+        .setTimeout(30)
+        .build()
+
+      if (fromSecret) {
+        tx.sign(Keypair.fromSecret(fromSecret))
+      }
+      return tx.toEnvelope().toXDR().toString('base64')
+    } catch (e: any) {
+      throw new XlmSdkError(e)
+    }
+  }
+
   return {
     sendTransaction,
     sendTrustlineTransaction,
     prepareSignedTransaction,
     prepareSignedTrustlineTransaction,
-  }
-}
-
-/**
- * Send Stellar transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
- * This operation is irreversible.
- * @param options mainnet or testnet version. If token and issuerAccount are present, instead of native XLM transfer, the transfer of a token is performed.
- * @param body content of the transaction to broadcast
- * @returns transaction id of the transaction in the blockchain
- */
-const sendTransaction = async (
-  body: TransferXlm,
-  options?: { testnet: boolean; token?: string; issuerAccount?: string },
-) => {
-  return ApiServices.blockchain.xlm.xlmBroadcast({ txData: await prepareSignedTransaction(body, options) })
-}
-
-/**
- * Send Stellar create trustline transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
- * This operation is irreversible.
- * @param options mainnet or testnet version.
- * @param body content of the transaction to broadcast
- * @returns transaction id of the transaction in the blockchain
- */
-const sendTrustlineTransaction = async (body: CreateTrustline, options?: { testnet: boolean }) => {
-  return ApiServices.blockchain.xlm.xlmBroadcast({
-    txData: await prepareSignedTrustlineTransaction(body, options),
-  })
-}
-
-/**
- * Sign Stellar transaction with private keys locally. Nothing is broadcast to the blockchain.
- * @param options mainnet or testnet version. If token and issuerAccount are present, instead of native XLM transfer, the transfer of a token is performed.
- * @param body content of the transaction to broadcast
- * @returns transaction data to be broadcast to blockchain.
- */
-const prepareSignedTransaction = async (
-  body: TransferXlm,
-  options?: { testnet: boolean; token?: string; issuerAccount?: string },
-) => {
-  try {
-    const { fromAccount, fromSecret, to, amount, message, initialize, signatureId } = body
-
-    if (!fromSecret && !signatureId) {
-      throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
-    }
-    if (fromSecret && signatureId) {
-      throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
-    }
-    let memPhrase
-    if (message) {
-      memPhrase = message?.length > 28 ? Memo.hash(message) : Memo.text(message)
-    }
-    const memo = memPhrase
-    const account = await ApiServices.blockchain.xlm.xlmGetAccountInfo(fromAccount)
-    const balance = account.balances?.find((b) => b.asset_type === Asset.native().getCode())
-    if (
-      !balance ||
-      new BigNumber(balance.balance as string).isLessThan(new BigNumber(amount).plus(0.00001))
-    ) {
-      throw new XlmSdkError(SdkErrorCode.INSUFFICIENT_FUNDS)
-    }
-
-    if (!account?.sequence || typeof account.sequence !== 'string') {
-      throw new XlmSdkError(SdkErrorCode.XLM_NO_SEQUENCE)
-    }
-
-    const builder = new TransactionBuilder(new Account(fromAccount, account.sequence), {
-      fee: '100',
-      networkPassphrase: options?.testnet ? Networks.TESTNET : Networks.PUBLIC,
-      memo,
-    }).setTimeout(300)
-
-    const operation = initialize
-      ? Operation.createAccount({
-          destination: to.trim(),
-          startingBalance: amount,
-        })
-      : Operation.payment({
-          destination: to.trim(),
-          asset:
-            options?.token && options?.issuerAccount
-              ? new Asset(options.token, options.issuerAccount)
-              : Asset.native(),
-          amount,
-        })
-
-    const tx = builder.addOperation(operation).build()
-
-    if (fromSecret) {
-      tx.sign(Keypair.fromSecret(fromSecret))
-    }
-    return tx.toEnvelope().toXDR().toString('base64')
-  } catch (e: any) {
-    throw new XlmSdkError(e)
-  }
-}
-
-/**
- * Sign Stellar create trustline transaction with private keys locally. Nothing is broadcast to the blockchain.
- * @param options mainnet or testnet version.
- * @param body content of the transaction to broadcast
- * @returns transaction data to be broadcast to blockchain.
- */
-const prepareSignedTrustlineTransaction = async (body: CreateTrustline, options?: { testnet: boolean }) => {
-  try {
-    const { fromAccount, fromSecret, issuerAccount, token, limit, signatureId } = body
-
-    if (!fromSecret && !signatureId) {
-      throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
-    }
-    if (fromSecret && signatureId) {
-      throw new XlmSdkError(SdkErrorCode.PARAMETER_MISMATCH)
-    }
-    const account = await ApiServices.blockchain.xlm.xlmGetAccountInfo(fromAccount)
-    const balance = account.balances?.find((b) => b.asset_type === Asset.native().getCode())
-    if (!balance || new BigNumber(balance.balance as string).isLessThan(0.00001)) {
-      throw new XlmSdkError(SdkErrorCode.INSUFFICIENT_FUNDS)
-    }
-
-    if (!account?.sequence || typeof account.sequence !== 'string') {
-      throw new XlmSdkError(SdkErrorCode.XLM_NO_SEQUENCE)
-    }
-
-    const builder = new TransactionBuilder(new Account(fromAccount, account.sequence), {
-      fee: '100',
-      networkPassphrase: options?.testnet ? Networks.TESTNET : Networks.PUBLIC,
-    }).setTimeout(300)
-
-    const tx = builder
-      .addOperation(
-        Operation.changeTrust({
-          limit,
-          asset: new Asset(token, issuerAccount),
-        }),
-      )
-      .setTimeout(30)
-      .build()
-
-    if (fromSecret) {
-      tx.sign(Keypair.fromSecret(fromSecret))
-    }
-    return tx.toEnvelope().toXDR().toString('base64')
-  } catch (e: any) {
-    throw new XlmSdkError(e)
   }
 }
--- packages/blockchain/xrp/src/lib/services/xrp.tx.ts
+++ packages/blockchain/xrp/src/lib/services/xrp.tx.ts
@@ -5,72 +5,79 @@ import { Payment } from 'ripple-lib/dist/npm/transaction/payment'
 import { XrpSdkError } from '../xrp.sdk.errors'
 import { SdkErrorCode } from '@tatumio/shared-abstract-sdk'
 
-export const xrpTxService = () => {
+export const xrpTxService = (
+  apiCalls: {
+    getAccountDetail: typeof ApiServices.blockchain.xrp.xrpGetAccountInfo
+    getFee: typeof ApiServices.blockchain.xrp.xrpGetFee
+  } = {
+    getAccountDetail: ApiServices.blockchain.xrp.xrpGetAccountInfo,
+    getFee: ApiServices.blockchain.xrp.xrpGetFee,
+  },
+) => {
+  /**
+   * Send Xrp transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
+   * This operation is irreversible.
+   * @param body content of the transaction to broadcast
+   * @returns transaction id of the transaction in the blockchain
+   */
+  const sendTransaction = async (body: TransferXrpBlockchain) => {
+    return ApiServices.blockchain.xrp.xrpBroadcast({ txData: await prepareSignedTransaction(body) })
+  }
+
+  /**
+   * Sign Xrp transaction with private keys locally. Nothing is broadcast to the blockchain.
+   * @param body content of the transaction to broadcast
+   * @returns transaction data to be broadcast to blockchain.
+   */
+  const prepareSignedTransaction = async (body: TransferXrpBlockchain) => {
+    try {
+      const { fromAccount, fromSecret, to, amount, fee, sourceTag, destinationTag } = body
+
+      if (fee && new BigNumber(fee).isZero()) throw new XrpSdkError(SdkErrorCode.FEE_TOO_SMALL)
+      const xrpFee = new BigNumber((await apiCalls.getFee())?.drops?.base_fee as string)
+      const finalFee = Math.max(new BigNumber(fee || '0').toNumber(), xrpFee.toNumber())
+      const payment: Payment = {
+        source: {
+          address: fromAccount,
+          maxAmount: {
+            currency: 'XRP',
+            value: amount,
+          },
+          tag: sourceTag,
+        },
+        destination: {
+          address: to,
+          amount: {
+            currency: 'XRP',
+            value: amount,
+          },
+          tag: destinationTag,
+        },
+      }
+      const accountInfo = await apiCalls.getAccountDetail(fromAccount)
+      if (
+        new BigNumber(accountInfo.account_data?.Balance as string).isLessThan(
+          new BigNumber(amount).plus(finalFee),
+        )
+      ) {
+        throw new XrpSdkError(SdkErrorCode.INSUFFICIENT_FUNDS)
+      }
+      const sequence = accountInfo.account_data?.Sequence
+      const maxLedgerVersion = accountInfo.ledger_current_index! + 500
+      const rippleAPI = new RippleAPI()
+      const prepared = await rippleAPI.preparePayment(fromAccount, payment, {
+        fee: `${finalFee}`,
+        sequence,
+        maxLedgerVersion,
+      })
+      const signed = rippleAPI.sign(prepared.txJSON, fromSecret)
+      return signed.signedTransaction
+    } catch (e: any) {
+      throw new XrpSdkError(e)
+    }
+  }
   return {
     sendTransaction,
     prepareSignedTransaction,
-  }
-}
-
-/**
- * Send Xrp transaction to the blockchain. This method broadcasts signed transaction to the blockchain.
- * This operation is irreversible.
- * @param body content of the transaction to broadcast
- * @returns transaction id of the transaction in the blockchain
- */
-const sendTransaction = async (body: TransferXrpBlockchain) => {
-  return ApiServices.blockchain.xrp.xrpBroadcast({ txData: await prepareSignedTransaction(body) })
-}
-
-/**
- * Sign Xrp transaction with private keys locally. Nothing is broadcast to the blockchain.
- * @param body content of the transaction to broadcast
- * @returns transaction data to be broadcast to blockchain.
- */
-const prepareSignedTransaction = async (body: TransferXrpBlockchain) => {
-  try {
-    const { fromAccount, fromSecret, to, amount, fee, sourceTag, destinationTag } = body
-
-    if (fee && new BigNumber(fee).isZero()) throw new XrpSdkError(SdkErrorCode.FEE_TOO_SMALL)
-    const xrpFee = new BigNumber((await ApiServices.blockchain.xrp.xrpGetFee())?.drops?.base_fee as string)
-    const finalFee = Math.max(new BigNumber(fee || '0').toNumber(), xrpFee.toNumber())
-    const payment: Payment = {
-      source: {
-        address: fromAccount,
-        maxAmount: {
-          currency: 'XRP',
-          value: amount,
-        },
-        tag: sourceTag,
-      },
-      destination: {
-        address: to,
-        amount: {
-          currency: 'XRP',
-          value: amount,
-        },
-        tag: destinationTag,
-      },
-    }
-    const accountInfo = await ApiServices.blockchain.xrp.xrpGetAccountInfo(fromAccount)
-    if (
-      new BigNumber(accountInfo.account_data?.Balance as string).isLessThan(
-        new BigNumber(amount).plus(finalFee),
-      )
-    ) {
-      throw new XrpSdkError(SdkErrorCode.INSUFFICIENT_FUNDS)
-    }
-    const sequence = accountInfo.account_data?.Sequence
-    const maxLedgerVersion = accountInfo.ledger_current_index! + 500
-    const rippleAPI = new RippleAPI()
-    const prepared = await rippleAPI.preparePayment(fromAccount, payment, {
-      fee: `${finalFee}`,
-      sequence,
-      maxLedgerVersion,
-    })
-    const signed = rippleAPI.sign(prepared.txJSON, fromSecret)
-    return signed.signedTransaction
-  } catch (e: any) {
-    throw new XrpSdkError(e)
   }
 }
