12222faccf057331f8644bf6dcfc58c3006a32e9
jakcinmarina <52315405+jakcinmarina@users.noreply.github.com>
2022-10-21 09:12:37 +0200

    Bsc add offchain txs (#622)
    * Add bsc offchain functionality and examples, ad smart contract examples
    
    * format
    
    * improvements
    
    * Add bep20 to offchain transaction, tweaks
    
    * Update readme
    
    * Fix typo

---
+++ examples/bsc-example/src/app/bsc.smartContract.example.ts
@@ -0,0 +1,65 @@
+import { TransactionHash } from '@tatumio/api-client'
+import { TatumBscSDK } from '@tatumio/bsc'
+
+const bscSDK = TatumBscSDK({ apiKey: '75ea3138-d0a1-47df-932e-acb3ee807dab' })
+
+//https://apidoc.tatum.io/tag/BNB-Smart-Chain#operation/BscBlockchainSmartContractInvocation
+export async function bscSmartContractExample(): Promise<void> {
+  // if you don't already have a wallet, address and private key - generate them
+  // https://apidoc.tatum.io/tag/BNB-Smart-Chain#operation/BscGenerateWallet
+  const { mnemonic } = await bscSDK.wallet.generateWallet()
+  // https://apidoc.tatum.io/tag/BNB-Smart-Chain#operation/BscGenerateAddressPrivateKey
+  const fromPrivateKey = await bscSDK.wallet.generatePrivateKeyFromMnemonic(mnemonic, 0)
+  const address = bscSDK.wallet.generateAddressFromPrivateKey(fromPrivateKey)
+
+  // FUND YOUR SENDER ACCOUNT WITH BNB FROM https://testnet.binance.org/faucet-smart
+  console.log(`Fund address: ${address}`)
+
+  // any previously deployed contract address
+  const contractAddress = '0x687422eEA2cB73B5d3e242bA5456b782919AFc85'
+
+  // smart contract read method
+  const { data } = await bscSDK.smartContract.send.smartContractReadMethodInvocationTransaction({
+    contractAddress,
+    methodName: 'transfer',
+    methodABI: {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: 'amount',
+          type: 'uint256',
+        },
+      ],
+      name: 'stake',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    params: ['0x632'],
+  })
+
+  console.log(`Smart contract data: ${data}`)
+
+  // smart contract write method
+  const { txId } = (await bscSDK.smartContract.send.smartContractMethodInvocationTransaction({
+    contractAddress,
+    methodName: 'transfer',
+    methodABI: {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: 'amount',
+          type: 'uint256',
+        },
+      ],
+      name: 'stake',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    params: ['0x632'],
+    fromPrivateKey,
+  })) as TransactionHash
+
+  console.log(`Transaction successfully sent with ID ${txId}`)
+}
--- packages/blockchain/bsc/src/lib/bsc.sdk.ts
+++ packages/blockchain/bsc/src/lib/bsc.sdk.ts
@@ -10,12 +10,14 @@ import { bscWeb3 } from './services/bsc.web3'
 import { bscKmsService } from './services/bsc.kms'
 import { bscTxService } from './services/bsc.tx'
 import { bscAuctionService } from './services/bsc.auction'
+import { virtualAccountService } from './services/bsc.virtualAccount'
 
 const blockchain = Blockchain.BSC
 
 export const TatumBscSDK = (args: SDKArguments) => {
   const web3 = bscWeb3({ blockchain })
   const api = BnbSmartChainService
+  const virtualAccount = virtualAccountService({ blockchain, web3 })
   const txService = bscTxService({ blockchain, web3 })
   const { nft, ...evmSdk } = evmBasedSdk({ ...args, blockchain, web3 })
 
@@ -60,7 +62,7 @@ export const TatumBscSDK = (args: SDKArguments) => {
     },
     smartContract: txService.smartContract,
     multiToken: txService.multiToken,
-    gasPucustodial: txService.custodial,
+    custodial: txService.custodial,
     gasPump: txService.gasPump,
     marketplace: {
       auction: bscAuctionService({ blockchain, web3 }),
@@ -88,5 +90,6 @@ export const TatumBscSDK = (args: SDKArguments) => {
       generateWallet: BnbSmartChainService.bscGenerateWallet,
       web3Driver: BnbSmartChainService.bscWeb3Driver,
     },
+    virtualAccount,
   }
 }
