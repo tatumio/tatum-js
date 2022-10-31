f8505708f5b7ec401850ac5617833b37de23d8f9
Oleksandr Loiko <oleksandr.loiko@tatum.io>
2022-10-31 13:11:21 +0200

    TT-3404 Doge examples. Revert index

--- examples/doge-example/src/index.ts
+++ examples/doge-example/src/index.ts
@@ -1,21 +1,9 @@
-// import { dogeWalletExample } from './app/doge.wallet.example'
-// import { dogeBlockchainExample } from './app/doge.blockchain.example'
-// import { dogeKmsExample } from './app/doge.kms.example'
-// import { dogeOffchainExample } from './app/doge.offchain.example'
-// import { exchangeRateExample } from './app/doge.root.example'
-// import { dogeTransactionsExample } from './app/doge.tx.example'
-import { dogeTransaction2Example } from './app/doge.tx2.example'
+import { dogeWalletExample } from './app/doge.wallet.example'
+import { dogeBlockchainExample } from './app/doge.blockchain.example'
+import { dogeOffchainExample } from './app/doge.offchain.example'
+import { dogeTransactionsExample } from './app/doge.tx.example'
 
-async function examples() {
-  await dogeTransaction2Example()
-  // await dogeWalletExample()
-}
-
-void examples()
-
-// console.log(`Running ${dogeWalletExample()}`)
-// console.log(`Running ${dogeBlockchainExample()}`)
-// console.log(`Running ${dogeKmsExample()}`)
-// console.log(`Running ${dogeOffchainExample()}`)
-// console.log(`Running ${exchangeRateExample()}`)
-// console.log(`Running ${dogeTransactionsExample()}`)
+console.log(`Running ${dogeWalletExample()}`)
+console.log(`Running ${dogeBlockchainExample()}`)
+console.log(`Running ${dogeOffchainExample()}`)
+console.log(`Running ${dogeTransactionsExample()}`)
