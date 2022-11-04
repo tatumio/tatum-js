f32f562a2c16add32d251d9d83f020ee4a70b623
Oleksandr Loiko <oleksandr.loiko@tatum.io>
2022-11-04 12:05:34 +0200

    TT-3404 Doge examples polishing

--- examples/doge-example/src/index.ts
+++ examples/doge-example/src/index.ts
@@ -2,8 +2,10 @@ import { dogeWalletExample } from './app/doge.wallet.example'
 import { dogeBlockchainExample } from './app/doge.blockchain.example'
 import { dogeVirtualAccountExample } from './app/doge.virtualAccount.example'
 import { dogeTransactionsExample } from './app/doge.tx.example'
+import { dogeSubscriptionsExample } from './app/doge.subscriptions.example'
 
 console.log(`Running ${dogeWalletExample()}`)
 console.log(`Running ${dogeBlockchainExample()}`)
 console.log(`Running ${dogeVirtualAccountExample()}`)
 console.log(`Running ${dogeTransactionsExample()}`)
+console.log(`Running ${dogeSubscriptionsExample()}`)
