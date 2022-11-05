fd7e4a298b446a8981afbe6ac086b362b059c502
isra67 <isramko@gmail.com>
2022-11-05 15:46:33 +0100

    TT-3374 - SDK ALGO - fix test

--- packages/blockchain/algo/src/lib/__tests__/algo.tx.spec.ts
+++ packages/blockchain/algo/src/lib/__tests__/algo.tx.spec.ts
@@ -1,5 +1,6 @@
 import { TEST_DATA } from '@tatumio/shared-testing-common'
 import { TatumAlgoSDK } from '../algo.sdk'
+import { TransactionHash, TransferAlgorandBlockchain } from '@tatumio/api-client'
 
 describe('AlgoSDK - tx', () => {
   const sdk = TatumAlgoSDK({ apiKey: '75ea3138-d0a1-47df-932e-acb3ee807dab' })
@@ -9,22 +10,18 @@ describe('AlgoSDK - tx', () => {
 
     // @TODO missing assets
     it.skip('should prepare ALGO signed transaction', async () => {
-      const tx = await sdk.transaction.prepare.signedTransaction(
+      const { txId } = (await sdk.transaction.signedTransaction(
         {
-          account: testData.ADDRESS_0,
-          privateKey: testData.PRIVATE_KEY_0,
-          address: testData.ADDRESS_100,
+          from: testData.ADDRESS_0,
+          fromPrivateKey: testData.PRIVATE_KEY_0,
+          to: testData.ADDRESS_100,
           amount: '0.001',
           fee: '0.001',
-        },
+        } as TransferAlgorandBlockchain,
         true,
-      )
+      )) as TransactionHash
 
-      const parsedTx = JSON.parse(tx)
-
-      expect(parsedTx.sender).toBe(testData.ADDRESS_0)
-      expect(parsedTx.receiver).toBe(testData.ADDRESS_100)
-      expect(parsedTx.signature).toBeDefined()
+      expect(txId).toBeDefined()
     })
   })
 })
