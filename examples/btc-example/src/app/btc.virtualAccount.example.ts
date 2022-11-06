953057f867061c51b5556ebd150c3c885c5af446
Oleksandr Loiko <oleksandr.loiko@tatum.io>
2022-11-06 15:15:48 +0200

    TT-3447 Btc virtual account example

--- examples/btc-example/src/app/btc.virtualAccount.example.ts
+++ examples/btc-example/src/app/btc.virtualAccount.example.ts
@@ -1,86 +1,86 @@
 import { TatumBtcSDK } from '@tatumio/btc'
+import { Currency } from '@tatumio/api-client'
 
 export async function btcVirtualAccountExample() {
   const btcSDK = TatumBtcSDK({ apiKey: '75ea3138-d0a1-47df-932e-acb3ee807dab' })
+  const options = { testnet: true }
 
-  // Create an extended public key
-  const { mnemonic, xpub } = await btcSDK.wallet.generateWallet(undefined, { testnet: true })
-  const { xpub: plainXpub } = await btcSDK.wallet.generateWallet(undefined, { testnet: true })
+  // Create an extended public key for virtual accounts
+  const { mnemonic, xpub } = await btcSDK.wallet.generateWallet(undefined, options)
+  console.log(`Mnemonic: ${mnemonic}`)
+  console.log(`Xpub: ${xpub}`)
 
-  // Generate a blockchain address from xpub (for the plainAccount)
-  const plainAccountAddress = btcSDK.wallet.generateAddressFromXPub(plainXpub, 1, { testnet: true })
+  // Generate a recipient blockchain address
+  const { xpub: recipientXpub } = await btcSDK.wallet.generateWallet(undefined, options)
+  const recipientAddress = btcSDK.wallet.generateAddressFromXPub(recipientXpub, 1, options)
+  console.log(`Recipient Xpub: ${recipientXpub}`)
+  console.log(`Recipient address: ${recipientAddress}`)
 
-  // Create an account with xpub and without xpub
+  // Create an account with xpub
   // You can find more details in https://apidoc.tatum.io/tag/Account#operation/createAccount
-  const xpubAccount = await btcSDK.ledger.account.create({
-    currency: 'BTC',
+  const account = await btcSDK.ledger.account.create({
+    currency: Currency.BTC,
     xpub,
   })
-  console.log(`Created xpub account: ${JSON.stringify(xpubAccount)}`)
-  const plainAccount = await btcSDK.ledger.account.create({
-    currency: 'BTC',
-  })
-  console.log(`Created account: ${JSON.stringify(plainAccount)}`)
+  console.log(`Created xpub account: ${JSON.stringify(account)}`)
 
   // Check if an account exists
+  const accountId = account.id
+
   try {
-    const account = await btcSDK.virtualAccount.depositAddress.checkExists(xpubAccount.id)
-    console.log(`Account: ${JSON.stringify(account)}`)
+    const existingAccount = await btcSDK.virtualAccount.depositAddress.checkExists(accountId)
+    console.log(`Account: ${JSON.stringify(existingAccount)}`)
   } catch (e) {
     console.log(`Account: ${e.message}`)
   }
 
   // Create a deposit address for an account and derivation index
   // You can find more details in https://apidoc.tatum.io/tag/Blockchain-addresses#operation/generateDepositAddress
-  const address = await btcSDK.virtualAccount.depositAddress.create(xpubAccount.id, 1)
+  const address = await btcSDK.virtualAccount.depositAddress.create(accountId)
   console.log(`Deposit address: ${JSON.stringify(address)}`)
 
   // Create multiple deposit addresses for an account and derivation index
   // You can find more details in https://apidoc.tatum.io/tag/Account#operation/createAccountBatch
-  const addresses = await btcSDK.virtualAccount.depositAddress.createMultiple({
-    addresses: [
-      {
-        accountId: xpubAccount.id,
-        derivationKey: 2,
-      },
-      {
-        accountId: xpubAccount.id,
-        derivationKey: 3,
-      },
-    ],
-  })
-  console.log(`Deposit addresses: ${JSON.stringify(addresses)}`)
-
-  // Assign a given deposit address to a given account (non-xpub account is mandatory)
-  // You can find more details in https://apidoc.tatum.io/tag/Blockchain-addresses#operation/assignAddress
-  const assignedAddress = await btcSDK.virtualAccount.depositAddress.assign(
-    plainAccount.id,
-    plainAccountAddress,
-    1,
-  )
-  console.log(`Assign address: ${JSON.stringify(assignedAddress)}`)
+  try {
+    const addresses = await btcSDK.virtualAccount.depositAddress.createMultiple({
+      addresses: [
+        {
+          accountId: accountId,
+          derivationKey: 2,
+        },
+        {
+          accountId: accountId,
+          derivationKey: 3,
+        },
+      ],
+    })
+    console.log(`Deposit addresses: ${JSON.stringify(addresses)}`)
+  } catch (e) {
+    console.log('Deposit addresses exists', e.message)
+  }
 
   // Get all deposit addresses for an account
   // You can find more details in https://apidoc.tatum.io/tag/Blockchain-addresses#operation/getAllDepositAddresses
-  const addressByAccount = await btcSDK.virtualAccount.depositAddress.getByAccount(xpubAccount.id)
+  const addressByAccount = await btcSDK.virtualAccount.depositAddress.getByAccount(accountId)
   console.log(`Account addresses: ${JSON.stringify(addressByAccount)}`)
 
-  // Remove a deposit address from an account
-  // You can find more details in https://apidoc.tatum.io/tag/Blockchain-addresses#operation/removeAddress
-  await btcSDK.virtualAccount.depositAddress.remove(plainAccount.id, plainAccountAddress)
-
-  // Fund your address here: http://testnet.litecointools.com/
+  // Fund your address here: https://testnet-faucet.com/btc-testnet/
   console.log(`Fund me ${address.address} to send virtual account transaction!`)
 
+  // Get account info
+  // You can find more details in https://apidoc.tatum.io/tag/Account#operation/getAccountByAccountId
+  const accountById = await btcSDK.ledger.account.get(accountId)
+  console.log(`Account info: ${JSON.stringify(accountById)}`)
+
   // Send assets from virtualAccount to blockchain address
-  // This example requires a funded blockchain address, you can top up your testnet balance with https://testnet-faucet.com/BTC-testnet/
-  // https://apidoc.tatum.io/tag/Blockchain-operations#operation/BTCTransfer
+  // This example requires a funded blockchain address, you can top up your testnet balance with https://testnet-faucet.com/btc-testnet/
+  // https://apidoc.tatum.io/tag/Blockchain-operations#operation/BtcTransfer
   const transfer = await btcSDK.virtualAccount.send({
-    senderAccountId: xpubAccount.id,
+    senderAccountId: accountId,
     amount: '0.0001',
     mnemonic: mnemonic,
     xpub: xpub,
-    address: plainAccountAddress,
+    address: recipientAddress,
     fee: '0.00001',
   })
 
