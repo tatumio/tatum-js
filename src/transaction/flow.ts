103860419b5f52d3a06dd59af0ac6c4c7af2fd6b
ssramko <samuel.sramko@tatum.io>
2022-11-18 10:29:26 +0100

    Increase default flow fee limit to max

--- src/transaction/flow.ts
+++ src/transaction/flow.ts
@@ -157,7 +157,7 @@ const sendTransaction = async (testnet: boolean, {
             fcl.proposer(proposer),
             fcl.authorizations(authorizations),
             fcl.payer(payer),
-            fcl.limit(20000),
+            fcl.limit(9999),
         ]);
     } catch (e) {
         try {
