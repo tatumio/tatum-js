1ab2cc2a5bb87bfbe7b14d53fc836d5f1a8cf206
Nick Papirniy <nick.papirnyi@tatum.io>
2022-12-08 15:43:22 +0100

    fix(eth): removed ethgasstation

---
+++ packages/api-client/src/generated/models/BlockchainFee.ts
@@ -0,0 +1,33 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Recommended fees to perform blockchain transaction
+ */
+export type BlockchainFee = {
+    /**
+     * Fast transaction acceptance time into block. For btc-based chains - fee per byte. For evm-based chains - gas price in wei
+     */
+    fast: number;
+    /**
+     * Medium transaction acceptance time into block. For btc-based chains - fee per byte. For evm-based chains - gas price in wei
+     */
+    medium: number;
+    /**
+     * Slow transaction acceptance time into block. For btc-based chains - fee per byte. For evm-based chains - gas price in wei
+     */
+    slow: number;
+    /**
+     * (evm-based only) This is the minimum fee needs to paid in order for the tx to be accepted into block.
+     */
+    baseFee?: number;
+    /**
+     * Last time fees were recalculated
+     */
+    time: string;
+    /**
+     * Last used to calculate fee from
+     */
+    block: string;
+}
