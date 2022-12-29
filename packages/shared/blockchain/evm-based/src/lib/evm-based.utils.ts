d41a752087b5b72dbaff684b55ed7bb590bc294b
Jan Dočekal <hello@jdckl.dev>
2022-12-29 17:00:28 +0100

    TT-4075 - add missing call statement

--- packages/shared/blockchain/evm-based/src/lib/evm-based.utils.ts
+++ packages/shared/blockchain/evm-based/src/lib/evm-based.utils.ts
@@ -93,17 +93,24 @@ export const evmBasedUtils = {
     return signedTransaction.rawTransaction
   },
 
-  validateErc20Balance: async (client: Web3, privateKey: string, contractAddress: string, tx: TransactionConfig) => {
+  validateErc20Balance: async (
+    client: Web3,
+    privateKey: string,
+    contractAddress: string,
+    tx: TransactionConfig,
+  ) => {
     const { value } = tx
     const account = client.eth.accounts.privateKeyToAccount(privateKey)
     const contract = new client.eth.Contract(Erc20Token.abi as any, contractAddress)
-    const balance = await contract.methods.balanceOf(account.address)
+    const balance = await contract.methods.balanceOf(account.address).call()
 
     if (!balance || new BigNumber(balance).isLessThan(value.toString())) {
       throw new EvmBasedSdkError({
         code: SdkErrorCode.INSUFFICIENT_FUNDS,
         error: new Error(
-          `Insufficient funds burn erc20 transaction from account ${account.address} -> available balance is ${balance}, required balance is ${value.toString()}`,
+          `Insufficient funds burn erc20 transaction from account ${
+            account.address
+          } -> available balance is ${balance}, required balance is ${value.toString()}`,
         ),
       })
     }
