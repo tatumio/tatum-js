d4334decb4f7d4f276167662cbc56f8ab73a61ff
Hathoriel <lukas.kotol@tatum.io>
2023-02-09 08:49:00 +0100

    Added IP based auth

--- src/e2e/tatum.spec.ts
+++ src/e2e/tatum.spec.ts
@@ -1,4 +1,5 @@
 import { TatumSdk } from '../service/tatum/tatum'
+import { Chain } from '../service/tatum/tatum.dto'
 
 describe('Tatum Init', () => {
   describe('Api Key Auth', () => {
@@ -38,6 +39,33 @@ describe('Tatum Init', () => {
       await expect(TatumSdk.init({
         apiKey: process.env.TESTNET_API_KEY,
       })).rejects.toThrow('Testnet flag is required when apiKey is set. Please set it to true or false.')
+    })
+  })
+
+  describe('IP Auth', () => {
+    it('Default Mainnet', async () => {
+      const tatum: TatumSdk = await TatumSdk.init()
+      const nfts = await tatum.nft.getBalance({ chain: Chain.ETH, address: '0x51abC4c9e7BFfaA99bBE4dDC33d75067EBD0384F' })
+      const contract = nfts.filter(nft => nft.contractAddress === '0x60f80121c31a0d46b5279700f9df786054aa5ee5')
+      expect(contract).toHaveLength(1)
+    })
+
+    it('Testnet', async () => {
+      const tatum: TatumSdk = await TatumSdk.init({
+        testnet: true,
+      })
+      const nfts = await tatum.nft.getBalance({ chain: Chain.ETH, address: '0x51abC4c9e7BFfaA99bBE4dDC33d75067EBD0384F' })
+      const contract = nfts.filter(nft => nft.contractAddress === '0x0e4b1a84b504660e0fa473da1f491e5baeb43897')
+      expect(contract).toHaveLength(1)
+    })
+
+    it('Mainnet', async () => {
+      const tatum: TatumSdk = await TatumSdk.init({
+        testnet: false,
+      })
+      const nfts = await tatum.nft.getBalance({ chain: Chain.ETH, address: '0x51abC4c9e7BFfaA99bBE4dDC33d75067EBD0384F' })
+      const contract = nfts.filter(nft => nft.contractAddress === '0x60f80121c31a0d46b5279700f9df786054aa5ee5')
+      expect(contract).toHaveLength(1)
     })
   })
 })
