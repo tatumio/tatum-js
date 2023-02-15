2747c100c3ad0e8f95ab3af77f3c7f25ce0325e5
Hathoriel <lukas.kotol@tatum.io>
2023-02-15 16:13:22 +0100

    ALL-496 - nextjs

--- examples/nextjs/pages/api/hello.ts
+++ examples/nextjs/pages/api/hello.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-
 type Data = {
   name: string
 }
@@ -10,6 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const test = TatumSdk
+
   res.status(200).json({ name: 'John Doe' })
 }
