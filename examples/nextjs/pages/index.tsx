8adc99f1d5b187cce6d8d9d5d3088175809a00c5
Hathoriel <lukas.kotol@tatum.io>
2023-02-17 16:03:11 +0100

    ALL-5 - sections

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -25,6 +25,17 @@ const Home: NextPage = () => {
           className='text-3xl py-7 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#523bff] to-[#57c7ff]'>
           Subscriptions
         </h2>
+        // TODO: https://parceljs.org/docs/
+        <a href="/getting-started/webapp/"
+           className="bg-gradient-to-r card dark:from-blue-800 dark:text-white dark:to-purple-800 from-blue-200 hover:scale-105 p-5 rounded-xl to-purple-200 transform transition">
+          <svg aria-hidden="true" className="flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"
+               stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+          </svg>
+          <div className="font-bold mb-2 mt-4 text-lg">Building a webapp with Parcel</div>
+          <div>Learn how to set up a new web application from scratch with Parcel.</div>
+        </a>
         {!isLoading ? <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
           <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
             <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
