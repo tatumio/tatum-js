49383e3011ddbcd65e2df201ce19afa2123926f4
Hathoriel <lukas.kotol@tatum.io>
2023-03-02 11:09:09 +0100

    ALL-45 -  release alpha 19 - fix package json export

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -1,7 +1,7 @@
-const TatumSdk = require('@tatumcom/js')
+import { TatumSdk } from '@tatumcom/js'
 
 const run = async () => {
-  const tatumSdk = await TatumSdk.TatumSdk.init({ apiKey: '452826a8-5cd4-4c46-b710-e130934b5102_1000', network: 'Testnet' });
+  const tatumSdk = await TatumSdk.init({ apiKey: '452826a8-5cd4-4c46-b710-e130934b5102_1000', network: 'Testnet' });
   const { data } = await tatumSdk.getApiInfo()
   console.log(data)
   console.log(await tatumSdk.notification.getAll())
