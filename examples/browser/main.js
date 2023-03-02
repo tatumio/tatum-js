f5000478f77eb497bf0cd00341cfcd13e76dd018
Hathoriel <lukas.kotol@tatum.io>
2023-03-02 15:08:19 +0100

    ALL-45 - fix examples with 30

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -1,7 +1,7 @@
-import { TatumSdk } from '@tatumcom/js'
+import { Network, TatumSdk } from '@tatumcom/js'
 
 const run = async () => {
-  const tatumSdk = await TatumSdk.init({ apiKey: '452826a8-5cd4-4c46-b710-e130934b5102_1000', network: 'Testnet' });
+  const tatumSdk = await TatumSdk.init({ apiKey: '452826a8-5cd4-4c46-b710-e130934b5102_1000', network: Network.Testnet });
   const { data } = await tatumSdk.getApiInfo()
   console.log(data)
   console.log(await tatumSdk.notification.getAll())
--- examples/typescript/src/tatum.ts
+++ examples/typescript/src/tatum.ts
@@ -1,6 +1,6 @@
 import { Network, TatumSdk } from '@tatumcom/js'
 
 export const initTatum = async () => await TatumSdk.init({
-  apiKey: '452826a8-5cd4-4c46-b710-e130934b5102',
+  apiKey: '452826a8-5cd4-4c46-b710-e130934b5102_1000',
   network: Network.Testnet,
 })
