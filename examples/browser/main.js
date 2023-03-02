2c2d0b2fee5cbb800136c837d1a4c1be91a23a84
Hathoriel <lukas.kotol@tatum.io>
2023-03-02 10:35:38 +0100

    ALL-45 -  release alpha 17 - reexport browser

---
+++ examples/browser/main.js
@@ -0,0 +1,9 @@
+const TatumSdk = require('@tatumcom/js')
+
+const run = async () => {
+  const tatumSdk = await TatumSdk.TatumSdk.init({ apiKey: '452826a8-5cd4-4c46-b710-e130934b5102_1000', network: 'Testnet' });
+  const { data } = await tatumSdk.getApiInfo()
+  console.log(data)
+  console.log(await tatumSdk.notification.getAll())
+}
+run()
