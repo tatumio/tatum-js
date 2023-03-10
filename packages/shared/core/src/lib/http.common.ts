e7700798761da74b7414e0832afb7f2b02bff49e
Hathoriel <lukas.kotol@tatum.io>
2023-03-10 15:37:16 +0100

    ALL-841 - fix http helper

--- packages/shared/core/src/lib/http.common.ts
+++ packages/shared/core/src/lib/http.common.ts
@@ -34,11 +34,7 @@ const isWebWorker =
   self.constructor &&
   ['DedicatedWorkerGlobalScope', 'ServiceWorkerGlobalScope'].includes(self.constructor.name)
 
-export const httpHelper = {
-  get: axios.get,
-  post: axios.post,
-  axios: axios.create(),
-  CancelToken: axios.CancelToken,
+export const httpCommon = {
   web3Endpoint: (blockchain: EvmBasedBlockchain, url: string, apiKey: string) => {
     return `${url}/${TATUM_API_CONSTANTS.API_VERSION}/${EndpointsMapping[blockchain]}/web3/${apiKey}`
   },
@@ -47,3 +43,34 @@ export const httpHelper = {
     return `${url}/${TATUM_API_CONSTANTS.API_VERSION}/blockchain/node/${chain}/${apiKey}`
   },
 }
+
+
+export const httpAxios = {
+  get: axios.get,
+  post: axios.post,
+  axios: axios.create(),
+  CancelToken: axios.CancelToken,
+  ...httpCommon,
+}
+
+export const httpFetch = {
+  get: async (url: string, config?: any) => {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: { ...config?.headers, 'Content-Type': 'application/json' },
+    })
+    return response.json()
+  },
+  post: async (url: string, body: any, config?: any) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { ...config?.headers, 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    return response.json()
+  },
+  ...httpCommon,
+}
+
+
+export const httpHelper = isWebWorker ? httpFetch : httpAxios
