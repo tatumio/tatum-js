feeb70f4c98941a692720ff255685c832e25fbd6
Smrecz <diegoxlt@gmail.com>
2023-03-20 13:02:24 +0100

    nextjs demo app fixed10

--- examples/nextjs/components/modal.tsx
+++ examples/nextjs/components/modal.tsx
@@ -36,7 +36,7 @@ export const useModal = ({ response, handleSubmit, inputs, title }: UseModalProp
   const [loading, setLoading] = useState(false)
   const [show, setShow] = useState(false)
 
-  const [selectedNotificationType, setSelectedNotificationType] = useState('');
+  const [selectedNotificationType, setSelectedNotificationType] = useState('ADDRESS_EVENT');
 
   const ref = useRef(null)
 
@@ -79,8 +79,16 @@ export const useModal = ({ response, handleSubmit, inputs, title }: UseModalProp
                     />
                   );
                 })}
-                {inputs.text.map((input, i) => <TextInputModal key={i.toString()} label={input.label}
-                                                               placeholder={input.placeholder} id={input.id} />)}
+                {inputs.text.map((input, i) =>
+                  selectedNotificationType !== 'FAILED_TXS_PER_BLOCK' || input.id !== 'address' ? (
+                    <TextInputModal
+                      key={i.toString()}
+                      label={input.label}
+                      placeholder={input.placeholder}
+                      id={input.id}
+                    />
+                  ) : null
+                )}
                 <button type='submit'
                         className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
                   {!loading ? <div>Add</div> :
