bd390c49c4462097cc3bb703799d01cfb6462d06
ssramko <samuel.sramko@tatum.io>
2023-04-14 22:47:30 +0200

    Removed support for Open Network Added support for native RPC calls for UTXO and EVM blockchains Changed the way how SDK is being initialized

--- src/e2e/tatum.rpc.spec.ts
+++ src/e2e/tatum.rpc.spec.ts
@@ -1,3 +1,38 @@
-jest.setTimeout(50_000)
+import { Network, TatumSDK } from '../service'
+import { Bitcoin, BitcoinTestnet, Polygon, PolygonMumbai } from '../dto'
 
-describe('RPCs', () => {})
+describe('RPCs', () => {
+
+  describe('Bitcoin', () => {
+    describe('testnet', () => {
+      it('should get chain info', async () => {
+        const sdk = await TatumSDK.init<BitcoinTestnet>({network: Network.BITCOIN_TESTNET, verbose: true})
+        const info = await sdk.rpc.getBlockChainInfo()
+        expect(info.chain).toBe('test')
+      })
+    })
+    describe('mainnet', () => {
+      it('should get chain info', async () => {
+        const sdk = await TatumSDK.init<Bitcoin>({network: Network.BITCOIN, verbose: true})
+        const info = await sdk.rpc.getBlockChainInfo()
+        expect(info.chain).toBe('main')
+      })
+    })
+  })
+  describe('Polygon', () => {
+    describe('testnet', () => {
+      it('should get chain info', async () => {
+        const sdk = await TatumSDK.init<PolygonMumbai>({network: Network.POLYGON_MUMBAI, verbose: true})
+        const info = await sdk.rpc.chainId()
+        expect(info.toNumber()).toBe(80001)
+      })
+    })
+    describe('mainnet', () => {
+      it('should get chain info', async () => {
+        const sdk = await TatumSDK.init<Polygon>({network: Network.POLYGON, verbose: true})
+        const info = await sdk.rpc.chainId()
+        expect(info.toNumber()).toBe(137)
+      })
+    })
+  })
+})
