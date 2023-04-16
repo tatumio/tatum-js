4ad97b9908f040c60a359d1244d1253b23e962c2
ssramko <samuel.sramko@tatum.io>
2023-04-16 09:57:35 +0200

    Removed support for Open Network Added support for native RPC calls for UTXO and EVM blockchains Changed the way how SDK is being initialized

--- src/e2e/e2e.util.ts
+++ src/e2e/e2e.util.ts
@@ -13,15 +13,15 @@ export const e2eUtil = {
     getAddress: (network: Network): string => {
       switch (network) {
         case Network.ETHEREUM_SEPOLIA:
-          return '0xC3bB8b3F623e3E3a6Fea8A02B0F79c7E21e467b0'
+          return '0xdb4C3b4350EE869F2D0a2F43ce0292865E2Aa149'
         case Network.CELO_ALFAJORES:
-          return '0x9b38ffbab4acb7a9fd15a7b3bbab57967241663f'
+          return '0xdf083B077F1FD890fC71feCaBbd3F68F94cD21Bf'
         case Network.POLYGON_MUMBAI:
-          return '0x9b38ffbab4acc7a9fd15a7b3bbab5796724166ef'
+          return '0xd608d48c6ddB41254d73ec0619FE31B254Fb5ab8'
         case Network.KLAYTN_BAOBAB:
-          return '0x9B38fFBaB4Acb7A9FD15a7B3BBAb5796724166EF'
+          return '0xdc7Dfb8Aa86D41b7e39441711Fe1669f2d843C06'
         case Network.BINANCE_SMART_CHAIN_TESTNET:
-          return '0x9b38ffbab4acb7a9fd15a7b3bbab5796784166ef'
+          return '0xddde2061b144Be4b5921eE1F1Cd2Db4eDC9AE6aA'
         case Network.SOLANA_DEVNET:
           return '53U8WBbpjfwMprZ9YQr5frU5DtY1gNRBwzxkcPHtxQP8'
         case Network.TRON_SHASTA:
@@ -38,13 +38,13 @@ export const e2eUtil = {
         case Network.DOGECOIN_TESTNET:
           return 'npMHRfj9bdR7bBdQe7RNQNK1QM2nRAN8Sh'
         case Network.ETHEREUM:
-          return '0xC3bB8b3F623e3E3a6Fea8A02B0F79c7E21e467b0'
+          return '0xd6B4Dfd10441c15e0D95a2Bce023b4F002634c72'
         case Network.CELO:
-          return '0x9b38ffbab4acb7a9fd15a7b3bbac5796724166ef'
+          return '0xd5f3B4e29A6f2263a9e3F6518309C78d73E407d4'
         case Network.POLYGON:
-          return '0x9b38ffbab4acb7a9fd15a7b3bbab5796724166ee'
+          return '0xdbFad06Aa5cC4a5EDB88ee63A515F8Cc31C7cA52'
         case Network.KLAYTN:
-          return '0x9b38ffbab4acb7a9fd15a7b3bbab5796724166e4'
+          return '0xd8Dc7359aF648462f13e8207aff04559271DD791'
         case Network.BINANCE_SMART_CHAIN:
           return '0x9b38ffbac4acb7a9fd15a7b3bbab5796724166ef'
         case Network.SOLANA:
