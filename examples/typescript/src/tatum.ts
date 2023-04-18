bb951bf3737f220c7488e4fe25f0febf2016f508
Smrecz <diegoxlt@gmail.com>
2023-04-18 11:11:12 +0200

    demo prep

--- examples/typescript/src/tatum.ts
+++ examples/typescript/src/tatum.ts
@@ -1,3 +1,3 @@
 import { Ethereum, Network, TatumSDK } from '@tatumcom/js'
 
-export const initTatum = async () => await TatumSDK.init<Ethereum>({ network: Network.ETHEREUM, verbose: true })
+export const initTatum = async () => await TatumSDK.init<Ethereum>({ network: Network.ETHEREUM_SEPOLIA, verbose: true })
