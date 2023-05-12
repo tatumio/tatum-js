1efdcb2fa642dc59a202ccec7b97a7e21ce953ca
ssramko <samuel.sramko@tatum.io>
2023-05-12 09:28:54 +0200

    Added support for Deploying contracts using MetaMask. It's possible to deploy NFT Collections (ERC-721/1155) or Fungible Tokens (ERC-20) directly from the browser.

---
+++ src/dto/walletProvider/CreateFungibleToken.ts
@@ -0,0 +1,34 @@
+export interface CreateFungibleToken {
+  /**
+   * Name of the token.
+   */
+  name: string
+  /**
+   * Symbol of the token.
+   */
+  symbol: string
+  /**
+   * Number of decimals of the token. Defaults to 18.
+   */
+  decimals?: number
+  /**
+   * Total supply of the token.
+   */
+  initialSupply: number
+  /**
+   * (Optional) Address of the initial holder of the token. Defaults to the connected MetaMask account.
+   */
+  initialHolder?: string
+  /**
+   * (Optional) Address of the admin of the token. Defaults to the connected MetaMask account. Admin can add new minters and pausers.
+   */
+  admin?: string
+  /**
+   * (Optional) Address of the minter of the token. Defaults to the connected MetaMask account. Minters can mint new tokens.
+   */
+  minter?: string
+  /**
+   * (Optional) Address of the pauser of the token. Defaults to the connected MetaMask account. Pausers can pause and unpause the token transactions.
+   */
+  pauser?: string
+}
