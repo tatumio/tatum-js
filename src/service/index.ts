1eef57c505366d2cf0de53a6d4acba2260ad14b8
Smrecz <diegoxlt@gmail.com>
2023-05-17 14:55:00 +0200

    ALL-1568 - export newly added types from root

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,3 +1,7 @@
 export * from './notification'
 export * from './rpc'
 export * from './tatum'
+export * from './nft'
+export * from './address'
+export * from './fee'
+export * from './walletProvider'
