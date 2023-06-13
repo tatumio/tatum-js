234e9525e9d74254ee15e64b07d4620271744435
jiri.andras <jiri.andras@tatum.io>
2023-06-13 08:08:15 +0200

    Replace .then with await

--- src/api/tatum.api.ts
+++ src/api/tatum.api.ts
@@ -41,42 +41,38 @@ export class TatumApi {
   }
 
   public async getTokensFromCollection(params: ApiMetadataRequest): Promise<ApiCollectionsResponse[]> {
-    return this.connector
-      .get<{ result: ApiCollectionsResponse[] }, ApiMetadataRequest>({
-        path: `data/collections`,
-        params,
-      })
-      .then((r) => r.result)
+    const { result } = await this.connector.get<{ result: ApiCollectionsResponse[] }, ApiMetadataRequest>({
+      path: `data/collections`,
+      params,
+    })
+    return result
   }
 
   public async getTokenMetadata(params: ApiMetadataRequest): Promise<ApiMetadataResponse[]> {
-    return this.connector
-      .get<{ result: ApiMetadataResponse[] }, ApiMetadataRequest>({
-        path: `data/metadata`,
-        params,
-      })
-      .then((r) => r.result)
+    const { result } = await this.connector.get<{ result: ApiMetadataResponse[] }, ApiMetadataRequest>({
+      path: `data/metadata`,
+      params,
+    })
+    return result
   }
 
   public async getBalancesOfAddresses(params: ApiBalanceRequest): Promise<ApiBalanceResponse[]> {
-    return this.connector
-      .get<{ result: ApiBalanceResponse[] }, ApiBalanceRequest>({
-        path: `data/balances`,
-        params,
-      })
-      .then((r) => r.result)
+    const { result } = await this.connector.get<{ result: ApiBalanceResponse[] }, ApiBalanceRequest>({
+      path: `data/balances`,
+      params,
+    })
+    return result
   }
 
   public async getOwnersOfToken(params: ApiOwnersRequest): Promise<string[]> {
-    return this.connector
-      .get<{ result: string[] }, ApiOwnersRequest>({
-        path: `data/owners`,
-        params,
-      })
-      .then((r) => r.result)
+    const { result } = await this.connector.get<{ result: string[] }, ApiOwnersRequest>({
+      path: `data/owners`,
+      params,
+    })
+    return result
   }
 
-  public async checkOwner(params: ApiCheckOwnersRequest): Promise<boolean> {
+  public checkOwner(params: ApiCheckOwnersRequest): Promise<boolean> {
     return this.connector.get<boolean, ApiCheckOwnersRequest>({
       path: `data/owners/address`,
       params,
@@ -84,47 +80,44 @@ export class TatumApi {
   }
 
   public async getTransactions(params: ApiTransactionsRequest): Promise<ApiTxData[]> {
-    return this.connector
-      .get<{ result: ApiTxData[] }, ApiTransactionsRequest>({
-        path: `data/transactions`,
-        params,
-      })
-      .then((r) => r.result)
+    const { result } = await this.connector.get<{ result: ApiTxData[] }, ApiTransactionsRequest>({
+      path: `data/transactions`,
+      params,
+    })
+    return result
   }
 
   public async getTransactionsByHash(params: ApiTransactionByHashRequest): Promise<ApiTxData[]> {
-    return this.connector
-      .get<{ result: ApiTxData[] }, ApiTransactionsRequest>({
-        path: `data/transactions`,
-        params,
-      })
-      .then((r) => r.result)
+    const { result } = await this.connector.get<{ result: ApiTxData[] }, ApiTransactionsRequest>({
+      path: `data/transactions`,
+      params,
+    })
+    return result
   }
 
   public async getEvents(params: ApiEventsRequest): Promise<Event[]> {
-    return this.connector
-      .get<{ result: Event[] }, ApiEventsRequest>({
-        path: `data/events`,
-        params,
-      })
-      .then((r) => r.result)
+    const { result } = await this.connector.get<{ result: Event[] }, ApiEventsRequest>({
+      path: `data/events`,
+      params,
+    })
+    return result
   }
 
-  public async getBlocks(params: ApiGetBlockRequest): Promise<Block[]> {
+  public getBlocks(params: ApiGetBlockRequest): Promise<Block[]> {
     return this.connector.get<Block[], ApiGetBlockRequest>({
       path: `data/blocks`,
       params,
     })
   }
 
-  public async getLatestBlock(params: ApiLatestBlockRequest): Promise<Block> {
+  public getLatestBlock(params: ApiLatestBlockRequest): Promise<Block> {
     return this.connector.get<Block, ApiLatestBlockRequest>({
       path: `data/blocks/latest`,
       params,
     })
   }
 
-  public async getTokenInfo(
+  public getTokenInfo(
     params: ApiTokensRequest,
   ): Promise<FungibleInfo | NftInfo | MultitokenInfo | NftTokenInfo> {
     return this.connector.get<FungibleInfo | NftInfo | MultitokenInfo | NftTokenInfo, ApiTokensRequest>({
@@ -133,14 +126,14 @@ export class TatumApi {
     })
   }
 
-  public async getUtxosByAddress(params: ApiUtxoByAddress): Promise<ApiUtxoResponse[]> {
+  public getUtxosByAddress(params: ApiUtxoByAddress): Promise<ApiUtxoResponse[]> {
     return this.connector.get<ApiUtxoResponse[], ApiUtxoByAddress>({
       path: `data/utxos`,
       params,
     })
   }
 
-  public async createFungibleToken(body: ApiCreateTokenRequest): Promise<TxIdResponse> {
+  public createFungibleToken(body: ApiCreateTokenRequest): Promise<TxIdResponse> {
     return this.connector.post<TxIdResponse, ApiCreateTokenRequest>({
       path: `contract/deploy`,
       body,
