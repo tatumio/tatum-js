b3c75272c7d6de0d90a13bb0a05e4c3deb693672
Alex Loiko <oleksandr.loiko@tatum.com>
2023-06-16 12:30:08 +0300

    ALL-1946 Fixed matic gas estimation

--- packages/shared/core/src/lib/third-party.common.ts
+++ packages/shared/core/src/lib/third-party.common.ts
@@ -1,3 +1,4 @@
 export const THIRD_PARTY_API = {
-  POLYGON_GAS_STATION: 'https://gasstation-mainnet.matic.network',
+  POLYGON_GAS_STATION: 'https://gasstation.polygon.technology/v2',
+  POLYGON_TESTNET_GAS_STATION: 'https://gasstation-testnet.polygon.technology/v2', //TODO introduce testnet gas estimation
 }
