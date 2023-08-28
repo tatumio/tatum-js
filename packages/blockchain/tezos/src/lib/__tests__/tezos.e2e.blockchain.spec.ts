4bd006ed66f421858b6439e72dd4cdfa73af36f0
Jiri Andras <jiriandras@protonmail.com>
2023-08-28 16:00:23 +0200

    ALL-1479-tezos-mint (#899)
    Co-authored-by: oliverjantar <jiri.andras@protonmail.com>

--- packages/blockchain/tezos/src/lib/__tests__/tezos.e2e.blockchain.spec.ts
+++ packages/blockchain/tezos/src/lib/__tests__/tezos.e2e.blockchain.spec.ts
@@ -1,11 +1,11 @@
 import { TatumTezosSDK } from '../tezos.sdk'
 import {
-  BurnTezosNft,
+  BurnTezosToken,
   DeployTezosNft,
-  MintTezosNft,
-  Nft,
-  TransferTezosNft,
-  UpdateOperatorTezosNft,
+  MintTezosToken,
+  Token,
+  TransferTezosToken,
+  UpdateOperatorTezosToken,
 } from '../services/tezos.tzip'
 
 describe.skip('TatumTezosSDK - blockchain', () => {
@@ -13,14 +13,16 @@ describe.skip('TatumTezosSDK - blockchain', () => {
 
   describe('Tezos NFT', () => {
     const testnet = 'https://rpc.ghostnet.teztnets.xyz'
-    const contractAddress = 'KT1Th4M8mbfS1PfEidswXp4pX5xcH4tdzKbD'
+    const contractAddress = 'KT1TY97bAsoXiXrhMorceLSm5GCQdraExjKm'
     const addr1 = 'tz1cCRCbBwnazV6howgD84a6fuhikKTnsWHZ'
     const addr2 = 'tz1f1nboqWEhZJHwZnxnokQ9QoTiT21qMZxG'
     const addr3 = 'tz1LszyhzQS6JgySBSNkM55szM18inGwEgPa'
-    const privateKey = ''
-    const privateKey2 = ''
+    const privateKey =
+      'edskRvJQqeNih91dkoRkTG1PWxV5AruYbtrkp5DKibL2iMkPGWBbdYp8jpsY3N3skCNrwf9socTv9Sstonkckc4jTb4vMQCSpn'
+    const privateKey2 =
+      'edskSAMSXCK8bm5E1xfGGUJ8MrJjrTPBEr9oHfU1ULt6nR6e4Wg2xXufLxZpcKUdu5rDXrzhWZw8NnTak9cxpJ7hjvbojR7b4p'
 
-    it.skip('Should deploy TZIP-12 contract on testnet', async () => {
+    it('Should deploy TZIP-12 contract on testnet', async () => {
       const sdk = TatumTezosSDK()
       const tezosWeb = sdk.tezosWeb
 
@@ -30,82 +32,82 @@ describe.skip('TatumTezosSDK - blockchain', () => {
         metadata: JSON.stringify({ name: 'contract name', symbol: 'TZ-gold' }),
       }
 
-      const contractAddress = await sdk.nft({ tezosWeb }).deploy.tzip12(body, testnet)
+      const opHash = await sdk.nft({ tezosWeb }).deploy.tzip12(body, testnet)
 
-      console.log(contractAddress)
+      console.log(opHash)
     })
 
-    it.skip('Should mint an NFT collection on testnet', async () => {
+    it('Should mint a Tzip-12 tokens on testnet', async () => {
       const sdk = TatumTezosSDK()
       const tezosWeb = sdk.tezosWeb
 
-      const token: Nft = {
+      const token: Token = {
         id: '6',
         ipfs: 'ipfs://Qmcz7iquheYehi4rmA2v9ZWakxMJCJusC5K7Harz8WNdza',
       }
 
-      const token2: Nft = {
+      const token2: Token = {
         id: '7',
         ipfs: 'ipfs://QmS7W7vtC6f3kSPUUkapVtW5bAXQvtn2tDDBDiBH1R3g2h',
       }
 
-      const body: MintTezosNft = {
+      const body: MintTezosToken = {
         contractAddress,
         owner: addr1,
         nfts: [token, token2],
         privateKey,
       }
 
-      const tx = await sdk.nft({ tezosWeb }).mintNft(body, testnet)
+      const tx = await sdk.nft({ tezosWeb }).mintTzip12(body, testnet)
 
       console.log(tx)
     })
 
-    it.skip('Should burn an NFT on testnet', async () => {
+    it('Should burn a Tzip-12 token on testnet', async () => {
       const sdk = TatumTezosSDK()
       const tezosWeb = sdk.tezosWeb
 
-      const body: BurnTezosNft = {
+      const body: BurnTezosToken = {
         contractAddress,
         owner: addr1,
-        tokens: ['1'],
+        tokens: ['6'],
         privateKey,
       }
 
-      const tx = await sdk.nft({ tezosWeb }).burnNft(body, testnet)
+      const tx = await sdk.nft({ tezosWeb }).burnTzip12(body, testnet)
 
       console.log(tx)
     })
-    it.skip('Should transfer an NFT on testnet', async () => {
+    it('Should add an operator to a token on testnet', async () => {
       const sdk = TatumTezosSDK()
       const tezosWeb = sdk.tezosWeb
 
-      const body: TransferTezosNft = {
-        contractAddress,
-        from: addr2,
-        to: addr3,
-        tokenId: '3',
-        amount: '1',
-        privateKey: privateKey2,
-      }
-
-      const tx = await sdk.nft({ tezosWeb }).transferNft(body, testnet)
-
-      console.log(tx)
-    })
-    it.skip('Should add operator to a token on testnet', async () => {
-      const sdk = TatumTezosSDK()
-      const tezosWeb = sdk.tezosWeb
-
-      const body: UpdateOperatorTezosNft = {
+      const body: UpdateOperatorTezosToken = {
         contractAddress,
         owner: addr1,
         operator: addr2,
-        tokenId: '3',
+        tokenId: '7',
         privateKey,
       }
 
       const tx = await sdk.nft({ tezosWeb }).updateOperator(body, true, testnet)
+
+      console.log(tx)
+    })
+    it('Should transfer a Tzip-12 token on testnet', async () => {
+      const sdk = TatumTezosSDK()
+      const tezosWeb = sdk.tezosWeb
+
+      const body: TransferTezosToken = {
+        contractAddress,
+        from: addr1,
+        to: addr3,
+        tokenId: '7',
+        amount: '1',
+        privateKey: privateKey2,
+      }
+
+      const tx = await sdk.nft({ tezosWeb }).transferTzip12(body, testnet)
 
       console.log(tx)
     })
