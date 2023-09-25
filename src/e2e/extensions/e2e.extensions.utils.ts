f4a86ad1280204696463d171d5294a348176cef0
Smrecz <diegoxlt@gmail.com>
2023-09-25 11:45:38 +0200

    ALL-2835 - Wallet Providers as extensions - tests added

---
+++ src/e2e/extensions/e2e.extensions.utils.ts
@@ -0,0 +1,51 @@
+import { ITatumSdkContainer, TatumConfig, TatumSdkExtension, TatumSdkWalletProvider } from "../../service";
+
+export class TestExtension extends TatumSdkExtension {
+    private readonly sdkConfig: TatumConfig
+
+    constructor(tatumSdkContainer: ITatumSdkContainer, private readonly mockTestExtension: any) {
+        super(tatumSdkContainer)
+        this.sdkConfig = this.tatumSdkContainer.getConfig()
+    }
+
+    async sayHello(){
+        this.mockTestExtension.sayHello()
+        this.mockTestExtension.network(this.sdkConfig.network)
+    }
+
+    init(): Promise<void> {
+        this.mockTestExtension.init()
+        return Promise.resolve(undefined)
+    }
+
+    destroy(): void {
+        this.mockTestExtension.destroy()
+    }
+}
+
+export class TestWalletProvider extends TatumSdkWalletProvider {
+    private readonly sdkConfig: TatumConfig
+
+    constructor(tatumSdkContainer: ITatumSdkContainer, private readonly mockTestExtension: any) {
+        super(tatumSdkContainer)
+        this.sdkConfig = this.tatumSdkContainer.getConfig()
+    }
+
+    async sayHello(){
+        this.mockTestExtension.sayHello()
+        this.mockTestExtension.network(this.sdkConfig.network)
+    }
+
+    async connect(){
+        return 'connected'
+    }
+
+    init(): Promise<void> {
+        this.mockTestExtension.init()
+        return Promise.resolve(undefined)
+    }
+
+    destroy(): void {
+        this.mockTestExtension.destroy()
+    }
+}
---
+++ src/e2e/extensions/tatum.extensions.spec.ts
@@ -0,0 +1,52 @@
+import { Ethereum, Network, TatumSDK } from '../../service'
+import { TestExtension, TestWalletProvider } from "./e2e.extensions.utils";
+
+const mockTestExtension = {
+  sayHello: jest.fn(),
+  init: jest.fn(),
+  destroy: jest.fn(),
+  network: jest.fn()
+}
+
+describe('Tatum Extension Ecosystem', () => {
+  describe('General Extension', () => {
+    it('should work after being registered', async () => {
+      const tatum = await TatumSDK.init<Ethereum>({
+        network: Network.ETHEREUM_SEPOLIA,
+        configureExtensions:[
+          {type: TestExtension, config: mockTestExtension}
+        ]
+      })
+
+      await tatum.extension(TestExtension).sayHello()
+
+      tatum.destroy()
+
+      expect(mockTestExtension.sayHello).toHaveBeenCalled()
+      expect(mockTestExtension.init).toHaveBeenCalled()
+      expect(mockTestExtension.destroy).toHaveBeenCalled()
+      expect(mockTestExtension.network).toBeCalledWith(Network.ETHEREUM_SEPOLIA)
+    })
+  })
+
+  describe('Wallet Extension', () => {
+    it('should work after being registered', async () => {
+      const tatum = await TatumSDK.init<Ethereum>({
+        network: Network.ETHEREUM_SEPOLIA,
+        configureWalletProviders:[
+          {type: TestWalletProvider, config: mockTestExtension}
+        ]
+      })
+
+      const result = await tatum.walletProvider.use(TestWalletProvider).connect()
+
+      tatum.destroy()
+
+      expect(result).toBe('connected')
+      expect(mockTestExtension.sayHello).toHaveBeenCalled()
+      expect(mockTestExtension.init).toHaveBeenCalled()
+      expect(mockTestExtension.destroy).toHaveBeenCalled()
+      expect(mockTestExtension.network).toBeCalledWith(Network.ETHEREUM_SEPOLIA)
+    })
+  })
+})
