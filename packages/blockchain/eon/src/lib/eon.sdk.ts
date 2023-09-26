a1a78c85a7cbef5bea8fc9fb4f356a036af7097f
jiri.andras <jiri.andras@tatum.io>
2023-09-26 11:15:52 +0200

    ALL-2699 Add mint support for Horizen EON

--- packages/blockchain/eon/src/lib/eon.sdk.ts
+++ packages/blockchain/eon/src/lib/eon.sdk.ts
@@ -1,26 +1,23 @@
 import { evmBasedSdk } from '@tatumio/shared-blockchain-evm-based'
 import { Blockchain } from '@tatumio/shared-core'
-import {} from '@tatumio/api-client'
 import { abstractSdkNft, SDKArguments } from '@tatumio/shared-abstract-sdk'
 import { eonWeb3 } from './services/eon.web3'
 import { eonTxService } from './services/eon.tx'
 
 const blockchain = Blockchain.EON
 
-export const TatumEonSDK = (args: SDKArguments, estimateGasFn: (tx: any) => Promise<number>) => {
+export const TatumEonSDK = (args: SDKArguments) => {
   const web3 = eonWeb3({ blockchain })
 
   const txService = eonTxService({
     blockchain,
     web3,
-    estimateGasFn,
   })
   const evmSdk = evmBasedSdk({ ...args, blockchain, web3 })
   const { nft } = abstractSdkNft()
 
   return {
     ...evmSdk,
-
     nft: {
       ...txService.erc721,
       ...nft,
