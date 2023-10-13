ec9d85f4bcda58f166420e27e57cd52bbc4fffe2
juraj.bacovcin <juraj.bacovcin@tatum.io>
2023-10-13 15:03:09 +0200

    ALL-3068 Adjust faucet tests based on happy flow changes

--- src/e2e/tatum.faucet.spec.ts
+++ src/e2e/tatum.faucet.spec.ts
@@ -2,6 +2,7 @@ import { Ethereum, Network, TatumSDK } from '../service'
 
 describe('Tatum faucet', () => {
   const SEPOLIA_VAULT = '0x712e3a792c974b3e3dbe41229ad4290791c75a82'
+  const SDK_VAULT = '0x8be1a5737817d3c8bf65039a8d374f920819563a'
 
   describe('invalid request', () => {
     const EXPECTED_RES = 'validation.failed'
@@ -28,16 +29,24 @@ describe('Tatum faucet', () => {
   })
 
   describe('valid request', () => {
-    const EXPECTED_RES = 'faucet.balance'
-
     it('should only stop at balance being above limit', async () => {
+      const tatum = await TatumSDK.init<Ethereum>({
+        network: Network.POLYGON_MUMBAI,
+      })
+      const res = await tatum.faucet.fund(SDK_VAULT)
+
+      await tatum.destroy()
+      expect(res.error?.code).toBe('faucet.balance')
+    })
+
+    it('should return valid transaction hash', async () => {
       const tatum = await TatumSDK.init<Ethereum>({
         network: Network.ETHEREUM_SEPOLIA,
       })
       const res = await tatum.faucet.fund(SEPOLIA_VAULT)
 
       await tatum.destroy()
-      expect(res.error?.code).toBe(EXPECTED_RES)
+      expect(res.data?.txId).toBeDefined()
     })
   })
 })
