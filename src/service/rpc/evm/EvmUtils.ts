e44d6c474dc2342fe8c64cb9b72cfb57e4226575
Smrecz <diegoxlt@gmail.com>
2023-11-22 10:19:19 +0100

    ALL-3495 - Add getBlockByNumber to possible archive list

--- src/service/rpc/evm/EvmUtils.ts
+++ src/service/rpc/evm/EvmUtils.ts
@@ -21,7 +21,6 @@ export const ARCHIVE_METHODS = [
   'getTransactionReceipt',
   'getUncleCountByBlockHash',
   'getUncleCountByBlockNumber',
-  'getBlockByNumber',
   'getBlockTransactionCountByHash',
   'getBlockTransactionCountByNumber',
   'getBlockReceipts',
@@ -37,6 +36,7 @@ export const POSSIBLE_ARCHIVE_METHODS = [
   { method: 'call', index: 1 }, // second param block
   { method: 'getBalance', index: 1 }, // second param block
   { method: 'getCode', index: 1 }, // second param block
+  { method: 'getBlockByNumber', index: 0 }, // second param block
 ]
 
 export const EvmUtils = {
@@ -47,7 +47,7 @@ export const EvmUtils = {
     }
 
     const possibleArchiveMethod = POSSIBLE_ARCHIVE_METHODS.find(
-      (possibleArchiveMethod) => possibleArchiveMethod.method === rpc.method,
+      (possibleArchiveMethod) => rpc.method.includes(possibleArchiveMethod.method),
     )
     if (possibleArchiveMethod) {
       const param = rpc?.params?.[possibleArchiveMethod.index]
