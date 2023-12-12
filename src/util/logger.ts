9095d5a62db745ef7b673e0722e178289498a99f
juraj.bacovcin <juraj.bacovcin@tatum.io>
2023-12-12 15:29:43 +0100

    ALL-3782 Adjust welcome message design

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,34 +2,47 @@ import chalk from 'chalk'
 
 const log = console.log
 
-export const logWelcomeMessage = () => {
-  const logoLineHorizontal = '#######'
-  const logoLineVertical = '#####'
-  const titleLine = '---------------------'
-  const endLine = '-----------------------------------------------------'
+const center = (message: string, pl = 0) => {
+  const cols = process?.stdout?.columns
+  if (!cols) return message
 
-  const logoGreenHorizontal = chalk.green(logoLineHorizontal)
-  const logoBlueHorizontal = chalk.blue(logoLineHorizontal)
-  const logoVertical = chalk.blue(logoLineVertical)
+  // eslint-disable-next-line no-control-regex
+  const messageWithoutAnsi = message.replace(/\x1B\[\d+m|\n/g, '')
+  return ' '.repeat((cols - messageWithoutAnsi.length) / 2 + pl) + message
+}
 
-  const logo = `
-            ${logoGreenHorizontal}
-  ${logoBlueHorizontal}   ${logoGreenHorizontal}
-  ${logoBlueHorizontal}
+const getLine = (count: number) => center(chalk.blue('-'.repeat(count)))
 
-        ${logoVertical}
-        ${logoVertical}
-        ${logoVertical}
-        ${logoVertical}
-  `
+const getLogo = () => {
+  const logoExtraPadding = 5
+  const logoHorizontal = '#'.repeat(logoExtraPadding + 2)
 
-  log(logo)
-  log(chalk.blue(titleLine))
-  log(chalk.green(' Welcome to Tatum! 👋'))
-  log(chalk.blue(titleLine))
-  log('\nThe complete SDK for Web3 development!\n')
-  log(`Random line with ${chalk.yellow('highligt')} for illustration purpose...`)
-  log(`Random line with ${chalk.red('warning')} for illustration purpose...\n`)
-  log(`Explore all SDK capabilities at ${chalk.cyan('https://docs.tatum.io')}\n`)
-  log(`${chalk.blue(endLine)}\n`)
+  const logoGreenHorizontal = chalk.green(logoHorizontal)
+  const logoBlueHorizontal = chalk.blue(logoHorizontal)
+
+  return (
+    center(logoGreenHorizontal + '\n', logoExtraPadding) +
+    center(`${logoBlueHorizontal}   ${logoGreenHorizontal}\n`) +
+    center(logoBlueHorizontal + '\n\n', 0 - logoExtraPadding) +
+    center(chalk.blue('#'.repeat(logoExtraPadding)) + '\n').repeat(4)
+  )
+}
+
+export const generateWelcomeLog = () => {
+  const titleLine = getLine(21)
+  const border = `\n${getLine(process?.stdout?.columns || 53)}\n`
+
+  log(border)
+  log(getLogo())
+
+  log(titleLine)
+  log(center(chalk.green(' Welcome to Tatum! 👋')))
+  log(titleLine, '\n')
+
+  log(center('The complete SDK for Web3 development!\n'))
+  log(center(`Random line with ${chalk.yellow('highligt')} for illustration purpose...`))
+  log(center(`Random line with ${chalk.red('warning')} for illustration purpose...\n`))
+
+  log(center(`Explore all SDK capabilities at ${chalk.cyan('https://docs.tatum.io')}`))
+  log(border)
 }
