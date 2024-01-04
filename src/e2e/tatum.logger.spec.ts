d6fdbfff80f5248913a2acc3699dbcaab89396ef
Filip Kaštovský <filip.kastovsky@tatum.io>
2024-01-04 16:16:12 +0100

    ALL-3453: add logger test suite

---
+++ src/e2e/tatum.logger.spec.ts
@@ -0,0 +1,29 @@
+import { Network } from '../dto'
+import { Ethereum, TatumSDK } from '../service'
+import { Logger } from '../service/logger'
+
+describe('Logger', () => {
+  let logger: Logger
+
+  beforeEach(() => {
+    logger = {
+      trace: jest.fn(),
+      debug: jest.fn(),
+      error: jest.fn(),
+      info: jest.fn(),
+      warn: jest.fn(),
+    }
+  })
+
+  it('should warn on missing API key', async () => {
+    const tatum = await TatumSDK.init<Ethereum>({
+      network: Network.ETHEREUM_SEPOLIA,
+      logger,
+    })
+    await tatum.destroy()
+
+    expect(logger.warn).toHaveBeenCalledWith(
+      'API key not provided - only a subset of SDK features will be enabled. Generate an API Key by accessing your Dashboard: https://co.tatum.io/signup',
+    )
+  })
+})
