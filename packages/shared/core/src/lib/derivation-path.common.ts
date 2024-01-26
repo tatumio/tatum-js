d868a1f8c0ec31d7935281f2ced86c0e3036417b
Lukas Kotol <lukas.kotol@gmail.com>
2024-01-26 13:52:17 +0100

    ALL-4358 Add FLR to blockchain

--- packages/shared/core/src/lib/derivation-path.common.ts
+++ packages/shared/core/src/lib/derivation-path.common.ts
@@ -32,6 +32,7 @@ export const DERIVATION_PATH: Record<Blockchain, string> = {
   TEZOS: "m/44'/1729'/0'/0",
   EON: "m/44'/60'/0'/0",
   CHILIZ: "m/44'/2182'/0'/0",
+  FLR: "m/44'/60'/0'/0",
 }
 
 export const COMMON_TESTNET_DERIVATION_PATH = "m/44'/1'/0'/0"
