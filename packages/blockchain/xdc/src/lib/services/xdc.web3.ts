d103efdad6cfb85464a031cc6fbd010bbcf5e46c
Lukas Kotol <lukas.kotol@gmail.com>
2024-02-21 12:34:12 +0100

    ALL-5195 - Fix XDC transaction

--- packages/blockchain/xdc/src/lib/services/xdc.web3.ts
+++ packages/blockchain/xdc/src/lib/services/xdc.web3.ts
@@ -1,28 +1,7 @@
 import { evmBasedWeb3 } from '@tatumio/shared-blockchain-evm-based'
 import Web3 from 'web3'
-import { EvmBasedBlockchain, httpHelper } from '@tatumio/shared-core'
-import { hexToNumber } from 'web3-utils'
+import { EvmBasedBlockchain } from '@tatumio/shared-core'
 
 export const xdcWeb3 = (args: { blockchain: EvmBasedBlockchain; client?: Web3 }) => {
-  const evmBasedWeb3Result = evmBasedWeb3(args)
-
-  return {
-    ...evmBasedWeb3Result,
-    async getGasPriceInWei(): Promise<string> {
-      const gasStationUrl = 'https://rpc.xinfin.network/'
-
-      try {
-        const { data } = await httpHelper.post(`${gasStationUrl}gasPrice`, {
-          jsonrpc: '2.0',
-          method: 'eth_gasPrice',
-          params: [],
-          id: 1,
-        })
-        const asNumber = hexToNumber(data.result)
-        return (data ? Web3.utils.toWei(`${asNumber}`, 'wei') : Web3.utils.toWei('0.25', 'gwei')).toString()
-      } catch (e) {
-        return Web3.utils.toWei('0.25', 'gwei')
-      }
-    },
-  }
+  return evmBasedWeb3(args)
 }
