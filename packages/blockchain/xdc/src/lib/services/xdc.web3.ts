0bb787c153326f3953defd5cb6b26ec1ee6268e5
Lukas Kotol <lukas.kotol@gmail.com>
2024-02-21 12:35:30 +0100

    ALL-5195 - Fix XDC transaction

--- packages/blockchain/xdc/src/lib/services/xdc.web3.ts
+++ packages/blockchain/xdc/src/lib/services/xdc.web3.ts
@@ -3,5 +3,6 @@ import Web3 from 'web3'
 import { EvmBasedBlockchain } from '@tatumio/shared-core'
 
 export const xdcWeb3 = (args: { blockchain: EvmBasedBlockchain; client?: Web3 }) => {
+  console.log('xdcWeb3')
   return evmBasedWeb3(args)
 }
