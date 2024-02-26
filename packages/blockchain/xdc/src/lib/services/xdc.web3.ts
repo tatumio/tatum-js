64a4eecf7b783ec7dc4ec1d60a8a7035db6b48ab
Lukas Kotol <lukas.kotol@gmail.com>
2024-02-26 12:22:35 +0100

    ALL-5214 - Fix XDC estimate

--- packages/blockchain/xdc/src/lib/services/xdc.web3.ts
+++ packages/blockchain/xdc/src/lib/services/xdc.web3.ts
@@ -1,7 +1,20 @@
 import { evmBasedWeb3 } from '@tatumio/shared-blockchain-evm-based'
 import Web3 from 'web3'
 import { EvmBasedBlockchain } from '@tatumio/shared-core'
+import BigNumber from 'bignumber.js'
 
 export const xdcWeb3 = (args: { blockchain: EvmBasedBlockchain; client?: Web3 }) => {
-  return evmBasedWeb3(args)
+  const evmBasedWeb3Result = evmBasedWeb3(args)
+
+  return {
+    ...evmBasedWeb3Result,
+    async getGasPriceInWei(): Promise<string> {
+      try {
+        const gasPriceInWei = await evmBasedWeb3Result.getGasPriceInWei()
+        return BigNumber(gasPriceInWei).times(2).toString()
+      } catch (e) {
+        return '20000000000'
+      }
+    },
+  }
 }
