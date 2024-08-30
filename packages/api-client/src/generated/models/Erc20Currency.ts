1b44570bce50855f4566c41bcd47c6f1a0f4e49e
alexloiko <13203602+alexloiko@users.noreply.github.com>
2024-08-30 16:23:00 +0300

    ALL-8604 Added POL_ETH currency to v2 (#1130)
    Co-authored-by: Alex Loiko <oleksandr.loiko@tatum.com>

--- packages/api-client/src/generated/models/Erc20Currency.ts
+++ packages/api-client/src/generated/models/Erc20Currency.ts
@@ -2,4 +2,4 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export type Erc20Currency = 'BAT' | 'BUSD' | 'COIIN' | 'ETH' | 'FREE' | 'GMC' | 'LATOKEN' | 'LEO' | 'LINK' | 'MATIC_ETH' | 'MKR' | 'MMY' | 'PAX' | 'PAXG' | 'PLTC' | 'REVV' | 'SAND' | 'TUSD' | 'UNI' | 'USDC' | 'USDT' | 'WBTC' | 'XCON';
+export type Erc20Currency = 'BAT' | 'BUSD' | 'COIIN' | 'ETH' | 'FREE' | 'GMC' | 'LATOKEN' | 'LEO' | 'LINK' | 'MATIC_ETH' | 'POL_ETH' | 'MKR' | 'MMY' | 'PAX' | 'PAXG' | 'PLTC' | 'REVV' | 'SAND' | 'TUSD' | 'UNI' | 'USDC' | 'USDT' | 'WBTC' | 'XCON';
