550f54e64b78409e64b082f5f1b5c914e2c252a8
Filip Kaštovský <devfilipkastovsky@gmail.com>
2025-01-29 13:54:14 +0100

    Feature/all 10234 add ron (#1142)
    ALL-10234: add ronin
    
    Co-authored-by: Filip Kaštovský <filip.kastovsky@tatum.io>

--- packages/shared/core/src/lib/derivation-path.common.ts
+++ packages/shared/core/src/lib/derivation-path.common.ts
@@ -40,6 +40,7 @@ export const DERIVATION_PATH: Record<Blockchain, string> = {
   FTM: "m/44'/60'/0'/0",
   TON: "m/44'/60'/0'/0",
   ZK_SYNC: "m/44'/60'/0'/0",
+  RON: "m/44'/60'/0'/0",
 }
 
 export const COMMON_TESTNET_DERIVATION_PATH = "m/44'/1'/0'/0"
