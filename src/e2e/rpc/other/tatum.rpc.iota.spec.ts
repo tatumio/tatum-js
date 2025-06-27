f2f8b0d5ae796940dab3649c77e8edca07491794
Levan Ozashvili <levan.ozashvili@tatum.io>
2025-06-27 13:32:26 +0400

    ALL-12600: Skip some tests.

--- src/e2e/rpc/other/tatum.rpc.iota.spec.ts
+++ src/e2e/rpc/other/tatum.rpc.iota.spec.ts
@@ -4,7 +4,7 @@ import { e2eUtil } from '../../e2e.util'
 const getIotaRpc = async (testnet?: boolean) => await TatumSDK.init<Iota>(e2eUtil.initConfig(testnet ? Network.IOTA_TESTNET : Network.IOTA))
 
 describe.each([true, false])('Iota', (testnet) => {
-  describe(`${testnet ? Network.IOTA_TESTNET : Network.IOTA}`, () => {
+  describe.skip(`${testnet ? Network.IOTA_TESTNET : Network.IOTA}`, () => {
     it('getNodeInfo', async () => {
       const tatum = await getIotaRpc(testnet)
       const info = await tatum.rpc.getNodeInfo()
